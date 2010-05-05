d88fe6be4f3af7103636410bb75ab63a6b40a4b5
Jerome Vouillon <jerome.vouillon@pps.jussieu.fr>
2010-05-06 00:08:15 +0200

    -
    Ignore-this: 66f83e9dd63e7640cb1ca12c41a5b2ce
    
    darcs-hash:20100505220815-41a61-77873f476d9de36cfd2015216823647f8a4a6f17

--- main.ml
+++ main.ml
@@ -5,6 +5,7 @@ let _ =
   Code.print_program (fun _ _ -> "") p;
 *)
 
+Format.eprintf "Tail-call optimization...@.";
   let p = Tailcall.f p in
 
 Format.eprintf "Data flow...@.";
@@ -15,23 +16,16 @@ Format.eprintf "Control flow simplifications...@.";
   let p = Control.simpl p in
 Format.eprintf "Dead-code...@.";
   let (p, live_vars) = Deadcode.f p in
-Format.eprintf "Tail-call optimization...@.";
 
 (*
-
-  let p = Control.simpl p in
-
+Format.eprintf "Data flow...@.";
   let p = Flow.f p in
-  let p = Deadcode.f p in
-
-  let p = Control.simpl p in
+Format.eprintf "Dead-code...@.";
+  let (p, _) = Deadcode.f p in
 *)
+
 (*
 *)
   Code.print_program (fun _ _ -> "") p;
 
-(*
-Struct.f p;
-*)
-
-Generate.f p live_vars
+  Generate.f p live_vars
