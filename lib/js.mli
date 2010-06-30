e510ecfb52dfd41f4eaac476bf3dfa07c8159924
jerome.vouillon <jerome.vouillon@pps.jussieu.fr>
2010-06-30 22:10:59 +0200

    Property 'length' missing from String objects
    Ignore-this: f2f6e1b2752ba98403c56ce49e781210
    
    darcs-hash:20100630201059-c8b6b-2e985b53e0916e445c157ea4b2eb76c38b1f763d

--- lib/js.mli
+++ lib/js.mli
@@ -197,6 +197,7 @@ class type js_string = object
   method toLocaleLowerCase : js_string t meth
   method toUpperCase : js_string t meth
   method toLocaleUpperCase : js_string t meth
+  method length : int readonly_prop
 end
 
 (** Specification of Javascript regular expression objects. *)
