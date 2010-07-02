f083b721ab4a20bf44c7d9ae4ce4cf3562ed41c2
jerome.vouillon <jerome.vouillon@pps.jussieu.fr>
2010-07-02 16:39:31 +0200

    Split jslib runtime: move apart code specific to Js_of_ocaml
    Ignore-this: 19504e23f4445fc4cffe58fe6f9cf7ac
    
    darcs-hash:20100702143931-c8b6b-97ab27c06be119a7091846e37ae3fb1288a95f3c

--- runtime/jslib.js
+++ runtime/jslib.js
@@ -19,89 +19,10 @@
 
 ///////////// Jslib
 
-//Provides: caml_js_from_bool const
-function caml_js_from_bool(x) { return !!x; }
-//Provides: caml_js_to_bool const
-function caml_js_to_bool(x) { return +x; }
-//Provides: caml_js_from_float const
-function caml_js_from_float(x) { return x; }
-//Provides: caml_js_to_float const
-function caml_js_to_float(x) { return x; }
-//Provides: caml_js_from_string mutable
-function caml_js_from_string(s) { return s.toString(); }
-//Provides: caml_js_to_string const
-//Requires: MlString
-function caml_js_to_string(s) { return new MlWrappedString(s); }
-//Provides: caml_js_from_array mutable
-function caml_js_from_array(a) { return a.slice(1); }
-//Provides: caml_js_to_array mutable
-function caml_js_to_array(a) { return [0].concat(a); }
-
 //Provides: caml_js_set
 function caml_js_set(o,f,v) { o[f]=v; }
 //Provides: caml_js_get mutable
 function caml_js_get(o,f) { return o[f]; }
-
-//Provides: caml_js_var mutable
-function caml_js_var(x) { return eval(x.toString()); }
-//Provides: caml_js_const const
-function caml_js_const(x) {
-  switch (caml_string_to_js(x)) {
-  case "null": return null;
-  case "true": return true;
-  case "false": return false;
-  // case "undefined: return undefined;
-  }
-}
-//Provides: caml_js_call
-function caml_js_call(f, o, args) { return f.apply(o, args.slice(1)); }
-//Provides: caml_js_fun_call
-function caml_js_fun_call(f, args) { return f.apply(null, args.slice(1)); }
-//Provides: caml_js_meth_call
-function caml_js_meth_call(o, f, args) { return o.f.apply(o, args.slice(1)); }
-//Provides: caml_js_new
-function caml_js_new(c, a) {
-  switch (a.length) {
-  case 1: return new c;
-  case 2: return new c (a[1]);
-  case 3: return new c (a[1],a[2]);
-  case 4: return new c (a[1],a[2],a[3]);
-  case 5: return new c (a[1],a[2],a[3],a[4]);
-  case 6: return new c (a[1],a[2],a[3],a[4],a[5]);
-  case 7: return new c (a[1],a[2],a[3],a[4],a[5],a[6]);
-  case 8: return new c (a[1],a[2],a[3],a[4],a[5],a[6], a[7]);
-  }
-  function F() { return c.apply(this, args.slice(1)); }
-  F.prototype = c.prototype;
-  return new F;
-}
-//Provides: caml_js_wrap_callback const
-//Requires: caml_call_gen
-function caml_js_wrap_callback(f) {
-  var toArray = Array.prototype.slice;
-  return function () {
-    var args = (arguments.length > 0)?toArray.call (arguments):[0];
-    return caml_call_gen(f, args);
-  }
-}
-//Provides: caml_js_wrap_meth_callback const
-//Requires: caml_call_gen
-function caml_js_wrap_meth_callback(f) {
-  var toArray = Array.prototype.slice;
-  return function () {
-    var args = (arguments.length > 0)?toArray.call (arguments):[0];
-    args.unshift (this);
-    return caml_call_gen(f, args);
-  }
-}
-//Provides: caml_js_equals mutable
-function caml_js_equals (x, y) { return +(x == y); }
-//Provides: caml_js_from_byte_string
-function caml_js_from_byte_string (s) {return s.getFullBytes();}
-//Provides: caml_js_to_byte_string
-function caml_js_to_byte_string (s) {return new MlString (s);}
-
-/////////////////////////////////////////////////////////////////////
 
 //Provides: caml_js_on_ie const
 function caml_js_on_ie () {
