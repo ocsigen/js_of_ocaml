64db940af1cd577bf747dd9ad9c945ad4109ea07
jerome.vouillon <jerome.vouillon@pps.jussieu.fr>
2010-11-02 17:37:33 +0100

    Revised data flow analysis
    Ignore-this: 220f0271298c90a801aa7524576bdbca
    
    darcs-hash:20101102163733-c8b6b-09ba42311a6ddee2990bc8ca12a5ea9d2178df63

--- compiler/flow.ml
+++ compiler/flow.ml
@@ -109,7 +109,7 @@ let program_deps (_, blocks, _) =
        match block.branch with
          Return _ | Raise _ | Stop ->
            ()
-       | Branch cont ->
+       | Branch cont | Poptrap cont ->
            cont_deps blocks vars deps defs cont
        | Cond (_, _, cont1, cont2) ->
            cont_deps blocks vars deps defs cont1;
@@ -118,43 +118,35 @@ let program_deps (_, blocks, _) =
            Array.iter (fun cont -> cont_deps blocks vars deps defs cont) a1;
            Array.iter (fun cont -> cont_deps blocks vars deps defs cont) a2
        | Pushtrap (cont, _, _, _) ->
-           cont_deps blocks vars deps defs cont
-       | Poptrap cont ->
            cont_deps blocks vars deps defs cont)
     blocks;
   (vars, deps, defs)
 
+let var_set_lift f s =
+  VarSet.fold (fun y s -> VarSet.union (f y) s) s VarSet.empty
+
 let propagate1 deps defs st x =
   match defs.(Var.idx x) with
     Param ->
-      VarSet.empty
+      VarSet.singleton x
   | Phi s ->
-      VarSet.fold (fun y s -> VarSet.union (VarMap.find y st) s) s VarSet.empty
+      var_set_lift (fun y -> VarMap.find y st) s
   | Expr e ->
       match e with
         Const _ | Constant _  | Apply _ | Prim _
       | Closure _ | Block _ ->
           VarSet.singleton x
       | Field (y, n) ->
-          let s = VarMap.find y st in
-          VarSet.fold
-            (fun z s ->
+          var_set_lift
+            (fun z ->
                match defs.(Var.idx z) with
-                 Expr (Block (_, a)) ->
-(*Format.eprintf "%d/%d@." n (Array.length a);*)
-                   if n >= Array.length a then s else begin
-                     let t = a.(n) in
-(*Format.eprintf "%a --> %a@." Var.print t Var.print x;*)
-                     add_dep deps x t;
-                     let s' = VarMap.find t st in
-                     if VarSet.is_empty s' then
-                       VarSet.add t s
-                     else
-                       VarSet.union s' s
-                   end
+                 Expr (Block (_, a)) when n < Array.length a ->
+                   let t = a.(n) in
+                   add_dep deps x t;
+                   VarMap.find t st
                | Phi _ | Param | Expr _ ->
-                   s)
-            s VarSet.empty
+                   VarSet.empty)
+            (VarMap.find y st)
       | Variable _ ->
           assert false
 
@@ -177,139 +169,148 @@ let solver1 vars deps defs =
 
 (****)
 
-type approx = Known | AnyBlock | Any
+type mutability_state =
+  { defs : def array;
+    known_origins : Code.VarSet.t Code.VarMap.t;
+    may_escape : bool array;
+    possibly_mutable : bool array }
 
-let a_max u v =
-  match u, v with
-    Any, _ | _, Any -> Any
-  | Known, Known    -> Known
-  | _               -> AnyBlock
-
-let rec block_approx defs def_approx approx x =
-  let s = VarMap.find x def_approx in
+let rec block_escape st x =
   VarSet.iter
     (fun y ->
-       match defs.(Var.idx y) with
-         Expr (Block (_, l)) ->
-           let idx = Var.idx y in
-           if not approx.(idx) then begin
-             approx.(idx) <- true;
-             Array.iter (fun z -> block_approx defs def_approx approx z) l
-           end
-       | _ ->
-           ())
-    s
+       let idx = Var.idx y in
+       if not st.may_escape.(idx) then begin
+         st.may_escape.(idx) <- true;
+         st.possibly_mutable.(idx) <- true;
+         match st.defs.(Var.idx y) with
+           Expr (Block (_, l)) -> Array.iter (fun z -> block_escape st z) l
+         | _                   -> ()
+       end)
+    (VarMap.find x st.known_origins)
 
-let expr_approx defs def_approx approx x e =
+let expr_escape st x e =
   match e with
     Const _ | Constant _ | Closure _ | Block _ | Field _ ->
       ()
   | Apply (_, l, _) ->
-      List.iter (fun x -> block_approx defs def_approx approx x) l
+      List.iter (fun x -> block_escape st x) l
   | Prim (_, l) ->
       List.iter
         (fun x ->
            match x with
-             Pv x -> block_approx defs def_approx approx x
+             Pv x -> block_escape st x
            | Pc _ -> ())
         l
   | Variable y ->
       assert false
 
-let program_approx defs def_approx (_, blocks, _) =
+let program_escape defs known_origins (_, blocks, _) =
   let nv = Var.count () in
-  let approx = Array.make nv false in
+  let may_escape = Array.make nv false in
+  let possibly_mutable = Array.make nv false in
+  let st =
+    { defs = defs;
+      known_origins = known_origins;
+      may_escape = may_escape;
+      possibly_mutable = possibly_mutable }
+  in
   AddrMap.iter
     (fun pc block ->
        List.iter
          (fun i ->
             match i with
               Let (x, e) ->
-                expr_approx defs def_approx approx x e
+                expr_escape st x e
             | Set_field (x, _, _) | Array_set (x, _, _) | Offset_ref (x, _) ->
-                let s = VarMap.find x def_approx in
-                VarSet.iter (fun y -> approx.(Var.idx y) <- true) s)
+                VarSet.iter (fun y -> possibly_mutable.(Var.idx y) <- true)
+                  (VarMap.find x known_origins))
          block.body;
        match block.branch with
          Return x | Raise x ->
-           block_approx defs def_approx approx x
+           block_escape st x
        | Stop | Branch _ | Cond _ | Switch _ | Pushtrap _ | Poptrap _ ->
            ())
     blocks;
-  approx
+  possibly_mutable
 
-let propagate2 defs def_approx approx st x =
+(****)
+
+type approx = Known | Maybe_unknown
+
+let a_max u v =
+  match u, v with
+    Known, Known    -> Known
+  | _               -> Maybe_unknown
+
+let approx_lift f s = VarSet.fold (fun y u -> a_max (f y) u) s Known
+
+let propagate2 defs known_origins possibly_mutable st x =
   match defs.(Var.idx x) with
     Param ->
-      Any
+      false
   | Phi s ->
-      VarSet.fold (fun y u -> a_max (VarMap.find y st) u) s Known
+      VarSet.exists (fun y -> VarMap.find y st) s
   | Expr e ->
       match e with
-        Const _ | Constant _ | Closure _ ->
-          Known
-      | Apply _ | Prim _ ->
-          AnyBlock
-      | Block _ ->
-          if approx.(Var.idx x) then AnyBlock else Known
+        Const _ | Constant _ | Closure _ | Apply _ | Prim _ | Block _ ->
+          false
       | Field (y, n) ->
-          if VarMap.find y st <> Known then Any else begin
-            let s = VarMap.find y def_approx in
-            VarSet.fold
-              (fun z u ->
-                 match defs.(Var.idx z) with
-                   Expr (Block (_, a)) ->
-                     if n >= Array.length a then
-                       Any
-                     else if VarMap.find z st <> Known then
-                       Any
-                     else begin
-                       let t = a.(n) in
-                       a_max (VarMap.find t st) u
-                     end
-                 | Phi _ | Param | Expr _ ->
-                     Any)
-              s Known
-          end
+          VarMap.find y st
+            ||
+          VarSet.exists
+            (fun z ->
+               match defs.(Var.idx z) with
+                 Expr (Block (_, a)) ->
+                   n >= Array.length a
+                    ||
+                   possibly_mutable.(Var.idx z)
+                    ||
+                   VarMap.find (a.(n)) st
+               | Phi _ | Param | Expr _ ->
+                   true)
+              (VarMap.find y known_origins)
       | Variable _ ->
           assert false
 
 module Domain2 = struct
-  type t = approx
-  let equal (u : approx) v = u = v
-  let bot = Known
+  type t = bool
+  let equal (u : bool) v = u = v
+  let bot = false
 end
 
 module Solver2 = G.Solver (Domain2)
 
-let solver2 vars deps defs def_approx approx =
+let solver2 vars deps defs known_origins possibly_mutable =
   let g =
     { G.domain = vars;
       G.fold_children = fun f x a -> VarSet.fold f (deps.(Var.idx x)) a }
   in
-  Solver2.f g (propagate2 defs def_approx approx)
+  Solver2.f g (propagate2 defs known_origins possibly_mutable)
 
 (****)
 
-let the_def_of (defs, def_approx, known_approx) x =
-  let s = VarMap.find x def_approx in
-  if VarSet.cardinal s = 1 && VarMap.find x known_approx <> Any then
-    match defs.(Var.idx (VarSet.choose s)) with
-      Expr e -> Some e
-    | _      -> None
-  else
-    None
+let get_approx (defs, known_origins, maybe_unknown) f top join x =
+  let s = VarMap.find x known_origins in
+  if VarMap.find x maybe_unknown then top else
+  match VarSet.cardinal s with
+    0 -> top
+  | 1 -> f (VarSet.choose s)
+  | _ -> VarSet.fold (fun x u -> join (f x) u) s (f (VarSet.choose s))
 
-let function_cardinality (defs, def_approx, known_approx) x =
-  let card x =
-    match defs.(Var.idx x) with
-      Expr (Closure (l, _)) -> Some (List.length l)
-    | _                     -> None
-  in
-  let s = VarMap.find x def_approx in
-  if VarMap.find x known_approx = Any || VarSet.cardinal s = 0 then None else
-  let n = card (VarSet.choose s) in
-  if VarSet.for_all (fun x -> card x = n) s then n else None
+let the_def_of ((defs, _, _) as info) x =
+  get_approx info
+    (fun x -> match defs.(Var.idx x) with Expr e -> Some e | _ -> None)
+    None (fun u v -> None) x
+
+let function_cardinality ((defs, _, _) as info) x =
+  get_approx info
+    (fun x ->
+       match defs.(Var.idx x) with
+         Expr (Closure (l, _)) -> Some (List.length l)
+       | _                     -> None)
+    None
+    (fun u v -> match u, v with Some n, Some m when n = m -> u | _ -> None)
+    x
 
 let specialize_instr info i =
   match i with
@@ -395,35 +396,57 @@ let specialize_instrs info (pc, blocks, free_pc) =
     AddrMap.map
       (fun block ->
          { block with Code.body =
-             List.map
-               (fun i -> specialize_instr info i)
-               block.body })
+             List.map (fun i -> specialize_instr info i) block.body })
       blocks
   in
   (pc, blocks, free_pc)
 
 (****)
 
-let build_subst def_approx known_approx =
+(*XXX Maybe we could iterate? *)
+let direct_approx defs known_origins maybe_unknown possibly_mutable x =
+  match defs.(Var.idx x) with
+    Expr (Field (y, n)) ->
+      get_approx (defs, known_origins, maybe_unknown)
+        (fun z ->
+           if possibly_mutable.(Var.idx z) then None else
+           match defs.(Var.idx z) with
+             Expr (Block (_, a)) when n < Array.length a ->
+               Some a.(n)
+           | _ ->
+               None)
+        None
+        (fun u v ->
+           match u, v with
+             Some n, Some m when Var.compare n m = 0 -> u
+           | _ -> None)
+        y
+  | _ ->
+      None
+
+let build_subst defs known_origins maybe_unknown possibly_mutable =
   let nv = Var.count () in
   let subst = Array.make nv None in
-  VarMap.fold
-    (fun x u () ->
-       if u <> Any then begin
-         let s = VarMap.find x def_approx in
+  VarMap.iter
+    (fun x u ->
+       if not u then begin
+         let s = VarMap.find x known_origins in
          if VarSet.cardinal s = 1 then
            subst.(Var.idx x) <- Some (VarSet.choose s)
-       end)
-    known_approx ();
+       end;
+       if subst.(Var.idx x) = None then
+         subst.(Var.idx x) <-
+           direct_approx defs known_origins maybe_unknown possibly_mutable x)
+    maybe_unknown;
   subst
 
 (****)
 
 let f ((pc, blocks, free_pc) as p) =
   let (vars, deps, defs) = program_deps p in
-  let def_approx = solver1 !vars deps defs in
-  let approx = program_approx defs def_approx p in
-  let known_approx = solver2 !vars deps defs def_approx approx in
+  let known_origins = solver1 !vars deps defs in
+  let possibly_mutable = program_escape defs known_origins p in
+  let maybe_unknown = solver2 !vars deps defs known_origins possibly_mutable in
 
   if debug () then begin
     VarMap.iter
@@ -431,15 +454,12 @@ let f ((pc, blocks, free_pc) as p) =
          if not (VarSet.is_empty s) (*&& VarSet.choose s <> x*) then begin
     Format.eprintf "%a: {%a} / %s@."
     Var.print x Code.print_var_list (VarSet.elements s)
-    (match VarMap.find x known_approx with
-       Known -> "known"
-     | AnyBlock -> "anyblock"
-     | Any -> "any")
+    (if VarMap.find x maybe_unknown then "any" else "known")
            end)
-        def_approx;
+        known_origins;
   end;
 
-  let p = specialize_instrs (defs, def_approx, known_approx) p in
-  let s = build_subst def_approx known_approx in
+  let p = specialize_instrs (defs, known_origins, maybe_unknown) p in
+  let s = build_subst defs known_origins maybe_unknown possibly_mutable in
   let p = Subst.program (Subst.from_array s) p in
   p
