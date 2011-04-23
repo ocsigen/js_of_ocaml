40751fb3be326c4d874a28e01fdaf40b4ea78f89
raphael proust <none>
2011-04-23 12:42:34 +0200

    Moving comments in Url for better doc generation
    Ignore-this: b72689676daa8c08e3a4b0407ecc186
    
    darcs-hash:20110423104234-8ea92-3c29e37f3cc3b8ad6e308015788fcc8446c37b89

--- lib/url.mli
+++ lib/url.mli
@@ -26,17 +26,18 @@
 (** The first functions are mainly from and to string conversion functions for
      the different parts of a url. *)
 
-val urldecode : string -> string
 (** [urldecode s] swaps percent encoding characters for their usual
     representation. *)
+val urldecode : string -> string
 
-val urlencode : ?with_plus:bool -> string -> string
 (** [urlencode ?with_plus s] replace characters for their percent encoding
     representation. Note that the '/' (slash) character is escaped as well. If
     [with_plus] is [true] (default) then ['+']'s are escaped as ["%2B"]. If not,
     ['+']'s are left as is. *)
+val urlencode : ?with_plus:bool -> string -> string
 
 
+(** The type for HTTP(s) url. *)
 type http_url = {
   hu_host : string; (** The host part of the url. *)
   hu_port : int; (** The port for the connection if any. *)
@@ -46,90 +47,93 @@ type http_url = {
                                              association list.*)
   hu_fragment : string; (** The fragment part (after the ['#'] character). *)
 }
-(** The type for HTTP url. *)
 
+(** The type for local file urls. *)
 type file_url = {
   fu_path : string list;
   fu_path_string : string;
   fu_arguments : (string * string) list;
   fu_fragment : string;
 }
-(** The type for local file urls. *)
 
+(** The type for urls. *)
 type url =
-  | Http of http_url
-  | Https of http_url
-  | File of file_url
-(** The type for urls. [File] is for local files and [Exotic s] is for
-    unknown/unsupported protocols. *)
+  | Http of http_url  (**Non secure HTTP urls*)
+  | Https of http_url (**Secure HTTPS urls*)
+  | File of file_url  (**Local files*)
 
-val default_http_port : int
 (** The default port for [Http] communications (80). *)
+val default_http_port : int
 
-val default_https_port : int
 (** The default port for [Https] communications (443). *)
+val default_https_port : int
 
-val path_of_path_string : string -> string list
 (** [path_of_path_string s] splits [s] on each ["/"] character. *)
+val path_of_path_string : string -> string list
 
-val encode_arguments : (string * string) list -> string
 (** [encode_arguments a] expects a list of pair of values of the form
     [(name,value)] were [name] is the name of an argument and [value] it's
     associated value. *)
+val encode_arguments : (string * string) list -> string
 
-val decode_arguments : string -> (string * string) list
 (** [decode_arguments s] parses [s] returning the sliced-diced
     association list. *)
+val decode_arguments : string -> (string * string) list
+
 
 (** The following part allow one to handle Url object in a much higher level
     than what a string provides. *)
 
 
-
-val url_of_string : string -> url option
 (** [url_of_string s] parses [s] and builds a value of type [url] if [s] is not
     a valid url string, it returns [None]. *)
+val url_of_string : string -> url option
 
-val string_of_url : url -> string
 (** [string_of_url u] returns a valid string representation of [u]. Note that
   * [string_of_url ((fun Some u -> u) (url_of_string s))] is NOT necessarily
   * equal to [s]. However [url_of_string (string_of_url u) = u]. *)
+val string_of_url : url -> string
 
-module Current :
 (** This module can be used to handle the Url associated to the current
     document. *)
+module Current :
 
   sig
 
+    (** The host part of the current url. *)
     val host : string
 
+    (** The port of the current url. *)
     val port : int option
 
+    (** The path of the current url as one long string. *)
     val path_string : string
 
+    (** The path of the current url as a list of small string. *)
     val path : string list
 
+    (** The arguments of the current url as an association list. *)
     val arguments : (string * string) list
 
-    val get_fragment : unit -> string
     (** Because the [fragment] of the Url for the current document can change
         dynamically, we use a functional value here. *)
+    val get_fragment : unit -> string
 
-    val set_fragment : string -> unit
     (** [set_fragment s] replaces the current fragment by [s]. *)
+    val set_fragment : string -> unit
 
-    val get : unit -> url option
     (** [get ()] returns a value of type {!url} with fields reflecting the
         state of the current Url. *)
+    val get : unit -> url option
 
+    (** [set u] replaces the current Url for [u]. {b WARNING:} Causes the
+        document to change. *)
     val set : url -> unit
-    (** [set u] replaces the current Url for [u]. {b WARNING:} Causes the document
-        to change. *)
 
-    val as_string : string
     (** [as_string] is the original string representation of the current Url. It
         is NOT necessarily equals to [string_of_url (get ())] but
         [url_of_string as_string = get ()] holds. *)
+    val as_string : string
 
 
   end
