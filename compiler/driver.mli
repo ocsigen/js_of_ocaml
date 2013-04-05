80e62b76d4498c6a175041bfe29529674d51370d
jerome.vouillon <jerome.vouillon@pps.jussieu.fr>
2013-04-05 10:27:55 +0200

    Resolve merge conflict
    Ignore-this: 78e8626b9072a7fe0811baae1221362c
    
    darcs-hash:20130405082755-c8b6b-a2380b16c04718a1f71aec993e45098434312073

--- compiler/driver.mli
+++ compiler/driver.mli
@@ -18,7 +18,9 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  *)
 
-val f : ?standalone:bool -> Code.program * Parse.debug_loc -> Pretty_print.t -> unit
+val f :
+  ?standalone:bool -> ?linkall:bool ->
+  Code.program * Parse_bytecode.debug_loc -> Pretty_print.t -> unit
 
 val from_string : string array -> string -> Pretty_print.t -> unit
 
