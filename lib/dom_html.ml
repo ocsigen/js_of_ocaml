b9da812b06f1aaea480f810395df440510d29e25
Jerome Vouillon <jerome.vouillon@pps.univ-paris-diderot.fr>
2013-08-20 15:06:35 +0200

    Dom_html: make placeholder property of input elements write only for now.
    Indeed, this property is not supported by IE 9.

--- lib/dom_html.ml
+++ lib/dom_html.ml
@@ -541,7 +541,7 @@ class type inputElement = object ('self)
   method select : unit meth
   method click : unit meth
   method files : File.fileList t optdef readonly_prop
-  method placeholder : js_string t prop
+  method placeholder : js_string t writeonly_prop
   method onselect : ('self t, event t) event_listener prop
   method onchange : ('self t, event t) event_listener prop
   method oninput : ('self t, event t) event_listener prop
@@ -565,7 +565,7 @@ class type textAreaElement = object ('self)
   method blur : unit meth
   method focus : unit meth
   method select : unit meth
-  method placeholder : js_string t prop
+  method placeholder : js_string t writeonly_prop
   method onselect : ('self t, event t) event_listener prop
   method onchange : ('self t, event t) event_listener prop
   method oninput : ('self t, event t) event_listener prop
