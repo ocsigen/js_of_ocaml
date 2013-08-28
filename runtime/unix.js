59e36f7d95c0928d30ac87f00b9cd2ec42aa7692
Hugo Heuzard <hugo.heuzard@gmail.com>
2013-08-28 16:24:04 -0700

    Runtime: fix Unix.js

--- runtime/unix.js
+++ runtime/unix.js
@@ -12,26 +12,26 @@ function unix_time () {
 //Provides: unix_gmtime
 function unix_gmtime (t) {
   var d = new Date (t * 1000);
-  var januaryfirst = new Date(d.getUTCFullYear(), 0, 1);
+  var januaryfirst = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
   var doy = Math.floor((d - januaryfirst) / 86400000);
   return [0, d.getUTCSeconds(), d.getUTCMinutes(), d.getUTCHours(),
           d.getUTCDate(), d.getUTCMonth(), d.getUTCFullYear() - 1900,
           d.getUTCDay(), doy,
-          false /* for UTC daylight savings time is false */]
+          false | 0 /* for UTC daylight savings time is false */]
 }
 
 //Provides: unix_localtime
 function unix_localtime (t) {
   var d = new Date (t * 1000);
   var januaryfirst = new Date(d.getFullYear(), 0, 1);
-  var doy = Math.floor((d - januaryfirst) / 86400000); /* FIXME : daylight savings time ? */
+  var doy = Math.floor((d - januaryfirst) / 86400000);
   var jan = new Date(d.getFullYear(), 0, 1);
   var jul = new Date(d.getFullYear(), 6, 1);
   var stdTimezoneOffset = Math.max(jan.getTimezoneOffset(), jul.getTimezoneOffset());
   return [0, d.getSeconds(), d.getMinutes(), d.getHours(),
   d.getDate(), d.getMonth(), d.getFullYear() - 1900,
   d.getDay(), doy,
-  d.getTimezoneOffset() < stdTimezoneOffset /* daylight savings time  field. */]
+  (d.getTimezoneOffset() < stdTimezoneOffset) | 0 /* daylight savings time  field. */]
 }
 
 //Provides: unix_mktime
