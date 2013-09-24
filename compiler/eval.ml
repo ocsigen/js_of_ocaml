fb1a023622e7cb437c7b6724de41217cb28dd5be
Hugo Heuzard <hugo.heuzard@gmail.com>
2013-09-23 17:28:21 -0700

    COMPILER: remove some try-catch

--- compiler/eval.ml
+++ compiler/eval.ml
@@ -15,75 +15,74 @@ let eval_prim x =
     | Extern name, l ->
       let name = Primitive.resolve name in
       let module Int = Int32 in
-      let int2 = match l with
-        | [Int i; Int j] -> fun f ->
-          (try Some (Int (Int.to_int (f (Int.of_int i) (Int.of_int j)))) with _ -> None)
+      let int_binop = match l with
+        | [Int i; Int j] -> fun f -> Some (Int (Int.to_int (f (Int.of_int i) (Int.of_int j))))
         | _ -> fun _ -> None in
-      let int2_1 = match l with
-        | [Int i; Int j] -> fun f ->
-          (try Some (Int (Int.to_int (f (Int.of_int i) j))) with _ -> None)
+      let shift = match l with
+        | [Int i; Int j] -> fun f -> Some (Int (Int.to_int (f (Int.of_int i) j)))
         | _ -> fun _ -> None in
-      let f2_aux =
-        try
-          let i,j = match l with
-            | [Float i; Float j]-> i,j
-            | [Int i ; Int j] -> float_of_int i,float_of_int j
-            | [Int i ; Float j] -> float_of_int i,j
-            | [Float i ; Int j] -> i,float_of_int j
-            | _ -> raise Not_found
-          in
-          fun f -> (try Some (f i j) with _ -> None)
-        with _ -> fun _ -> None in
-      let f2 f = f2_aux (fun i j -> Float (f i j)) in
-      let f1 = match l with
-        | [Float i] -> fun f -> (try Some (Float (f i)) with _ -> None)
-        | [Int i] -> fun f -> (try Some (Float (f (float_of_int i))) with _ -> None)
+      let float_binop_aux =
+        let args = match l with
+          | [Float i; Float j]-> Some (i,j)
+          | [Int i ; Int j] -> Some (float_of_int i,float_of_int j)
+          | [Int i ; Float j] -> Some(float_of_int i,j)
+          | [Float i ; Int j] -> Some(i,float_of_int j)
+          | _ -> None
+        in
+        match args with
+          | None -> (fun _ -> None)
+          | Some (i,j) -> fun f -> Some (f i j) in
+      let float_binop f = float_binop_aux (fun i j -> Float (f i j)) in
+      let float_unop = match l with
+        | [Float i] -> fun f -> Some (Float (f i))
+        | [Int i] -> fun f -> Some (Float (f (float_of_int i)))
         | _ -> fun _ -> None in
-      let f2b f = f2_aux (fun i j -> Int (if f i j then 1 else 0)) in
+      let float_binop_bool f = float_binop_aux (fun i j -> Int (if f i j then 1 else 0)) in
       (match name, l with
         (* int *)
-        | "%int_add", _ -> int2 (Int.add)
-        | "%int_sub", _ -> int2 (Int.sub)
-        | "%direct_int_mul", _ -> int2 (Int.mul )
-        | "%direct_int_div", _ -> int2 (Int.div)
-        | "%direct_int_mod", _ -> int2 (Int.rem)
-        | "%int_and", _ -> int2 (Int.logand)
-        | "%int_or", _  -> int2 (Int.logor)
-        | "%int_xor", _ -> int2 (Int.logxor)
-        | "%int_lsl", _ -> int2_1 (Int.shift_left)
-        | "%int_lsr", _ -> int2_1 (Int.shift_right_logical)
-        | "%int_asr", _ -> int2_1 (Int.shift_right)
+        | "%int_add", _ -> int_binop (Int.add)
+        | "%int_sub", _ -> int_binop (Int.sub)
+        | "%direct_int_mul", _ -> int_binop (Int.mul )
+        | "%direct_int_div", [_; Int 0] -> None
+        | "%direct_int_div", _ -> int_binop (Int.div)
+        | "%direct_int_mod", _ -> int_binop (Int.rem)
+        | "%int_and", _ -> int_binop (Int.logand)
+        | "%int_or", _  -> int_binop (Int.logor)
+        | "%int_xor", _ -> int_binop (Int.logxor)
+        | "%int_lsl", _ -> shift (Int.shift_left)
+        | "%int_lsr", _ -> shift (Int.shift_right_logical)
+        | "%int_asr", _ -> shift (Int.shift_right)
         | "%int_neg", [Int i] -> Some (Int (Int.to_int (Int.neg (Int.of_int i) )))
         (* float *)
-        | "caml_eq_float", _ -> f2b (=)
-        | "caml_neq_float", _ -> f2b (<>)
-        | "caml_ge_float", _ -> f2b (>=)
-        | "caml_le_float", _ -> f2b (<=)
-        | "caml_gt_float", _ -> f2b (>)
-        | "caml_lt_float", _ -> f2b (<)
-        | "caml_add_float",_ -> f2 (+.)
-        | "caml_sub_float",_ -> f2 (-.)
-        | "caml_mul_float",_ -> f2 ( *. )
-        | "caml_div_float",_ -> f2 ( /. )
-        | "caml_fmod_float",_ -> f2 mod_float
+        | "caml_eq_float", _ -> float_binop_bool (=)
+        | "caml_neq_float", _ -> float_binop_bool (<>)
+        | "caml_ge_float", _ -> float_binop_bool (>=)
+        | "caml_le_float", _ -> float_binop_bool (<=)
+        | "caml_gt_float", _ -> float_binop_bool (>)
+        | "caml_lt_float", _ -> float_binop_bool (<)
+        | "caml_add_float",_ -> float_binop (+.)
+        | "caml_sub_float",_ -> float_binop (-.)
+        | "caml_mul_float",_ -> float_binop ( *. )
+        | "caml_div_float",_ -> float_binop ( /. )
+        | "caml_fmod_float",_ -> float_binop mod_float
         | "caml_int_of_float",[Float f] -> Some (Int (int_of_float f))
         | "to_int",[Float f]  -> Some (Int (int_of_float f))
         | "to_int",[Int i] -> Some (Int i)
           (* Math *)
-        | "caml_abs_float",_ -> f1 abs_float
-        | "caml_acos_float",_ -> f1 acos
-        | "caml_asin_float",_ -> f1 asin
-        | "caml_atan_float",_ -> f1 atan
-        | "caml_atan2_float",_ -> f2 atan2
-        | "caml_ceil_float",_ -> f1 ceil
-        | "caml_cos_float",_ -> f1 cos
-        | "caml_exp_float",_ -> f1 exp
-        | "caml_floor_float",_ -> f1 floor
-        | "caml_log_float",_ -> f1 log
-        | "caml_power_float",_ -> f2 ( ** )
-        | "caml_sin_float",_ -> f1 sin
-        | "caml_sqrt_float",_ -> f1 sqrt
-        | "caml_tan_float",_ -> f1 tan
+        | "caml_abs_float",_ -> float_unop abs_float
+        | "caml_acos_float",_ -> float_unop acos
+        | "caml_asin_float",_ -> float_unop asin
+        | "caml_atan_float",_ -> float_unop atan
+        | "caml_atan2_float",_ -> float_binop atan2
+        | "caml_ceil_float",_ -> float_unop ceil
+        | "caml_cos_float",_ -> float_unop cos
+        | "caml_exp_float",_ -> float_unop exp
+        | "caml_floor_float",_ -> float_unop floor
+        | "caml_log_float",_ -> float_unop log
+        | "caml_power_float",_ -> float_binop ( ** )
+        | "caml_sin_float",_ -> float_unop sin
+        | "caml_sqrt_float",_ -> float_unop sqrt
+        | "caml_tan_float",_ -> float_unop tan
         | _ -> None)
     | _ -> None
 
