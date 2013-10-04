ba38414b2743b216eb9eb0e0162b73bbbb13b232
Hugo Heuzard <hugo.heuzard@gmail.com>
2013-10-04 15:20:59 -0700

    Tests: detect phantomjs failure

--- tests/file/unix.js
+++ tests/file/unix.js
@@ -19,9 +19,11 @@ function  unix_inet_addr_of_string() {
 }
 
 //Provides: caml_sys_exit
-function   caml_sys_exit(n) {
+function caml_sys_exit(n) {
     if(this.phantom && this.phantom.exit)
         phantom.exit(n);
     else
         return 0;
 }
+
+window.onerror = function() { caml_sys_exit(1);}
