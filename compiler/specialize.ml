cbbc5aaac262f9e0473d8a2d17345c743e9221eb
Hugo Heuzard <hugo.heuzard@gmail.com>
2013-11-08 13:00:39 -0800

    COMPILER: Improve curryfication

--- compiler/specialize.ml
+++ compiler/specialize.ml
@@ -24,27 +24,53 @@ open Flow
 let function_cardinality info x =
   get_approx info
     (fun x ->
-       match info.info_defs.(Var.idx x) with
-         Expr (Closure (l, _)) -> Some (List.length l)
-       | _                     -> None)
+      match info.info_defs.(Var.idx x) with
+        | Expr (Closure (l, _)) -> Some (List.length l)
+        | _                     -> None)
     None
     (fun u v -> match u, v with Some n, Some m when n = m -> u | _ -> None)
     x
 
-let specialize_instr info i =
+let specialize_instr info (acc,free_pc,extra) i =
   match i with
-    | Let (x, Apply (f, l, _)) when Option.Optim.optcall () ->
-      Let (x, Apply (f, l, function_cardinality info f))
+    | Let (x, Apply (f, l, _)) when Option.Optim.optcall () -> begin
+      let n' = List.length l in
+      match function_cardinality info f with
+        | None -> i::acc,free_pc,extra
+        | Some n when n = n' -> Let (x, Apply (f, l, Some n))::acc,free_pc,extra
+        | Some n when n < n' ->
+          let v = Code.Var.fresh () in
+          let args,rest = Util.take n l in
+          (Let(v, Apply(f,args,Some n)))
+          ::(Let(x,Apply(v,rest,None)))
+          ::acc,free_pc,extra
+        | Some n when n > n' ->
+          let missing = Array.init (n - n') (fun _ -> Code.Var.fresh ()) in
+          let missing = Array.to_list missing in
+          let block =
+            let params' = Array.init (n - n') (fun _ -> Code.Var.fresh ()) in
+            let params' = Array.to_list params' in
+            let return' = Code.Var.fresh () in
+            { params=params';
+              body = [Let(return',Apply(f,l@params',Some n))];
+              branch = Return return';
+              handler = None;
+            } in
+          Let(x, Closure(missing,(free_pc,missing)))::acc,(free_pc + 1),(free_pc,block)::extra
+        | _ -> i::acc, free_pc,extra
+    end
     | _ ->
-      i
+      i::acc,free_pc,extra
 
 let specialize_instrs info (pc, blocks, free_pc) =
-  let blocks =
-    AddrMap.map
-      (fun block ->
-         { block with Code.body =
-             List.map (fun i -> specialize_instr info i) block.body })
-      blocks
+  let blocks,free_pc =
+    AddrMap.fold
+      (fun pc block (blocks,free_pc) ->
+        let body,free_pc,extra =
+          List.fold_right (fun i acc -> specialize_instr info acc i) block.body ([],free_pc,[]) in
+        let blocks = List.fold_left (fun blocks (pc,b) -> AddrMap.add pc b blocks) blocks extra in
+        (AddrMap.add pc { block with Code.body = body } blocks),free_pc)
+      blocks (AddrMap.empty,free_pc)
   in
   (pc, blocks, free_pc)
 
