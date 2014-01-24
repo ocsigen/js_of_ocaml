a07a31e436765c56172789ddb296e4fb9b57338e
Hugo Heuzard <hugo.heuzard@gmail.com>
2014-01-23 20:17:45 -0800

    fix script

--- tools/emacs-format-ocaml.el
+++ tools/emacs-format-ocaml.el
@@ -2,24 +2,23 @@
 ;;; Stan Warford
 ;;; 17 May 2006
 
-(   defun emacs-format-function ()
-   "Format the whole buffer."
-   (tuareg-mode)
-   (indent-region (point-min) (point-max) nil)
-   (delete-trailing-whitespace)
-   (save-buffer)
-)
+(defun emacs-format-function ()
+  "Format the whole buffer."
+  (tuareg-mode)
+  (indent-region (point-min) (point-max) nil)
+  (delete-trailing-whitespace)
+  (save-buffer)
+  )
 
-(   defun emacs-format-function-ident ()
-   "Format the whole buffer."
-   (tuareg-mode)
-   (indent-region (point-min) (point-max) nil)
-   (save-buffer)
-)
+(defun emacs-format-function-ident ()
+  "Format the whole buffer."
+  (tuareg-mode)
+  (indent-region (point-min) (point-max) nil)
+  (save-buffer)
+  )
 
-(   defun emacs-format-function-clean ()
-   "Format the whole buffer."
-   (tuareg-mode)
-   (delete-trailing-whitespace)
-   (save-buffer)
-)
+(defun emacs-format-function-clean ()
+  "Format the whole buffer."
+  (delete-trailing-whitespace)
+  (save-buffer)
+  )
