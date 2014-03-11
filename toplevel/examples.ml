b5d70688ac3ca14c50a56c2536df4686c865019f
Hugo Heuzard <hugo.heuzard@gmail.com>
2014-03-11 12:46:34 +0100

    Toplevel more sample

--- toplevel/examples.ml
+++ toplevel/examples.ml
@@ -7,13 +7,23 @@ let rec fact n = if n = 0 then 1. else float n *. fact (n - 1)
 let _ = Printf.printf "fact 20 = %f\n" (fact 20)
 let _ = "abc" < "def"
 
+(** Mutually recursive function *)
+let rec even n =
+  match n with
+  | 0 -> true
+  | x -> odd (x-1)
+and odd n =
+  match n with
+  | 0 -> false
+  | x -> even (x-1)
+
 (** Recusive module *)
 module rec Odd : sig
   val odd : int -> bool
 end = struct
-  let odd x = if x = 0 then true else Even.even (pred x)
+  let odd x = if x = 0 then false else Even.even (pred x)
 end and Even : sig
   val even : int -> bool
 end = struct
-  let even x = if x = 0 then false else Odd.odd (pred x)
+  let even x = if x = 0 then true else Odd.odd (pred x)
 end
