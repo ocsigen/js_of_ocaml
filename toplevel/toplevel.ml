28c514311d086c34ab47da9ca1b015651640350d
Hugo Heuzard <hugo.heuzard@gmail.com>
2014-03-16 19:31:36 +0100

    Toplevel: restore selection after indent

--- toplevel/toplevel.ml
+++ toplevel/toplevel.ml
@@ -205,6 +205,42 @@ let indent_caml s =
   IndentPrinter.proceed output stream IndentBlock.empty ""
 
 
+let indent_textarea textbox =
+  let rec loop s acc (i,pos') =
+    try
+      let pos = String.index_from s pos' '\n' in
+      loop s ((i,(pos',pos))::acc) (succ i,succ pos)
+    with _ -> List.rev ((i,(pos',String.length s)) :: acc) in
+  let rec find (l : (int * (int * int)) list ) c =
+    match l with
+    | [] -> assert false
+    | (i,(lo,up))::_ when up >= c -> c,i,lo,up
+    | (_,(lo,up))::rem -> find rem c in
+  let v = textbox##value in
+  let pos =
+    let c1 = (Obj.magic textbox)##selectionStart
+    and c2 = (Obj.magic textbox)##selectionEnd in
+    if Js.Opt.test (Obj.magic c1) && Js.Opt.test (Obj.magic c2)
+    then begin
+      let l = loop (Js.to_string v) [] (0,0) in
+      Some (find l c1,find l c2)
+    end
+    else None in
+  let v = indent_caml (Js.to_string v) in
+  textbox##value<-Js.string v;
+  begin match pos with
+    | Some ((c1,line1,lo1,up1),(c2,line2,lo2,up2)) ->
+      let l = loop v [] (0,0) in
+      let (lo1'',up1'') = List.assoc line1 l in
+      let (lo2'',up2'') = List.assoc line2 l in
+      let n1 = max (c1 + up1'' - up1) lo1'' in
+      let n2 = max (c2 + up2'' - up2) lo2'' in
+      (Obj.magic textbox)##setSelectionRange(n1,n2);
+      textbox##focus();
+      ()
+    | None -> () end
+
+
 let run _ =
   let container = by_id "toplevel-container" in
   let output = by_id "output" in
@@ -275,7 +311,7 @@ let run _ =
         | 13 ->
           Lwt.async resize;Js._true
         | 09 ->
-          textbox##value <- Js.string (indent_caml (Js.to_string textbox##value));
+          indent_textarea textbox;
           Js._false
         | 38 ->
           let str = Js.to_string textbox##value in
