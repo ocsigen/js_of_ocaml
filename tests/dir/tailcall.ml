e6530433a2784d8559f35d666164ef5e5cf3c1c0
Hugo Heuzard <hugo.heuzard@gmail.com>
2014-04-28 16:41:52 +0200

    Clean: remove warning

--- tests/dir/tailcall.ml
+++ tests/dir/tailcall.ml
@@ -28,7 +28,7 @@ let _ =
   let rec odd x = if x = 0 then false else even (x-1)
   and even x = if x = 0 then true else odd (x-1) in
   assert (odd 1 <> even 1);
-  (try odd 5000;log_success () with _ -> log_failure "too much recursion")
+  (try ignore(odd 5000);log_success () with _ -> log_failure "too much recursion")
 
 
 let () = log_stop ()
