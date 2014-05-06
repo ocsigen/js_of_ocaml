80365a65edb3e59f2ba8eda2cf6612b52146f68c
Hugo Heuzard <hugo.heuzard@gmail.com>
2014-05-06 13:06:39 +0200

    Build: move to ocamlbuild: tests

--- tests/file/unix.js
+++
@@ -1,9 +0,0 @@
-//Provides: caml_sys_exit
-function caml_sys_exit(n) {
-    if(joo_global_object.phantom && joo_global_object.phantom.exit)
-        phantom.exit(n);
-    else
-        return 0;
-}
-
-window.onerror = function() { caml_sys_exit(1);}
