6d0fdf34c3ecf76c47347fd60118380ac0f9023d
Hugo Heuzard <hugo.heuzard@gmail.com>
2014-06-05 16:22:29 +0200

    Revert "update jenkins.sh"
    This reverts commit f8baa41f4997eb93cd0f19627c88292507adcd3e.

--- .jenkins.sh
+++ .jenkins.sh
@@ -1,6 +1,4 @@
 opam pin js_of_ocaml .
-opam pin tyxml https://github.com/ocsigen/tyxml.git
-opam install tyxml react deriving
 opam install --deps-only js_of_ocaml
 opam install --verbose js_of_ocaml
 opam remove --verbose js_of_ocaml
