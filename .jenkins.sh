ede52405e108dd1de04d2102fbbcf20fa351fea1
Grégoire Henry <gregoire.henry@ocamlpro.com>
2014-07-13 19:23:57 +0200

    .jenkins.sh: install optionnal dependencies

--- .jenkins.sh
+++ .jenkins.sh
@@ -1,4 +1,5 @@
-opam pin add js_of_ocaml .
+opam pin add --no-action js_of_ocaml .
+opam install deriving tyxml reactiveData
 opam install --deps-only js_of_ocaml
 opam install --verbose js_of_ocaml
 opam remove --verbose js_of_ocaml
