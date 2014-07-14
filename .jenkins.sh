f7bd8b7879770c346ddc7ee7f2219364418abeb6
Grégoire Henry <gregoire.henry@ocamlpro.com>
2014-07-14 18:16:08 +0200

    Jenkins: build_doc.

--- .jenkins.sh
+++ .jenkins.sh
@@ -1,5 +1,17 @@
+
 opam pin add --no-action js_of_ocaml .
 opam install deriving tyxml reactiveData
 opam install --deps-only js_of_ocaml
 opam install --verbose js_of_ocaml
-opam remove --verbose js_of_ocaml
+
+do_build_doc () {
+  make -C doc clean
+  make -C doc doc
+  make -C doc wikidoc
+  cp -Rf doc/manual/* $(MANUAL_DIR)/
+  cp -Rf doc/api/wiki/*.wiki $(API_DIR)/
+}
+
+do_remove () {
+  opam remove --verbose js_of_ocaml
+}
