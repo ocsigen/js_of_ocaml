7764be22621470e4cc361cd9f91098e9560db709
Hugo Heuzard <hugo.heuzard@gmail.com>
2014-08-11 10:52:00 +0200

    fix doc

--- lib/toplevel/jsooTop.mli
+++ lib/toplevel/jsooTop.mli
@@ -31,7 +31,7 @@ val execute : bool -> ?pp_code:formatter -> formatter -> string -> unit
 (** Initialize Js_of_ocaml toplevel. *)
 val initialize : unit -> unit
 
-(*/*)
+(**/**)
 val get_camlp4_syntaxes : unit -> string list
 val register_camlp4_syntax : string ->
   (((string * (unit -> unit)) -> unit) -> unit)-> unit
