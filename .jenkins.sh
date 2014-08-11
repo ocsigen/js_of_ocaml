b29448f5251f1b1b19d388eae095352088e06fcd
Hugo Heuzard <hugo.heuzard@gmail.com>
2014-08-11 10:52:14 +0200

    update jenkins

--- .jenkins.sh
+++ .jenkins.sh
@@ -1,6 +1,5 @@
 
 opam pin add --no-action js_of_ocaml .
-opam pin add --no-action tyxml 'https://github.com/ocsigen/tyxml.git#master'
 opam install deriving tyxml reactiveData
 opam install --deps-only js_of_ocaml
 opam install --verbose js_of_ocaml
