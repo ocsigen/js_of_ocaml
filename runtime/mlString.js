e2eebdbc27b4d0ea3b2e0bda42793c3bea56d0ca
Jerome Vouillon <jerome.vouillon@pps.univ-paris-diderot.fr>
2014-09-23 22:33:23 +0200

    Runtime: performance improvement in caml_blit_string

--- runtime/mlString.js
+++ runtime/mlString.js
@@ -375,12 +375,14 @@ function caml_blit_string(s1, i1, s2, i2, len) {
   if (len == 0) return 0;
   if ((i2 == 0) &&
       (len >= s2.l || (s2.t == 2 /* PARTIAL */ && len >= s2.c.length))) {
-    s2.c = (s1.t == 4 /* ARRAY */)?caml_subarray_to_string(s1.c, i1, len):
-                                   s1.c.substr(i1, len);
+    s2.c = (s1.t == 4 /* ARRAY */)?
+             caml_subarray_to_string(s1.c, i1, len):
+             (i1 == 0 && s1.c.length == len)?s1.c:s1.c.substr(i1, len);
     s2.t = (s2.c.length == s2.l)?0 /* BYTES | UNKOWN */ :2; /* PARTIAL */
   } else if (s2.t == 2 /* PARTIAL */ && i2 == s2.c.length) {
-    s2.c += (s1.t == 4 /* ARRAY */)?caml_subarray_to_string(s1.c, i1, len):
-                                    s1.c.substr(i1, len);
+    s2.c += (s1.t == 4 /* ARRAY */)?
+             caml_subarray_to_string(s1.c, i1, len):
+             (i1 == 0 && s1.c.length == len)?s1.c:s1.c.substr(i1, len);
     s2.t = (s2.c.length == s2.l)?0 /* BYTES | UNKOWN */ :2; /* PARTIAL */
   } else {
     if (s2.t != 4 /* ARRAY */) caml_convert_string_to_array(s2);
