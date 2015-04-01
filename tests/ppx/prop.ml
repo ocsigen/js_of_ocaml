8ed7baa52dc721f6362a9e15cb7e8ddc7475f48a
Drup <drupyog@zoho.com>
2015-04-01 03:37:03 +0200

    Add tests for ppx's locations.
    Pipe a file segmented with ";;" in the toplevel and diff the output with
    what's expected. Seems to work, surprisingly.

---
+++ tests/ppx/prop.ml
@@ -0,0 +1,13 @@
+(* Test prop reading. *)
+
+fun (obj : int) -> obj##.p
+;;
+
+fun (obj : < > Js.t) -> obj##.p
+;;
+
+fun (obj : < p : float Js.writeonly_prop > Js.t) -> obj##.p + 1
+;;
+
+fun (obj : < p : float Js.prop > Js.t) -> obj##.p + 1
+;;
