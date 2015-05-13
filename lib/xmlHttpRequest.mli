2ae8c82d73413646aa93dce354f4f15a43bef0ea
pveber <philippe.veber@gmail.com>
2015-05-13 17:31:28 +0200

    XmlHttpRequest: API change for perform functions.
    The proposed change is to offer more possibilities for the body
    contents of a request. While only form-like bodies were available,
    this commit introduces two more possibilities: an arbitrary string and
    a blob object (for file upload notably).
    
    For this the signature of perform functions was changed: arguments
    `post_args` and `form_args` were replaced by a `contents` argument,
    which makes the behaviour of the function a bit more readable IMHO.
    
    In addition to breaking the API, there's one regression, namely that
    it is not possible to combine POST arguments with a FormData object,
    though I'm not sure if that's critical.

--- lib/xmlHttpRequest.mli
+++ lib/xmlHttpRequest.mli
@@ -104,12 +104,15 @@ exception Wrong_headers of (int * (string -> string option))
 val perform_raw :
     ?headers:(string * string) list
   -> ?content_type:string
-  -> ?post_args:((string * Form.form_elt) list)
   -> ?get_args:((string * string) list)  (* [] *)
-  -> ?form_arg:Form.form_contents
   -> ?check_headers:(int -> (string -> string option) -> bool)
   -> ?progress:(int -> int -> unit)
   -> ?upload_progress:(int -> int -> unit)
+  -> ?contents:
+    [< `POST_form of (string * Form.form_elt) list
+    | `Form_contents of Form.form_contents
+    | `String of string
+    | `Blob of #File.blob Js.t ]
   -> ?override_mime_type:string
   -> ?override_method:[< `GET | `POST | `HEAD | `PUT | `DELETE | `OPTIONS | `PATCH ]
   -> response_type:('a response)
@@ -123,12 +126,15 @@ val perform_raw :
 val perform_raw_url :
     ?headers:(string * string) list
   -> ?content_type:string
-  -> ?post_args:((string * Form.form_elt) list)
   -> ?get_args:((string * string) list)  (* [] *)
-  -> ?form_arg:Form.form_contents
   -> ?check_headers:(int -> (string -> string option) -> bool)
   -> ?progress:(int -> int -> unit)
   -> ?upload_progress:(int -> int -> unit)
+  -> ?contents:
+    [< `POST_form of (string * Form.form_elt) list
+    | `Form_contents of Form.form_contents
+    | `String of string
+    | `Blob of #File.blob Js.t ]
   -> ?override_mime_type:string
   -> ?override_method:[< `GET | `POST | `HEAD | `PUT | `DELETE | `OPTIONS | `PATCH ]
   -> string
@@ -147,12 +153,15 @@ val perform_raw_url :
 val perform :
     ?headers:(string * string) list
   -> ?content_type:string
-  -> ?post_args:((string * Form.form_elt) list)
   -> ?get_args:((string * string) list)  (* [] *)
-  -> ?form_arg:Form.form_contents
   -> ?check_headers:(int -> (string -> string option) -> bool)
   -> ?progress:(int -> int -> unit)
   -> ?upload_progress:(int -> int -> unit)
+  -> ?contents:
+    [< `POST_form of (string * Form.form_elt) list
+    | `Form_contents of Form.form_contents
+    | `String of string
+    | `Blob of #File.blob Js.t ]
   -> ?override_mime_type:string
   -> ?override_method:[< `GET | `POST | `HEAD | `PUT | `DELETE | `OPTIONS | `PATCH ]
   -> Url.url
