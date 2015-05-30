65d6e13cd9c1e8185ed6145e5aced200bbc254ba
Hugo Heuzard <hugo.heuzard@gmail.com>
2015-05-30 16:52:08 +0100

    Runtime: clean predefined exceptions

--- runtime/predefined_exceptions.js
+++ runtime/predefined_exceptions.js
@@ -1,20 +1,21 @@
 //Requires: caml_global_data, caml_new_string
 var predefined_exceptions =
-    [ {i:0, n:"Out_of_memory"},
-      {i:1, n:"Sys_error"},
-      {i:2, n:"Failure"},
-      {i:3, n:"Invalid_argument"},
-      {i:4, n:"End_of_file"},
-      {i:5, n:"Division_by_zero"},
-      {i:6, n:"Not_found"},
-      {i:7, n:"Match_failure"},
-      {i:8, n:"Stack_overflow"},
-      {i:9, n:"Sys_blocked_io"},
-      {i:10,n:"Assert_failure"},
-      {i:11,n:"Undefined_recursive_module"}]
+    [ {index:0, name:"Out_of_memory"},
+      {index:1, name:"Sys_error"},
+      {index:2, name:"Failure"},
+      {index:3, name:"Invalid_argument"},
+      {index:4, name:"End_of_file"},
+      {index:5, name:"Division_by_zero"},
+      {index:6, name:"Not_found"},
+      {index:7, name:"Match_failure"},
+      {index:8, name:"Stack_overflow"},
+      {index:9, name:"Sys_blocked_io"},
+      {index:10,name:"Assert_failure"},
+      {index:11,name:"Undefined_recursive_module"}]
 
-for(var p = 0; p < predefined_exceptions.length; p++){
-  var e = predefined_exceptions[p];
-  caml_global_data[e.i + 1] = [248, caml_new_string(e.n), - e.i]
-  caml_global_data[e.n] = caml_global_data[e.i + 1]
+for(var i = 0; i < predefined_exceptions.length; i++){
+  var info = predefined_exceptions[i];
+  var exn = [248, caml_new_string(info.name), - info.index];
+  caml_global_data[info.index + 1] = exn;
+  caml_global_data[info.name] = exn;
 }
