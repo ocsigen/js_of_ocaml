6c3bbeba274a2bcc2a1e23200afc7a8f62b98775
hhugo <hugo.heuzard@gmail.com>
2015-06-10 11:00:29 +0100

    Update bigstring.js

--- runtime/bigstring.js
+++ runtime/bigstring.js
@@ -24,3 +24,15 @@ function bigstring_blit_stub(s1, i1, s2, i2, len){
   for (var i = 0; i < len; i++) caml_ba_set_1(s2,i2 + i,caml_ba_get_1(s1,i1 + i));
   return 0;
 }
+
+//Provides: bigstring_memcmp_stub
+//Requires: caml_ba_get_1
+function bigstring_memcmp_stub(v_s1, v_s1_pos, v_s2, v_s2_pos, v_len){
+  for (var i = 0; i < v_len; i++) {
+    var a = caml_ba_get_1(v_s1,v_s1_pos + i);
+    var b = caml_ba_get_1(v_s2,v_s2_pos + i);
+    if (a < b) return -1;
+    if (a > b) return 1;
+  }
+  return 0;
+}
