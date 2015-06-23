3ad0748b1e6ad05764f4eb7ddb944a536d801d51
Drup <drupyog@zoho.com>
2015-06-23 15:01:27 +0200

    Fix camlp4 tests.

--- tests/camlp4/prop.ml
+++ tests/camlp4/prop.ml
@@ -1,19 +1,9 @@
-(* dummy "let _ = 3" are to work arround ... camlp4 weirdness I guess *)
 (* Test prop reading. *)
 
-let x1 = fun (obj : int) -> obj##p;;
+fun (obj : int) -> obj##p;;
 
-let _ = 3;; let _ = 3;;
+fun (obj : < > Js.t) -> obj##p;;
 
-let x2 = fun (obj : < > Js.t) -> obj##p;;
+fun (obj : < p : float Js.writeonly_prop > Js.t) -> obj##p + 1;;
 
-let _ = 3;; let _ = 3;;
-
-let x3 = fun (obj : < p : float Js.writeonly_prop > Js.t) -> obj##p + 1;;
-
-let _ = 3;; let _ = 3;;
-
-let x4 = fun (obj : < p : float Js.prop > Js.t) -> obj##p + 1;;
-
-let _ = 3;; let _ = 3;;
-
+fun (obj : < p : float Js.prop > Js.t) -> obj##p + 1;;
--- tests/camlp4/write_prop.ml
+++ tests/camlp4/write_prop.ml
@@ -1,22 +1,11 @@
-(* dummy "let _ = 3" are to work arround ... camlp4 weirdness I guess *)
 (* Test prop writing. *)
 
-let x1 = fun (obj : int) -> obj##p <- 2;;
+fun (obj : int) -> obj##p <- 2;;
 
-let _ = 3;; let _ = 3;;
+fun (obj : < > Js.t) -> obj##p <- 2;;
 
-let x2 = fun (obj : < > Js.t) -> obj##p <- 2;;
+fun (obj : < p : float Js.readonly_prop > Js.t) -> obj##p <- 1;;
 
-let _ = 3;; let _ = 3;;
+fun (obj : < p : float Js.prop > Js.t) -> obj##p <- 1;;
 
-let x3 = fun (obj : < p : float Js.readonly_prop > Js.t) -> obj##p <- 1;;
-
-let _ = 3;; let _ = 3;;
-
-let x4 = fun (obj : < p : float Js.prop > Js.t) -> obj##p <- 1;;
-
-let _ = 3;; let _ = 3;;
-
-let x5 = fun (obj : < p : int Js.prop > Js.t) -> (obj##p <- 1) + 1;;
-
-let _ = 3;; let _ = 3;;
+fun (obj : < p : int Js.prop > Js.t) -> (obj##p <- 1) + 1;;
