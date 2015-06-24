5a904797f424c20b28893ccc335eeac47fd80152
Hugo Heuzard <hugo.heuzard@gmail.com>
2015-06-24 20:17:32 +0100

    Runtime: bigstring for cstruct

--- runtime/bigstring.js
+++ runtime/bigstring.js
@@ -11,12 +11,21 @@ function bigstring_blit_bigstring_string_stub(v_bstr, v_src_pos, v_str, v_dst_po
   for (var i = 0; i < v_len; i++) caml_string_set(v_str,v_dst_pos + i,caml_ba_get_1(v_bstr,v_src_pos + i));
   return 0;
 }
+
+//Provides: caml_blit_bigstring_to_string
+//Requires: bigstring_blit_bigstring_string_stub
+var caml_blit_bigstring_to_string = bigstring_blit_bigstring_string_stub
+
 //Provides: bigstring_blit_string_bigstring_stub
 //Requires: caml_string_get, caml_ba_set_1
 function bigstring_blit_string_bigstring_stub(v_str, v_src_pos, v_bstr, v_dst_pos, v_len){
   for (var i = 0; i < v_len; i++) caml_ba_set_1(v_bstr,v_dst_pos + i,caml_string_get(v_str,v_src_pos + i));
   return 0;
 }
+
+//Provides: caml_blit_string_to_bigstring
+//Requires: bigstring_blit_string_bigstring_stub
+var caml_blit_string_to_bigstring = bigstring_blit_string_bigstring_stub
 
 //Provides: bigstring_blit_stub
 //Requires: caml_ba_get_1, caml_ba_set_1
