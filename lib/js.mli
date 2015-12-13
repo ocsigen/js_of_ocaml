a1e0170d93c303883755fc99ef8277df21a50784
Hugo Heuzard <hugo.heuzard@gmail.com>
2015-12-13 17:03:31 +0000

    Lib: callbacks

--- lib/js.mli
+++ lib/js.mli
@@ -290,15 +290,15 @@ class type ['a] js_array = object
   method unshift_3 : 'a -> 'a -> 'a -> int meth
   method unshift_4 : 'a -> 'a -> 'a -> 'a -> int meth
 
-  method some : ('a -> int -> 'a js_array t -> bool t ) -> bool t meth
-  method every : ('a -> int -> 'a js_array t -> bool t ) -> bool t meth
-  method forEach : ('a -> int -> 'a js_array t -> unit ) -> unit meth
-  method map : ('a -> int -> 'a js_array t -> 'b) -> 'b js_array t meth
-  method filter : ('a -> int -> 'a js_array t -> bool t) -> 'a js_array t meth
-  method reduce_init : ('b -> 'a -> int -> 'a js_array t -> 'b) -> 'b -> 'b meth
-  method reduce : ('a -> 'a -> int -> 'a js_array t -> 'a) -> 'a meth
-  method reduceRight_init : ('b -> 'a -> int -> 'a js_array t -> 'b) -> 'b -> 'b meth
-  method reduceRight : ('a -> 'a -> int -> 'a js_array t -> 'a) -> 'a meth
+  method some : ('a -> int -> 'a js_array t -> bool t ) callback -> bool t meth
+  method every : ('a -> int -> 'a js_array t -> bool t ) callback -> bool t meth
+  method forEach : ('a -> int -> 'a js_array t -> unit ) callback -> unit meth
+  method map : ('a -> int -> 'a js_array t -> 'b) callback -> 'b js_array t meth
+  method filter : ('a -> int -> 'a js_array t -> bool t) callback -> 'a js_array t meth
+  method reduce_init : ('b -> 'a -> int -> 'a js_array t -> 'b) callback -> 'b -> 'b meth
+  method reduce : ('a -> 'a -> int -> 'a js_array t -> 'a) callback -> 'a meth
+  method reduceRight_init : ('b -> 'a -> int -> 'a js_array t -> 'b) callback -> 'b -> 'b meth
+  method reduceRight : ('a -> 'a -> int -> 'a js_array t -> 'a) callback -> 'a meth
 
   method length : int prop
 end
