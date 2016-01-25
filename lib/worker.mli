78c0d269e97e2ab9b2ae6da360eb11e2e3df0bb8
Grégoire Henry <gregoire.henry@ocamlpro.com>
2016-01-25 18:31:56 +0100

    Fix ocamldoc syntax.

--- lib/worker.mli
+++ lib/worker.mli
@@ -19,8 +19,10 @@
 
 (** Low-level bindgins to javascript Web Workers.
 
-    See [https://developer.mozilla.org/en-US/docs/Web/API/Worker]
-    and [https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Using_web_workers].
+    See {{:https://developer.mozilla.org/en-US/docs/Web/API/Worker}
+    the documented Javascript API} and some more general documentation
+    {{:https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Using_web_workers}
+    about the usage of WebWorker}.
 
  *)
 
