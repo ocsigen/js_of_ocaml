7d6b654be397a8aeeab7f369921d80c15e198f44
Vasilis Papavasileiou <git@vasilis.airpost.net>
2016-03-28 23:16:13 +0200

    .jenkins.sh: pin 4.03.0 Lwt branch

--- .jenkins.sh
+++ .jenkins.sh
@@ -3,6 +3,12 @@ opam pin add --no-action js_of_ocaml .
 opam install async_kernel.113.33.00 || true
 opam install deriving || true
 opam install tyxml reactiveData cppo base64
+
+case $(opam switch show) in
+    4.03*) opam pin add lwt https://github.com/ocsigen/lwt.git#4.03.0 ;;
+    *) ;;
+esac
+
 opam install --deps-only js_of_ocaml
 opam install --verbose js_of_ocaml
 
