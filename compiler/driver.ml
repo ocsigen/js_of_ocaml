ocsigen/js_of_ocaml

commit 652a6378e671f2251556829751dba6961327ee98

Compiler: cleanup dynlink workflow

compiler/driver.ml
@@ -154,26 +154,20 @@ let o3 =
   loop 10 "flow" round2 1 >>
   print
 
-let generate d ~exported_runtime ~toplevel (p,live_vars) =
+let generate d ~exported_runtime (p,live_vars) =
   if times ()
   then Format.eprintf "Start Generation...@.";
-  Generate.f p ~toplevel ~exported_runtime live_vars d
+  Generate.f p ~exported_runtime live_vars d
 
 
-let header formatter ~standalone ~custom_header js =
-  if standalone then begin
-    let version = match Compiler_version.git_version with
-      | "" -> Compiler_version.s
-      | v  -> Printf.sprintf "%s+git-%s"Compiler_version.s v in
-
-    (match custom_header with
-     | None -> ()
-     | Some c -> Pretty_print.string formatter c);
-    Pretty_print.string formatter ("\n// Generated by js_of_ocaml " ^ version);
-
-    Pretty_print.newline formatter;
-  end;
-  js
+let header formatter ~custom_header =
+  (match custom_header with
+   | None -> ()
+   | Some c -> Pretty_print.string formatter (c ^ "\n"));
+  let version = match Compiler_version.git_version with
+    | "" -> Compiler_version.s
+    | v  -> Printf.sprintf "%s+git-%s"Compiler_version.s v in
+  Pretty_print.string formatter ("// Generated by js_of_ocaml " ^ version ^ "\n")
 
 let debug_linker = Option.Debug.find "linker"
 
@@ -242,105 +236,95 @@ let gen_missing js missing =
     (Statement (Variable_statement miss), N) :: js
 
 
-let link ~standalone ?linkall ~export_runtime js =
-  if standalone
+let link ~standalone ~linkall ~export_runtime js =
+  if not standalone then js else
+  let t = Util.Timer.make () in
+  if times ()
+  then Format.eprintf "Start Linking...@.";
+  let traverse = new Js_traverse.free in
+  let js = traverse#program js in
+  let free = traverse#get_free_name in
+
+  let prim = Primitive.get_external () in
+  let prov = Linker.get_provided () in
+
+  let all_external = StringSet.union prim prov in
+
+  let used = StringSet.inter free all_external in
+
+  let linkinfos = Linker.init () in
+  let linkinfos,missing = Linker.resolve_deps ~linkall linkinfos used in
+
+  (* gen_missing may use caml_failwith *)
+  let linkinfos,missing =
+    if not (StringSet.is_empty missing) && Option.Optim.genprim ()
+    then
+      let linkinfos,missing2 = Linker.resolve_deps linkinfos (StringSet.singleton "caml_failwith") in
+      linkinfos, StringSet.union missing missing2
+    else linkinfos, missing in
+
+  let js = if Option.Optim.genprim ()
+    then gen_missing js missing
+    else js in
+  if times () then Format.eprintf "  linking: %a@." Util.Timer.print t;
+  let js =
+    if export_runtime
+    then
+      let open Javascript in
+      let all = Linker.all linkinfos in
+      let all = List.map (fun name -> PNI name,EVar (S {name ;var=None})) all  in
+      (Statement (Expression_statement(
+         EBin(Eq,
+              EDot(EVar (S {name=global_object;var=None}),"jsoo_runtime"),
+              EObj all))),N)
+      :: js
+    else js
+  in
+  Linker.link js linkinfos
+
+let check_js js =
+  let t = Util.Timer.make () in
+  if times ()
+  then Format.eprintf "Start Checks...@.";
+
+  let traverse = new Js_traverse.free in
+  let js = traverse#program js in
+  let free = traverse#get_free_name in
+
+  let prim = Primitive.get_external () in
+  let prov = Linker.get_provided () in
+
+  let all_external = StringSet.union prim prov in
+
+  let missing = StringSet.inter free all_external in
+
+  let other =  StringSet.diff free missing in
+
+  let res = VarPrinter.get_reserved() in
+  let other = StringSet.diff other res in
+  if not (StringSet.is_empty missing)
+  then begin
+    report_missing_primitives missing
+  end;
+
+  let probably_prov = StringSet.inter other Reserved.provided in
+  let other = StringSet.diff other probably_prov in
+
+  if not (StringSet.is_empty other) && debug_linker ()
   then
     begin
-      let t = Util.Timer.make () in
-      if times ()
-      then Format.eprintf "Start Linking...@.";
-      let traverse = new Js_traverse.free in
-      let js = traverse#program js in
-      let free = traverse#get_free_name in
+      Util.warn "Missing variables:@.";
+      StringSet.iter (fun nm -> Util.warn "  %s@." nm) other
+    end;
 
-      let prim = Primitive.get_external () in
-      let prov = Linker.get_provided () in
-
-      let all_external = StringSet.union prim prov in
-
-      let used = StringSet.inter free all_external in
-
-      let linkinfos = Linker.init () in
-      let linkinfos,missing = Linker.resolve_deps ?linkall linkinfos used in
-
-      (* gen_missing may use caml_failwith *)
-      let linkinfos,missing =
-        if not (StringSet.is_empty missing) && Option.Optim.genprim ()
-        then
-          let linkinfos,missing2 = Linker.resolve_deps linkinfos (StringSet.singleton "caml_failwith") in
-          linkinfos, StringSet.union missing missing2
-        else linkinfos, missing in
-
-      let js = if Option.Optim.genprim ()
-        then gen_missing js missing
-        else js in
-      if times () then Format.eprintf "  linking: %a@." Util.Timer.print t;
-      let js =
-        if export_runtime
-        then
-          let open Javascript in
-          let all = Linker.all linkinfos in
-          let all = List.map (fun name -> PNI name,EVar (S {name ;var=None})) all  in
-          (Statement (Expression_statement(
-             EBin(Eq,
-                  EDot(EVar (S {name=global_object;var=None}),"jsoo_runtime"),
-                  EObj all))),N)
-          :: js
-        else js
-      in
-      Linker.link js linkinfos
-    end
-  else js
-
-
-let check_js ~standalone js =
-  if standalone
+  if not (StringSet.is_empty probably_prov) && debug_linker ()
   then
     begin
-      let t = Util.Timer.make () in
-      if times ()
-      then Format.eprintf "Start Checks...@.";
-
-      let traverse = new Js_traverse.free in
-      let js = traverse#program js in
-      let free = traverse#get_free_name in
-
-      let prim = Primitive.get_external () in
-      let prov = Linker.get_provided () in
-
-      let all_external = StringSet.union prim prov in
-
-      let missing = StringSet.inter free all_external in
-
-      let other =  StringSet.diff free missing in
-
-      let res = VarPrinter.get_reserved() in
-      let other = StringSet.diff other res in
-      if not (StringSet.is_empty missing)
-      then begin
-        report_missing_primitives missing
-      end;
-
-      let probably_prov = StringSet.inter other Reserved.provided in
-      let other = StringSet.diff other probably_prov in
-
-      if not (StringSet.is_empty other) && debug_linker ()
-      then
-        begin
-          Util.warn "Missing variables:@.";
-          StringSet.iter (fun nm -> Util.warn "  %s@." nm) other
-        end;
-
-      if not (StringSet.is_empty probably_prov) && debug_linker ()
-      then
-        begin
-          Util.warn "Variables provided by the browser:@.";
-          StringSet.iter (fun nm -> Util.warn "  %s@." nm) probably_prov
-        end;
-      if times () then Format.eprintf "  checks: %a@." Util.Timer.print t;
-      js
-    end
-  else js
+      Util.warn "Variables provided by the browser:@.";
+      StringSet.iter (fun nm -> Util.warn "  %s@." nm) probably_prov
+    end;
+  if times () then Format.eprintf "  checks: %a@." Util.Timer.print t;
+  js
 
 let coloring js =
   let t = Util.Timer.make () in
@@ -354,14 +338,15 @@ let coloring js =
   if times () then Format.eprintf "  coloring: %a@." Util.Timer.print t;
   js
 
-let output formatter ?source_map js =
+let output formatter ~standalone ~custom_header ?source_map () js =
   let t = Util.Timer.make () in
   if times ()
   then Format.eprintf "Start Writing file...@.";
+  if standalone then header ~custom_header formatter;
   Js_output.program formatter ?source_map js;
   if times () then Format.eprintf "  write: %a@." Util.Timer.print t
 
-let pack ~wrap_with_fun ~toplevel js =
+let pack ~wrap_with_fun js =
   let module J = Javascript in
   let t = Util.Timer.make () in
   if times ()
@@ -422,12 +407,7 @@ let pack ~wrap_with_fun ~toplevel js =
     if (Option.Optim.shortvar ())
     then
       let t5 = Util.Timer.make () in
-      let keeps =
-        if toplevel
-        then StringSet.add global_object (Primitive.get_external ())
-        else StringSet.empty in
-      let keeps = StringSet.add "caml_get_global_data" keeps in
-      let js = (new Js_traverse.rename_variable keeps)#program js in
+      let js = (new Js_traverse.rename_variable StringSet.empty)#program js in
       if times () then Format.eprintf "    shortten vars: %a@." Util.Timer.print t5;
       js
     else js in
@@ -446,23 +426,24 @@ let configure formatter p =
 type profile = Code.program -> Code.program
 
 let f ?(standalone=true) ?(wrap_with_fun=false) ?(profile=o1)
-    ?(toplevel=false) ?linkall ?source_map ?custom_header formatter d =
+    ?(dynlink=false) ?(linkall=false) ?source_map ?custom_header formatter d =
   let exported_runtime = not standalone in
+  let linkall = linkall || dynlink in
   configure formatter >>
   profile >>
   Generate_closure.f >>
   deadcode' >>
-  generate d ~exported_runtime ~toplevel >>
+  generate d ~exported_runtime >>
 
-  link ~standalone ?linkall ~export_runtime:toplevel >>
+  link ~standalone ~linkall ~export_runtime:dynlink >>
 
-  pack ~wrap_with_fun ~toplevel >>
+  pack ~wrap_with_fun >>
 
   coloring >>
 
-  check_js ~standalone >>
-  header formatter ~standalone ~custom_header >>
-  output formatter ?source_map
+  check_js >>
+
+  output formatter ~standalone ~custom_header ?source_map ()
 
 let from_string prims s formatter =
   let (p,d) = Parse_bytecode.from_string prims s in

compiler/driver.mli
@@ -24,7 +24,7 @@ val f :
   ?standalone:bool ->
   ?wrap_with_fun:bool ->
   ?profile:profile ->
-  ?toplevel:bool ->
+  ?dynlink:bool ->
   ?linkall:bool ->
   ?source_map:(string option * Source_map.t) ->
   ?custom_header:string ->

compiler/jsoo_compile.ml
@@ -48,8 +48,9 @@ let f {
     CompileArg.common;
     profile; source_map; runtime_files; input_file; output_file;
     params ; static_env;
-    linkall; toplevel; nocmis;
+    dynlink; linkall; toplevel; nocmis;
     include_dir; fs_files; fs_output; fs_external } =
+  let dynlink = dynlink || toplevel in
   let custom_header = common.CommonArg.custom_header in
   CommonArg.eval common;
   List.iter (fun (s,v) -> Option.Param.set s v) params;
@@ -80,10 +81,10 @@ let f {
   let p, cmis, d, standalone =
     match input_file with
       None ->
-        Parse_bytecode.from_channel ~includes:paths ~toplevel ~debug:need_debug stdin
+        Parse_bytecode.from_channel ~includes:paths ~toplevel ~dynlink ~debug:need_debug stdin
     | Some f ->
         let ch = open_in_bin f in
-        let res = Parse_bytecode.from_channel ~includes:paths ~toplevel ~debug:need_debug ch in
+        let res = Parse_bytecode.from_channel ~includes:paths ~toplevel ~dynlink ~debug:need_debug ch in
         close_in ch;
         res
   in
@@ -109,7 +110,7 @@ let f {
     | None ->
       let p = PseudoFs.f p cmis fs_files paths in
       let fmt = Pretty_print.to_out_channel stdout in
-      Driver.f ~standalone ?profile ~toplevel ~linkall
+      Driver.f ~standalone ?profile ~linkall ~dynlink
         ?source_map ?custom_header fmt d p
     | Some file ->
       gen_file file (fun chan ->
@@ -118,7 +119,7 @@ let f {
             then PseudoFs.f p cmis fs_files paths
             else p in
           let fmt = Pretty_print.to_out_channel chan in
-          Driver.f ~standalone ?profile ~toplevel ~linkall
+          Driver.f ~standalone ?profile ~linkall ~dynlink
             ?source_map ?custom_header fmt d p;
         );
       Util.opt_iter (fun file ->

compiler/compileArg.ml
@@ -30,6 +30,7 @@ type t = {
   params : (string * string) list;
   static_env : (string * string) list;
  (* toplevel *)
+  dynlink : bool;
   linkall : bool;
   toplevel : bool;
   nocmis : bool;
@@ -103,7 +104,11 @@ let options =
   in
   let linkall =
     let doc = "Link all primitives." in
-    Arg.(value & flag & info ["linkall"] ~docs:toplevel_section ~doc)
+    Arg.(value & flag & info ["linkall"] ~doc)
+  in
+  let dynlink =
+    let doc = "Enable dynlink." in
+    Arg.(value & flag & info ["dynlink"] ~doc)
   in
   let nocmis =
     let doc = "Do not include cmis when compiling toplevel." in
@@ -129,6 +134,7 @@ let options =
       common
       set_param
       set_env
+      dynlink
       linkall
       toplevel
       include_dir
@@ -193,6 +199,7 @@ let options =
         profile;
         static_env;
 
+        dynlink;
         linkall;
         toplevel;
 
@@ -215,6 +222,7 @@ let options =
           $ CommonArg.t
           $ set_param
           $ set_env
+          $ dynlink
           $ linkall
           $ toplevel
 

compiler/compileArg.mli
@@ -28,6 +28,7 @@ type t = {
   params : (string * string) list;
   static_env : (string * string) list;
   (* toplevel *)
+  dynlink : bool;
   linkall : bool;
   toplevel : bool;
   nocmis : bool;

compiler/generate.ml
@@ -1,3 +1,4 @@
+
 (* Js_of_ocaml compiler
  * http://www.ocsigen.org/js_of_ocaml/
  * Copyright (C) 2010 Jérôme Vouillon
@@ -1802,9 +1803,11 @@ let compile_program ctx pc =
   if debug () then Format.eprintf "@.@.";
   res
 
-let f ((pc, blocks, _) as p) ~toplevel ~exported_runtime live_vars debug =
+let f ((pc, blocks, _) as p) ~exported_runtime live_vars debug =
   let t' = Util.Timer.make () in
-  let share = Share.get ~alias_prims:(toplevel && Option.Optim.shortvar ()) p in
+  let share =
+    Share.get ~alias_prims:exported_runtime p
+  in
   let exported_runtime =
     if exported_runtime
     then Some (Code.Var.fresh_n "runtime")

compiler/generate.mli
@@ -19,6 +19,6 @@
  *)
 
 val f :
-  Code.program -> toplevel:bool -> exported_runtime:bool
+  Code.program -> exported_runtime:bool
   -> int array -> Parse_bytecode.Debug.data ->
   Javascript.program

compiler/parse_bytecode.ml
@@ -1855,7 +1855,7 @@ let read_toc ic =
   done;
   !section_table
 
-let exe_from_channel ~includes ?(toplevel=false) ~debug ~debug_data ic =
+let exe_from_channel ~includes ?(toplevel=false) ?(dynlink=false) ~debug ~debug_data ic =
 
   let toc = read_toc ic in
 
@@ -1912,10 +1912,11 @@ let exe_from_channel ~includes ?(toplevel=false) ~debug ~debug_data ic =
     with Not_found -> ()
   ) override_global;
 
-  if toplevel then
+  if toplevel || dynlink then
     begin
       (* export globals *)
       Tbl.iter (fun id n ->
+        (* Format.eprintf "export %d %d %s@." id.Ident.flags id.Ident.stamp id.Ident.name; *)
         globals.named_value.(n) <- Some id.Ident.name;
         globals.is_exported.(n) <- true) symbols.num_tbl;
       (* @vouillon: *)
@@ -2149,7 +2150,7 @@ let from_compilation_units ~includes:_ ~debug ~debug_data l =
   let body = Let (gdata, Prim (Extern "caml_get_global_data", [])) :: body in
   prepend prog body,Util.StringSet.empty, debug_data
 
-let from_channel ?(includes=[]) ?(toplevel=false) ?(debug=`No) ic =
+let from_channel ?(includes=[]) ?(toplevel=false) ?(dynlink=false) ?(debug=`No) ic =
   let debug_data = Debug.create () in
   let format =
     try
@@ -2211,7 +2212,7 @@ let from_channel ?(includes=[]) ?(toplevel=false) ?(debug=`No) ic =
       | `Exe ->
         if Option.Optim.check_magic () && magic <> Util.MagicNumber.current_exe
         then raise Util.MagicNumber.(Bad_magic_version magic);
-        let a,b,c = exe_from_channel ~includes ~toplevel ~debug ~debug_data ic in
+        let a,b,c = exe_from_channel ~includes ~toplevel ~dynlink ~debug ~debug_data ic in
         Code.invariant a;
         a,b,c,true
       | _ ->

commit 1b0a2c772f2790f644e2387a7cee68d8a1414274

Compiler: fix regression introduced by 652a6378e671f2251556829751dba6961327ee98

compiler/driver.ml
@@ -346,7 +346,7 @@ let output formatter ~standalone ~custom_header ?source_map () js =
   Js_output.program formatter ?source_map js;
   if times () then Format.eprintf "  write: %a@." Util.Timer.print t
 
-let pack ~wrap_with_fun js =
+let pack ~toplevel ~wrap_with_fun js =
   let module J = Javascript in
   let t = Util.Timer.make () in
   if times ()
@@ -407,7 +407,12 @@ let pack ~wrap_with_fun js =
     if (Option.Optim.shortvar ())
     then
       let t5 = Util.Timer.make () in
-      let js = (new Js_traverse.rename_variable StringSet.empty)#program js in
+      let keep =
+        if toplevel
+        then StringSet.singleton global_object
+        else StringSet.empty
+      in
+      let js = (new Js_traverse.rename_variable keep)#program js in
       if times () then Format.eprintf "    shortten vars: %a@." Util.Timer.print t5;
       js
     else js in
@@ -426,7 +431,7 @@ let configure formatter p =
 type profile = Code.program -> Code.program
 
 let f ?(standalone=true) ?(wrap_with_fun=false) ?(profile=o1)
-    ?(dynlink=false) ?(linkall=false) ?source_map ?custom_header formatter d =
+    ?(dynlink=false) ?(toplevel=false) ?(linkall=false) ?source_map ?custom_header formatter d =
   let exported_runtime = not standalone in
   let linkall = linkall || dynlink in
   configure formatter >>
@@ -437,7 +442,7 @@ let f ?(standalone=true) ?(wrap_with_fun=false) ?(profile=o1)
 
   link ~standalone ~linkall ~export_runtime:dynlink >>
 
-  pack ~wrap_with_fun >>
+  pack ~toplevel ~wrap_with_fun >>
 
   coloring >>
 

compiler/driver.mli
@@ -25,6 +25,7 @@ val f :
   ?wrap_with_fun:bool ->
   ?profile:profile ->
   ?dynlink:bool ->
+  ?toplevel:bool ->
   ?linkall:bool ->
   ?source_map:(string option * Source_map.t) ->
   ?custom_header:string ->

compiler/jsoo_compile.ml
@@ -113,7 +113,7 @@ let f {
     | None ->
       let p = PseudoFs.f p cmis fs_files paths in
       let fmt = Pretty_print.to_out_channel stdout in
-      Driver.f ~standalone ?profile ~linkall ~dynlink
+      Driver.f ~standalone ?profile ~toplevel ~linkall ~dynlink
         ?source_map ?custom_header fmt d p
     | Some file ->
       gen_file file (fun chan ->
@@ -122,14 +122,14 @@ let f {
             then PseudoFs.f p cmis fs_files paths
             else p in
           let fmt = Pretty_print.to_out_channel chan in
-          Driver.f ~standalone ?profile ~linkall ~dynlink
+          Driver.f ~standalone ?profile ~toplevel ~linkall ~dynlink
             ?source_map ?custom_header fmt d p;
         );
       Util.opt_iter (fun file ->
           gen_file file (fun chan ->
               let pfs = PseudoFs.f_empty cmis fs_files paths in
               let pfs_fmt = Pretty_print.to_out_channel chan in
-              Driver.f ~standalone ?profile ?custom_header pfs_fmt d pfs
+              Driver.f ~standalone ?profile ~toplevel ?custom_header pfs_fmt d pfs
             )
         ) fs_output
   end;