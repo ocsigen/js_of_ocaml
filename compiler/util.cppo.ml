231bb68f4006c1fac51faac2f51632fb9f6d0657
Hugo Heuzard <hugo.heuzard@gmail.com>
2016-06-08 13:57:18 +0100

    Compiler: better error message for read_file

--- compiler/util.cppo.ml
+++ compiler/util.cppo.ml
@@ -80,12 +80,17 @@ let absolute_path f =
   else f
 
 let read_file f =
-  let ic = open_in_bin f in
-  let n = in_channel_length ic in
-  let s = Bytes.create n in
-  really_input ic s 0 n;
-  close_in ic;
-  Bytes.unsafe_to_string s
+  try
+    let ic = open_in_bin f in
+    let n = in_channel_length ic in
+    let s = Bytes.create n in
+    really_input ic s 0 n;
+    close_in ic;
+    Bytes.unsafe_to_string s
+  with e ->
+    failwith
+      (Printf.sprintf "Cannot read content of %s.\n%s"
+         f (Printexc.to_string e))
 
 let filter_map f l =
   let l = List.fold_left (fun acc x -> match f x with
