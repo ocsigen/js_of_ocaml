0d56d33759cf079350b2c85600c9fea6ca87fb84
Julien Sagot <ju.sagot@gmail.com>
2016-06-29 11:23:42 +0200

    Added selection{Start,Stop} method to Dom_html.textAreaElement.

--- lib/dom_html.mli
+++ lib/dom_html.mli
@@ -533,7 +533,8 @@ class type textAreaElement = object ('self)
   method select : unit meth
   method required : bool t writeonly_prop (* Not supported by IE 9/Safari *)
   method placeholder : js_string t writeonly_prop (* Not supported by IE 9 *)
-
+  method selectionStart : int prop
+  method selectionEnd : int prop
   method onselect : ('self t, event t) event_listener prop
   method onchange : ('self t, event t) event_listener prop
   method oninput : ('self t, event t) event_listener prop
