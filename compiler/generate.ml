9a8c2970cf7ce9bc78528114a29f5f697c15fda9
Hugo Heuzard <hugo.heuzard@gmail.com>
2016-10-24 11:27:52 -0400

    Compiler: fix code generation when staticeval is disabled

--- compiler/generate.ml
+++ compiler/generate.ml
@@ -1111,7 +1111,13 @@ let rec translate_expr ctx queue loc _x e level : _ * J.statement_list =
         in
         let (prop, fields, queue) = build_fields queue fields in
         (J.EObj fields, prop, queue)
-      | Extern "caml_alloc_dummy_function", [_ ; Pc (Int i)] ->
+      | Extern "caml_alloc_dummy_function", [_ ; size] ->
+        let i,queue =
+          let ((_px, cx), queue) = access_queue' ~ctx queue size in
+          match cx with
+          | ENum i -> Int32.of_float i, queue
+          | _ -> assert false
+        in
         let args = Array.to_list (
           Array.init (Int32.to_int i)
             (fun _ -> J.V (Var.fresh ()))) in
