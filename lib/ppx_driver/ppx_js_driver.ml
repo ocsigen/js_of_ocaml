ce06a15083096622e8d196833e0af80329c2d405
Hugo Heuzard <hugo.heuzard@gmail.com>
2017-03-24 17:22:31 +0000

    Misc: remove support for JaneStreet package

--- lib/ppx_driver/ppx_js_driver.ml
+++
@@ -1,8 +0,0 @@
-let () =
-  Ppx_driver.add_arg "-no-js-of-ocaml-wrapper"
-    (Arg.Unit (fun () -> Ppx_js.wrapper := None))
-    ~doc:" Undocumented";
-  let js_mapper = Ppx_js.js_mapper [] in
-  Ppx_driver.register_transformation_using_ocaml_current_ast "js_of_ocaml"
-    ~impl:(js_mapper.Ast_mapper.structure js_mapper)
-    ~intf:(js_mapper.Ast_mapper.signature js_mapper)
