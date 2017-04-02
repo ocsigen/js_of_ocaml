6305a9fa76b1754be933852bff8e49e5054ef437
Hugo Heuzard <hugo.heuzard@gmail.com>
2017-04-02 20:46:48 +0100

    Tests: restore tests for lib

---
+++ lib/tests/main.ml
@@ -0,0 +1,11 @@
+
+module Closure1 = Closure1
+module Css_angle = Css_angle
+module Css_color = Css_color
+module Css_length = Css_length
+module Json_convert = Json_convert
+module Regexp1 = Regexp1
+module Side_effect = Side_effect
+module Tailcall = Tailcall
+module Time_PDT_minus8 = Time_PDT_minus8
+module Url1 = Url1
