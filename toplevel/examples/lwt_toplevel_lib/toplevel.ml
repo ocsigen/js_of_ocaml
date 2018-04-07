053aecd7aa1a28686b176db6721b2e01b6e61c06
Hugo Heuzard <hugo.heuzard@gmail.com>
2018-04-07 14:23:38 +0100

    Toplevel: fix toplevel

--- toplevel/examples/lwt_toplevel_lib/toplevel.ml
+++ toplevel/examples/lwt_toplevel_lib/toplevel.ml
@@ -226,6 +226,7 @@ module History = struct
   let idx = ref 0
   let get_storage () =
     match Js.Optdef.to_option Dom_html.window##.localStorage with
+    | exception _ -> raise Not_found
     | None -> raise Not_found
     | Some t -> t
 
