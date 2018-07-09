40d6863b22a58d059e77f589e22df81684ed16d7
Hugo Heuzard <hugo.heuzard@gmail.com>
2018-07-09 19:45:38 +0100

    Misc: improve stubs gen

--- lib/generate_stubs.sh
+++ lib/generate_stubs.sh
@@ -3,6 +3,10 @@ shopt -s extglob
 
 echo "#include <stdlib.h>";
 echo "#include <stdio.h>";
-echo "#include \"caml/backtrace.h\"";
-echo "#define D(f) void f () { caml_print_exception_backtrace(); fprintf(stderr, \"Unimplemented Javascript primitive %s!\\n\", #f); exit(1); }";
-sed -n -e 's/.*external.*"\([^"%]*\)".*/D(\1)/p'  !(*.pp).ml | sort | uniq;
+sed -n -e 's/.*external.*"\([^"%]*\)".*/\1/p' !(*.pp).ml | sort | uniq | \
+    while read prim; do
+        echo "void ${prim} () {
+               fprintf(stderr, \"Unimplemented Javascript primitive ${prim}!\\n\");
+               exit(1);
+              }";
+    done;
