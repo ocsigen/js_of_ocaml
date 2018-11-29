06c1d855f61e3f1000cc243f3f0934ad5ff6a62e
hhugo <hugo.heuzard@gmail.com>
2018-11-29 00:35:56 +0000

    fix travis (#720)

--- tools/travis.sh
+++ tools/travis.sh
@@ -1,101 +1,16 @@
 set -x
 
-
-
 # Install system packages.
-packages_apt () {
-    case $COMPILER in
-        4.02) PPA=avsm/ocaml42+opam12;;
-        4.03) PPA=avsm/ocaml42+opam12; DO_SWITCH=yes;;
-        4.04) PPA=avsm/ocaml42+opam12; DO_SWITCH=yes;;
-        4.05) PPA=avsm/ocaml42+opam12; DO_SWITCH=yes;;
-        4.06) PPA=avsm/ocaml42+opam12; DO_SWITCH=yes;;
-	4.07) PPA=avsm/ocaml42+opam12; DO_SWITCH=yes;;
-           *) echo Unsupported compiler $COMPILER; exit 1;;
-    esac
+wget https://raw.githubusercontent.com/ocaml/ocaml-travisci-skeleton/master/.travis-ocaml.sh
+bash -ex .travis-ocaml.sh
 
-    sudo add-apt-repository -y ppa:$PPA
-    sudo apt-get update -qq
-
-    if [ -z "$DO_SWITCH" ]
-    then
-        sudo apt-get install -qq ocaml-nox
-    fi
-
-    sudo apt-get install -qq opam
-
-}
-
-packages_homebrew () {
-    brew update > /dev/null
-
-    if [ "$COMPILER" = system ]
-    then
-        brew install ocaml
-        # The system compiler on Homebrew is now 4.06 or higher, and there is no
-        # system Camlp4 package compatible with that (at least not yet). See:
-        #   https://github.com/ocaml/opam-repository/pull/10455
-        HAVE_CAMLP4=no
-    else
-        DO_SWITCH=yes
-    fi
-
-    brew install opam
-
-}
-
-packages_osx () {
-    case $PACKAGE_MANAGER in
-               *) packages_homebrew;;
-    esac
-}
-
-packages () {
-    case $TRAVIS_OS_NAME in
-        linux) packages_apt;;
-          osx) packages_osx;;
-            *) echo Unsupported system $TRAVIS_OS_NAME; exit 1;;
-    esac
-}
-
-packages
-
-
-
-# Initialize OPAM and switch to the right compiler, if necessary.
-case $COMPILER in
-    4.02) OCAML_VERSION=4.02.3;;
-    4.03) OCAML_VERSION=4.03.0;;
-    4.04) OCAML_VERSION=4.04.2;;
-    4.05) OCAML_VERSION=4.05.0;;
-    4.06) OCAML_VERSION=4.06.0;;
-    4.07) OCAML_VERSION=4.07.0;;
-    system) OCAML_VERSION=`ocamlc -version`;;
-       *) echo Unsupported compiler $COMPILER; exit 1;;
-esac
-
-if [ "$FLAMBDA" = yes ]
-then
-    SWITCH="$OCAML_VERSION+flambda"
-else
-    SWITCH="$OCAML_VERSION"
-fi
-
-if [ -n "$DO_SWITCH" ]
-then
-    opam init --compiler=$SWITCH -y -a
-else
-    opam init -y -a
-fi
-
-eval `opam config env`
+eval $(opam env)
 
 ACTUAL_COMPILER=`ocamlc -version`
 if [ "$ACTUAL_COMPILER" != "$OCAML_VERSION" ]
 then
     echo Expected OCaml $OCAML_VERSION, but $ACTUAL_COMPILER is installed
 fi
-
 
 
 # Pin Js_of_ocaml, install dependencies, and then install Js_of_ocaml.
