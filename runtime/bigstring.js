7c1c8cff90712f6df67026ba7349941ae7ebf603
Hugo Heuzard <hugo.heuzard@gmail.com>
2018-11-29 23:23:08 +0800

    Runtime: mark some bigstring stubs as weakdef Theses stubs are being moved where they belong to (Base,Core_kernel, ..) Making stubs weak will provide a smoother transition.

--- runtime/bigstring.js
+++ runtime/bigstring.js
@@ -1,12 +1,16 @@
 ///////// BIGSTRING
 //Provides: bigstring_alloc
 //Requires: caml_ba_create
+//Weakdef
+//Will be defined in Base
 function bigstring_alloc(_,size){
   return caml_ba_create(12, 0, [0,size]);
 }
 
 //Provides: bigstring_destroy_stub
 //Requires: caml_invalid_argument, caml_ba_create_from
+//Weakdef
+//Will be defined in Core_kernel
 function bigstring_destroy_stub(v_bstr) {
   if (v_bstr.data2 != null) {
       caml_invalid_argument("bigstring_destroy: unsupported kind");
@@ -30,6 +34,8 @@ function bigstring_destroy_stub(v_bstr) {
 
 //Provides: bigstring_blit_bigstring_bytes_stub
 //Requires: caml_bytes_set, caml_ba_get_1
+//Weakdef
+//Will be defined in Base
 function bigstring_blit_bigstring_bytes_stub(v_bstr, v_src_pos, v_str, v_dst_pos, v_len){
   for(var i = 0; i < v_len; i++){
     var c = caml_ba_get_1(v_bstr,v_src_pos + i);
@@ -40,14 +46,18 @@ function bigstring_blit_bigstring_bytes_stub(v_bstr, v_src_pos, v_str, v_dst_pos
 
 //Provides: bigstring_blit_bigstring_string_stub
 //Requires: bigstring_blit_bigstring_bytes_stub
+//From cstruct
 var bigstring_blit_bigstring_string_stub = bigstring_blit_bigstring_bytes_stub
 
 //Provides: caml_blit_bigstring_to_string
 //Requires: bigstring_blit_bigstring_bytes_stub
+//From cstruct
 var caml_blit_bigstring_to_string = bigstring_blit_bigstring_bytes_stub
 
 //Provides: bigstring_blit_string_bigstring_stub
 //Requires: caml_string_get, caml_ba_set_1
+//Weakdef
+//Will be defined in Base
 function bigstring_blit_string_bigstring_stub(v_str, v_src_pos, v_bstr, v_dst_pos, v_len){
   for (var i = 0; i < v_len; i++) caml_ba_set_1(v_bstr,v_dst_pos + i,caml_string_get(v_str,v_src_pos + i));
   return 0;
@@ -55,6 +65,8 @@ function bigstring_blit_string_bigstring_stub(v_str, v_src_pos, v_bstr, v_dst_po
 
 //Provides: bigstring_blit_bytes_bigstring_stub
 //Requires: caml_bytes_get, caml_ba_set_1
+//Weakdef
+//Will be defined in Base
 function bigstring_blit_bytes_bigstring_stub(v_str, v_src_pos, v_bstr, v_dst_pos, v_len){
   for (var i = 0; i < v_len; i++) caml_ba_set_1(v_bstr,v_dst_pos + i,caml_bytes_get(v_str,v_src_pos + i));
   return 0;
@@ -65,6 +77,8 @@ var caml_blit_string_to_bigstring = bigstring_blit_string_bigstring_stub
 
 //Provides: bigstring_blit_stub
 //Requires: caml_ba_get_1, caml_ba_set_1
+//Weakdef
+//Will be defined in Base
 function bigstring_blit_stub(s1, i1, s2, i2, len){
   for (var i = 0; i < len; i++) caml_ba_set_1(s2,i2 + i,caml_ba_get_1(s1,i1 + i));
   return 0;
@@ -72,6 +86,8 @@ function bigstring_blit_stub(s1, i1, s2, i2, len){
 
 //Provides: bigstring_memcmp_stub
 //Requires: caml_ba_get_1
+//Weakdef
+//Will be defined in Base
 function bigstring_memcmp_stub(v_s1, v_s1_pos, v_s2, v_s2_pos, v_len){
   for (var i = 0; i < v_len; i++) {
     var a = caml_ba_get_1(v_s1,v_s1_pos + i);
@@ -84,6 +100,8 @@ function bigstring_memcmp_stub(v_s1, v_s1_pos, v_s2, v_s2_pos, v_len){
 
 //Provides: bigstring_find
 //Requires: caml_ba_get_1
+//Weakdef
+//Will be defined in Base
 function bigstring_find(bs, chr, pos, len){
   while(len > 0){
     if(caml_ba_get_1(bs,pos) == chr) return pos;
@@ -94,55 +112,17 @@ function bigstring_find(bs, chr, pos, len){
 }
 
 //Provides: bigstring_to_array_buffer mutable
+//js_of_ocaml lib
 function bigstring_to_array_buffer(bs) {
   return bs.data.buffer
 }
 
 //Provides: bigstring_of_array_buffer mutable
 //Requires: caml_ba_create_from
+//js_of_ocaml lib
 function bigstring_of_array_buffer(ab) {
   var ta = new joo_global_object.Uint8Array(ab);
   return caml_ba_create_from(ta, null, 0, 12, 0, [ta.length])
-}
-
-//Provides: bigstring_marshal_data_size_stub mutable
-//Requires: caml_failwith, caml_ba_uint8_get32
-function bigstring_marshal_data_size_stub (s, ofs) {
-  if (caml_ba_uint8_get32(s, ofs) != (0x8495A6BE|0))
-    caml_failwith("Marshal.data_size: bad object");
-  return (caml_ba_uint8_get32(s, ofs + 4));
-}
-
-//Provides: bigstring_unmarshal_stub mutable
-//Requires: BigStringReader, caml_input_value_from_reader
-function bigstring_unmarshal_stub(s,ofs) {
-  var reader = new BigStringReader (s, typeof ofs=="number"?ofs:ofs[0]);
-  return caml_input_value_from_reader(reader, ofs)
-}
-
-
-//Provides: bigstring_marshal_stub mutable
-//Requires: caml_output_val, bigstring_alloc, caml_ba_set_1
-function bigstring_marshal_stub (v, _fl) {
-  /* ignores flags... */
-  var arr = caml_output_val (v);
-  var bs  = bigstring_alloc(0,arr.length);
-  for(var i = 0; i < arr.length; i++){
-    caml_ba_set_1(bs, i, arr[i]);
-  }
-  return bs;
-}
-
-//Provides: bigstring_marshal_blit_stub
-//Requires: caml_output_val, caml_failwith, caml_ba_set_1
-function bigstring_marshal_blit_stub (s, ofs, len, v, _fl) {
-  /* ignores flags... */
-  var t = caml_output_val (v);
-  if (t.length > len) caml_failwith ("Marshal.to_buffer: buffer overflow");
-  for(var i = 0; i < t.length; i++){
-    caml_ba_set_1(s, (i + ofs), t[i]);
-  }
-  return t.length;
 }
 
 //Provides: caml_hash_mix_bigstring
