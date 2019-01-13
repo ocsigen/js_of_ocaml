54eb787e00f2222bbf9a213ddd95cb1246dbcff7
Hugo Heuzard <hugo.heuzard@gmail.com>
2019-01-13 18:02:42 +0800

    Lib: missing file

---
+++ lib/tests/nodefs.ml
@@ -0,0 +1,11 @@
+let ic = open_in "nodefs.ml"
+let () =
+  let l = ref [] in
+  let () =
+    try
+      while true do
+        l := input_line ic :: !l;
+      done
+    with End_of_file -> ()
+  in
+  ()          
