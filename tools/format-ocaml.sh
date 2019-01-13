b82781a9730d219c666f5c369bb84345ff6c37a9
Hugo Heuzard <hugo.heuzard@gmail.com>
2019-01-13 12:02:00 +0800

    Misc: update formatter

--- tools/format-ocaml.sh
+++ tools/format-ocaml.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo "# Removing tabs"
-find ./ -regex "^\.\(/[a-zA-Z0-9_-.]*\)*\.ml[il]?" -exec sed -i 's/	//g' {} \;
+git ls-files | grep -E "[a-zA-Z0-9_-.]*\.ml[il]?(.in)?$" | xargs sed -i 's/	//g'
 
 echo "# Whitespace"
 SCRIPT=$(readlink -f $0)
 SCRIPTPATH=`dirname $SCRIPT`
-find ./ -regex "^\.\(/[a-zA-Z0-9_-.]*\)*\.ml[il]?" -exec emacs -batch {} -l ${SCRIPTPATH}/emacs-format-ocaml.el -f emacs-format-function-clean \;
+git ls-files | grep -E "[a-zA-Z0-9_-.]*\.ml[il]?(.in)?$" | xargs emacs -batch {} -l ${SCRIPTPATH}/emacs-format-ocaml.el -f emacs-format-function-clean
 
 echo "# Indent files"
-find ./ -regex "^\.\(/[a-zA-Z0-9_-.]*\)*\.ml[il]?" -exec ocp-indent -i {} \;
+git ls-files | grep -E "[a-zA-Z0-9_-.]*\.ml[il]?(.in)?$" | xargs ocp-indent -i {}
