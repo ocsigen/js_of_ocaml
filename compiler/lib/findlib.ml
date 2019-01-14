4bd2531a75ff263d6fb3cad76b606a84ed4d2e77
Hugo Heuzard <hugo.heuzard@gmail.com>
2019-01-15 00:18:43 +0800

    reindent

--- compiler/lib/findlib.ml
+++ compiler/lib/findlib.ml
@@ -30,11 +30,11 @@ let rec find_in_findlib_paths ?(pkg="stdlib") paths name =
   | [] ->
     raise Not_found
   | path :: rem ->
-     let file = match path_require_findlib path with
-       | Some path ->
-          Filename.concat (Filename.concat (find_pkg_dir pkg) path) name
-       | None -> Filename.concat path name
-     in
-     if Sys.file_exists file
-     then file
-     else find_in_findlib_paths rem name
+    let file = match path_require_findlib path with
+      | Some path ->
+        Filename.concat (Filename.concat (find_pkg_dir pkg) path) name
+      | None -> Filename.concat path name
+    in
+    if Sys.file_exists file
+    then file
+    else find_in_findlib_paths rem name
