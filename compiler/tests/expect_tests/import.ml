999beec28255c6ef9d807f39d43fe2dab345194f
Ty Overby <ty@pre-alpha.com>
2019-04-11 16:19:49 +0800

    move some utilities into their own module

---
+++ compiler/tests/expect_tests/import.ml
@@ -0,0 +1,2 @@
+module J = Js_of_ocaml_compiler.Javascript
+module Jsoo = Js_of_ocaml_compiler
---
+++ compiler/tests/expect_tests/util.mli
@@ -0,0 +1,28 @@
+open Import
+
+val parse_js: string -> J.program
+
+val compile_ocaml_to_bytecode: string -> in_channel
+
+val print_compiled_js
+  :  ?pretty:bool
+  -> in_channel
+  -> string
+
+type find_result =
+  { expressions: J.expression list
+  ; statements: J.statement list
+  ; var_decls: J.variable_declaration list
+  }
+
+val find_javascript
+  :  ?expression:(J.expression -> bool)
+  -> ?statement:(J.statement -> bool)
+  -> ?var_decl:(J.variable_declaration -> bool)
+  -> J.program
+  -> find_result
+
+val expression_to_string
+  :  ?compact:bool
+  -> J.expression
+  -> string
