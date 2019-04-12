0c2d21c169b5bfa93b431d61a3142b617be54df3
Ty Overby <ty@pre-alpha.com>
2019-04-12 13:06:53 +0800

    Adds capibility for writing tests which inspect javascript
    This PR adds helper functions for inspecting Javascript.  While
    asserting that some Javascript produced by the compiler maintains
    some expected property.  Right now there is only one additional
    test, added in
    `compiler/tests/expect_tests/variable_declaration_output.ml`
    but further PRs are planned to be built on top of this infrastructure

---
+++ compiler/tests/expect_tests/util.ml
@@ -0,0 +1,118 @@
+open Import
+
+let parse_js source = Jsoo.Parse_js.(parse (lexer_from_string source))
+
+let rev_prop accessor name value =
+  let past = accessor () in
+  if past = value
+  then fun () -> ()
+  else if value
+  then (
+    Jsoo.Config.Flag.enable name;
+    fun () -> Jsoo.Config.Flag.disable name)
+  else (
+    Jsoo.Config.Flag.disable name;
+    fun () -> Jsoo.Config.Flag.enable name)
+
+let rec call_all = function
+  | [] -> ()
+  | f :: rest ->
+      f ();
+      call_all rest
+
+let print_compiled_js ?(pretty = true) cmo_channel =
+  let program, _, debug_data, _ =
+    Jsoo.Parse_bytecode.from_channel ~debug:`Names cmo_channel
+  in
+  let buffer = Buffer.create 100 in
+  let pp = Jsoo.Pretty_print.to_buffer buffer in
+  let silence_compiler () =
+    let prev = !Jsoo.Stdlib.quiet in
+    Jsoo.Stdlib.quiet := true;
+    fun () -> Jsoo.Stdlib.quiet := prev
+  in
+  let props =
+    if pretty
+    then
+      Jsoo.Config.Flag.
+        [ rev_prop genprim "genprim" false
+        ; rev_prop shortvar "shortvar" false
+        ; rev_prop share_constant "share" false
+        ; rev_prop excwrap "excwrap" false
+        ; rev_prop pretty "pretty" true
+        ; silence_compiler () ]
+    else
+      Jsoo.Config.Flag.
+        [ rev_prop genprim "genprim" true
+        ; rev_prop shortvar "shortvar" true
+        ; rev_prop share_constant "share" true
+        ; rev_prop excwrap "excwrap" true
+        ; rev_prop pretty "pretty" false
+        ; silence_compiler () ]
+  in
+  (try Jsoo.Driver.f pp debug_data program
+   with e ->
+     call_all props;
+     raise e);
+  Buffer.contents buffer
+
+let compile_ocaml_to_bytecode source =
+  let temp_file = Filename.temp_file "jsoo_test" ".ml" in
+  let out = open_out temp_file in
+  Printf.fprintf out "%s" source;
+  close_out out;
+  let prev_debug = !Clflags.debug in
+  Clflags.debug := true;
+  Compile.implementation Format.std_formatter temp_file temp_file;
+  Clflags.debug := prev_debug;
+  open_in (Format.sprintf "%s.cmo" temp_file)
+
+type find_result =
+  { expressions : J.expression list
+  ; statements : J.statement list
+  ; var_decls : J.variable_declaration list }
+
+type finder_fun =
+  { expression : J.expression -> unit
+  ; statement : J.statement -> unit
+  ; variable_decl : J.variable_declaration -> unit }
+
+class finder ff =
+  object
+    inherit Jsoo.Js_traverse.map as super
+
+    method! variable_declaration v =
+      ff.variable_decl v;
+      super#variable_declaration v
+
+    method! expression x =
+      ff.expression x;
+      super#expression x
+
+    method! statement s =
+      ff.statement s;
+      super#statement s
+  end
+
+let find_javascript
+    ?(expression = fun _ -> false)
+    ?(statement = fun _ -> false)
+    ?(var_decl = fun _ -> false)
+    program =
+  let expressions, statements, var_decls = ref [], ref [], ref [] in
+  let append r v = r := v :: !r in
+  let expression a = if expression a then append expressions a in
+  let statement a = if statement a then append statements a in
+  let variable_decl a = if var_decl a then append var_decls a in
+  let t = {expression; statement; variable_decl} in
+  let trav = new finder t in
+  ignore (trav#program program);
+  {statements = !statements; expressions = !expressions; var_decls = !var_decls}
+
+let expression_to_string ?(compact = false) e =
+  let e = [J.Statement (J.Expression_statement e), J.N] in
+  let buffer = Buffer.create 17 in
+  let pp = Jsoo.Pretty_print.to_buffer buffer in
+  Jsoo.Pretty_print.set_compact pp compact;
+  Jsoo.Js_output.program pp e;
+  Buffer.contents buffer
---
+++ compiler/tests/expect_tests/variable_declaration_output.ml
@@ -0,0 +1,47 @@
+open Util
+module J = Js_of_ocaml_compiler.Javascript
+
+let print_var_decl program n =
+  let {var_decls; _} =
+    find_javascript
+      ~var_decl:(function
+        | J.S {name; _}, _ when name = n -> true
+        | _ -> false)
+      program
+  in
+  print_string (Format.sprintf "var %s = " n);
+  match var_decls with
+  | [(_, Some (expression, _))] -> print_string (expression_to_string expression)
+  | _ -> print_endline "not found"
+
+let%expect_test _ =
+  let cmo =
+    compile_ocaml_to_bytecode
+      {|
+    let lr = ref (List.init 2 Obj.repr)
+    let black_box v = lr := (Obj.repr v) :: !lr
+
+    type r = {x: int; y: string}
+
+    let ex = {x = 5; y = "hello"} ;;
+    black_box ex
+    let ax = [|1;2;3;4|] ;;
+    black_box ax
+    let bx = [|1.0;2.0;3.0;4.0|] ;;
+    black_box bx ;;
+
+    (* combined with the black_box function above, this
+       will prevent the ocaml compiler from optimizing
+       away the constructions. *)
+    print_int ((List.length !lr) + (List.length !lr))
+  |}
+  in
+  let program = parse_js (print_compiled_js ~pretty:true cmo) in
+  print_var_decl program "ex";
+  print_var_decl program "ax";
+  print_var_decl program "bx";
+  [%expect
+    {|
+    var ex = [0,5,caml_new_string("hello")];
+    var ax = [0,1,2,3,4];
+    var bx = [254,1,2,3,4]; |}]
