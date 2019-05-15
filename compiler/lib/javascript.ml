d081e894296fd1df3022cd6186670cbe5a74261a
Hugo Heuzard <hugo.heuzard@gmail.com>
2019-05-15 08:36:09 +0800

    fix ident_compare

--- compiler/lib/javascript.ml
+++ compiler/lib/javascript.ml
@@ -284,14 +284,12 @@ and source_element =
 let compare_ident t1 t2 =
   match t1, t2 with
   | V v1, V v2 -> Code.Var.compare v1 v2
-  | S {name = s1; var = v1; _}, S {name = s2; var = v2; _} -> (
+  | S {name = s1; var = v1; loc = l1}, S {name = s2; var = v2; loc = l2} -> (
     match String.compare s1 s2 with
     | 0 -> (
-      match v1, v2 with
-      | None, None -> 0
-      | None, _ -> -1
-      | _, None -> 1
-      | Some v1, Some v2 -> Code.Var.compare v1 v2)
+      match Option.compare Code.Var.compare v1 v2 with
+      | 0 -> Poly.compare l1 l2
+      | n -> n)
     | n -> n)
   | S _, V _ -> -1
   | V _, S _ -> 1
