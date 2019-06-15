e4b40440e3d398eabd6353fab62541febd0d77f4
Hugo Heuzard <hugo.heuzard@gmail.com>
2019-06-15 12:42:33 +0800

    Fmt

--- compiler/tests/gl507.ml
+++ compiler/tests/gl507.ml
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-*)
+ *)
 
 (* https://github.com/ocsigen/js_of_ocaml/issues/507 *)
 (* https://github.com/ocsigen/js_of_ocaml/commit/e2f465dd1ac03da706ae086da37794184db21d31 *)
