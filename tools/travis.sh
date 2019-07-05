d669a18864a97e5867bc3b90e2dffc8a574a0110
Hugo Heuzard <hugo.heuzard@gmail.com>
2019-07-05 11:01:31 +0800

    Misc: migrate parsetree was updated

--- tools/travis.sh
+++ tools/travis.sh
@@ -52,8 +52,6 @@ case $MODE in
         opam pin add --no-action -y js_of_ocaml-tyxml.dev -k path .
         opam pin add --no-action -y js_of_ocaml-toplevel.dev -k path .
 
-        opam pin add --no-action ocaml-migrate-parsetree --dev
-
         opam install -y --best-effort --deps-only $PACKAGES || true
         opam install -y cohttp-lwt-unix menhir ppx_expect yojson sexplib
         opam upgrade --yes
