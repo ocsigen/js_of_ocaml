cb0de324854a88fb3a8e6b9bde1d148a639b249a
Ty Overby <ty@pre-alpha.com>
2019-11-19 16:04:46 -0500

    Runtime: Catch nodejs errors and re-raise them as Sys_error

---
+++ lib/tests/fs_errors.ml
@@ -0,0 +1,31 @@
+(* Js_of_ocaml tests
+ * http://www.ocsigen.org/js_of_ocaml/
+ * Copyright (C) 2019 Ty Overby
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *)
+
+external read_directory : string -> unit = "caml_sys_read_directory"
+
+let%expect_test _ =
+  (try open_in "/a.txt" |> close_in with e -> Printexc.to_string e |> print_endline);
+  [%expect {|
+  (Sys_error "Error: ENOENT: no such file or directory, open '/a.txt'") |}]
+
+let%expect_test _ =
+  (try read_directory "/hello" with e -> Printexc.to_string e |> print_endline);
+  [%expect
+    {|
+   (Sys_error "Error: ENOENT: no such file or directory, scandir '/hello'") |}]
