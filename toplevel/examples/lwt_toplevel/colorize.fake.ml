59515ddb4377f76d2cf4147977f653758ca11d30
hhugo <hugo.heuzard@gmail.com>
2020-03-10 12:04:37 +0000

    Build: switch to dune2 (#964)

--- toplevel/examples/lwt_toplevel/colorize.fake.ml
+++ toplevel/examples/lwt_toplevel/colorize.fake.ml
@@ -1,7 +1,7 @@
 open Js_of_ocaml
 open Js_of_ocaml_tyxml
 
-let text ~a_class:cl s = Tyxml_js.Html.(span ~a:[a_class [cl]] [txt s])
+let text ~a_class:cl s = Tyxml_js.Html.(span ~a:[ a_class [ cl ] ] [ txt s ])
 
 let ocaml = text
 
@@ -11,12 +11,16 @@ let highlight from_ to_ e =
   | Some x ->
       let x = Js.to_string x in
       let (`Pos from_) = from_ in
-      let to_ = match to_ with `Pos n -> n | `Last -> String.length x - 1 in
+      let to_ =
+        match to_ with
+        | `Pos n -> n
+        | `Last -> String.length x - 1
+      in
       e##.innerHTML := Js.string "";
       let span kind s =
         if s <> ""
         then
-          let span = Tyxml_js.Html.(span ~a:[a_class [kind]] [txt s]) in
+          let span = Tyxml_js.Html.(span ~a:[ a_class [ kind ] ] [ txt s ]) in
           Dom.appendChild e (Tyxml_js.To_dom.of_element span)
       in
       span "normal" (String.sub x 0 from_);
