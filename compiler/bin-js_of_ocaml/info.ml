4acdacbb49b36a110816f6015d9bb9c940c8a320
Hugo Heuzard <hugo.heuzard@gmail.com>
2020-04-13 01:23:00 +0200

    man

--- compiler/bin-js_of_ocaml/info.ml
+++ compiler/bin-js_of_ocaml/info.ml
@@ -33,7 +33,7 @@ let make ~name ~doc ~description =
     ; `S "AUTHORS"
     ; `P "Jerome Vouillon, Hugo Heuzard."
     ; `S "LICENSE"
-    ; `P "Copyright (C) 2010-2019."
+    ; `P "Copyright (C) 2010-2020."
     ; `P
         "js_of_ocaml is free software, you can redistribute it and/or modify it under \
          the terms of the GNU Lesser General Public License as published by the Free \
