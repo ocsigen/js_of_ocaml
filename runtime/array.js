0aac4ac1d16bfb9f597cb81ed2437c59b010143c
Hugo Heuzard <hugo.heuzard@gmail.com>
2020-04-23 02:15:10 +0200

    Runtime: move some code from stdlib.js to array.js

---
+++ runtime/array.js
@@ -0,0 +1,61 @@
+// Js_of_ocaml runtime support
+// http://www.ocsigen.org/js_of_ocaml/
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, with linking exception;
+// either version 2.1 of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+///////////// Array
+
+//Provides: caml_array_sub mutable
+function caml_array_sub (a, i, len) {
+  var a2 = new Array(len+1);
+  a2[0]=0;
+  for(var i2 = 1, i1= i+1; i2 <= len; i2++,i1++ ){
+    a2[i2]=a[i1];
+  }
+  return a2;
+}
+
+//Provides: caml_array_append mutable
+function caml_array_append(a1, a2) {
+  var l1 = a1.length, l2 = a2.length;
+  var l = l1+l2-1
+  var a = new Array(l);
+  a[0] = 0;
+  var i = 1,j = 1;
+  for(;i<l1;i++) a[i]=a1[i];
+  for(;i<l;i++,j++) a[i]=a2[j];
+  return a;
+}
+
+//Provides: caml_array_concat mutable
+function caml_array_concat(l) {
+  var a = [0];
+  while (l !== 0) {
+    var b = l[1];
+    for (var i = 1; i < b.length; i++) a.push(b[i]);
+    l = l[2];
+  }
+  return a;
+}
+
+//Provides: caml_array_blit
+function caml_array_blit(a1, i1, a2, i2, len) {
+  if (i2 <= i1) {
+    for (var j = 1; j <= len; j++) a2[i2 + j] = a1[i1 + j];
+  } else {
+    for (var j = len; j >= 1; j--) a2[i2 + j] = a1[i1 + j];
+  };
+  return 0;
+}
