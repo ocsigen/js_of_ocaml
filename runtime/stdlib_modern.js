25855c326a2d5e60cb4251bfe0b4d1a42ce708a1
Hugo Heuzard <hugo.heuzard@gmail.com>
2020-04-23 12:51:27 +0200

    Runtime: remove stale comment

--- runtime/stdlib_modern.js
+++ runtime/stdlib_modern.js
@@ -16,7 +16,6 @@
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
-//TODO: fix the implementation below
 //Provides: caml_call_gen (const, shallow)
 function caml_call_gen(f, args) {
   if(f.fun)
