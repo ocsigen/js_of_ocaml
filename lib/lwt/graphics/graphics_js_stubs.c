4415aaa2f8ae5d805f87b29f7d08c751225b5c4c
Hugo Heuzard <hugo.heuzard@gmail.com>
2020-07-13 14:36:19 +0200

    Lib: fix external in graphics_js, fix #1029

---
+++ lib/lwt/graphics/graphics_js_stubs.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+void caml_gr_doc_of_state () {
+  fprintf(stderr, "Unimplemented Javascript primitive caml_gr_doc_of_state!\n");
+  exit(1);
+}
+void caml_gr_state_create () {
+  fprintf(stderr, "Unimplemented Javascript primitive caml_gr_state_create!\n");
+  exit(1);
+}
+void caml_gr_state_get () {
+  fprintf(stderr, "Unimplemented Javascript primitive caml_gr_state_get!\n");
+  exit(1);
+}
+void caml_gr_state_set () {
+  fprintf(stderr, "Unimplemented Javascript primitive caml_gr_state_set!\n");
+  exit(1);
+}
