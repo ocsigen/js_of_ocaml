ec0597a94140ef587651d50e558b2c9aeb960134
Hugo Heuzard <hugo.heuzard@gmail.com>
2020-07-24 11:14:54 +0200

    Misc: update tests and changelog for 1043

---
+++ compiler/tests-compiler/inlining.ml
@@ -0,0 +1,32 @@
+(* Js_of_ocaml tests
+ * http://www.ocsigen.org/js_of_ocaml/
+ * Copyright (C) 2019 Hugo Heuzard
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *)
+
+open Util
+
+let%expect_test "inline recursive function" =
+  let program = compile_and_parse {|
+    let rec f () = f ()
+    and g () = f ()
+  |} in
+  print_fun_decl program (Some "f");
+  print_fun_decl program (Some "g");
+  [%expect
+    {|
+    function f(param){for(;;)continue}
+    function g(param){return f(0)} |}]
