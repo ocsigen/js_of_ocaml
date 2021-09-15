559b7ca665b07504b385b3a65830f7f6904c9545
Hugo Heuzard <hugo.heuzard@gmail.com>
2021-09-15 19:43:56 +0200

    Toplevel: Improve toplevel test

--- toplevel/test/test_toplevel.ml
+++ toplevel/test/test_toplevel.ml
@@ -1,13 +1,16 @@
-open Js_of_ocaml_toplevel
-
-let () = JsooTop.initialize ()
+let () = Js_of_ocaml_toplevel.JsooTop.initialize ()
 
 let fmt = Format.std_formatter
 
 let () =
-  JsooTop.execute
+  Js_of_ocaml.Sys_js.set_channel_flusher stderr (fun str -> Printf.printf "<ERR>: %s" str)
+
+let () =
+  Js_of_ocaml_toplevel.JsooTop.execute
     true
     ~pp_code:fmt
     ~highlight_location:(fun _ -> ())
     fmt
-    {|let () = print_endline "hello";;|}
+    {|
+let () = print_endline "hello";;
+1+;;|}
