f456f6c091a17c5c06d759284a9666ecf540c93b
Hugo Heuzard <hugo.heuzard@gmail.com>
2021-09-22 15:22:23 +0200

    Runtime: make stdin work on nodejs

---
+++ compiler/tests-io/cat.ml
@@ -0,0 +1,3 @@
+let () =
+  input_line stdin
+  |> output_string stdout
--- runtime/io.js
+++ runtime/io.js
@@ -129,17 +129,23 @@ function caml_ml_open_descriptor_out (fd) {
 
 //Provides: caml_ml_open_descriptor_in
 //Requires: caml_global_data,caml_sys_open,caml_raise_sys_error, caml_ml_channels
+//Requires: fs_node_supported, caml_string_of_jsstring
 function caml_ml_open_descriptor_in (fd)  {
   var data = caml_global_data.fds[fd];
   if(data.flags.wronly) caml_raise_sys_error("fd "+ fd + " is writeonly");
-
+  var refill = null;
+  if(fd == 0 && fs_node_supported()){
+    var fs = require('fs');
+    refill = function () {
+      return caml_string_of_jsstring(fs.readFileSync(0, 'utf8'))};
+  }
   var channel = {
     file:data.file,
     offset:data.offset,
     fd:fd,
     opened:true,
     out: false,
-    refill:null
+    refill:refill
   };
   caml_ml_channels[channel.fd]=channel;
   return channel.fd;
