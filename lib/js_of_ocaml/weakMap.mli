5cb9980543f8c14f738de85c513549825a867e5f
Thibaut <tgudin@besport.com>
2021-09-29 17:52:09 +0200

    adding weakmap experimentations 4

--- lib/js_of_ocaml/weakMap.mli
+++ lib/js_of_ocaml/weakMap.mli
@@ -39,4 +39,4 @@ class type weakMap =
     method has : 'a -> bool Js.meth
   end
 
-val new_weakMap : (?init:('a -> 'b) array -> unit -> weakMap Js.t) Js.constr
+val weakMap : (('a -> 'b) array -> weakMap Js.t) Js.constr
