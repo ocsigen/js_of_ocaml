78309a3d0d1efca280fdccb9d80febefedc15df8
Hugo Heuzard <hugo.heuzard@gmail.com>
2021-09-30 07:45:32 +0200

    Misc: handle no VC case better

--- tools/version/gen/gen.ml
+++ tools/version/gen/gen.ml
@@ -4,13 +4,13 @@ let git_version = Sys.argv.(2)
 
 let version =
   let ic = open_in version in
-  let version = input_line ic in
+  let version = try input_line ic with End_of_file -> "" in
   close_in ic;
   version
 
 let git_version =
   let ic = open_in git_version in
-  let git_version = input_line ic in
+  let git_version = try input_line ic with End_of_file -> "" in
   close_in ic;
   git_version
 
