ec0e3d1f678c7ef9e601cb2e91f5c6693aa30f70
Hugo Heuzard <hugo.heuzard@gmail.com>
2022-01-19 11:39:11 +0100

    Misc: automatic import of flow

---
+++ flow/import.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+[ -d flow ] || git clone git@github.com:facebook/flow.git
+[ -d ocaml-wtf8 ] || git clone  git@github.com:flowtype/ocaml-wtf8.git
+rm -rf parser
+mkdir parser
+
+cp flow/src/hack_forked/utils/collections/flow_set.ml parser/
+cp flow/src/hack_forked/utils/collections/flow_map.ml parser/
+cp flow/src/third-party/sedlex/flow_sedlexing.ml parser/
+cp flow/src/third-party/sedlex/flow_sedlexing.mli parser/
+cp flow/src/parser/*.ml parser/
+cp flow/src/parser/*.mli parser/
+rm parser/flow_parser_dot_js.ml
+rm parser/flow_parser_js.ml
+
+cat << EOF > parser/dune
+(library
+ (name flow_parser)
+ (libraries sedlex)
+ (preprocess (pps sedlex.ppx)))
+EOF
+
+cat << EOF > parser/.ocamlformat
+disable=true
+EOF
+
+cp ocaml-wtf8/src/wtf8.ml parser/
+cp ocaml-wtf8/src/wtf8.mli parser/
+
