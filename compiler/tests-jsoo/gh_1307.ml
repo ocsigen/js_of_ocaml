8c993985feb70314635d83d31ea7c0daa10efe37
hhugo <hugo.heuzard@gmail.com>
2022-10-14 17:17:42 +0200

    Tests: tests for parsing (#1315)

---
+++ compiler/tests-jsoo/gh_1307.ml
@@ -0,0 +1,28 @@
+let test content =
+  Printf.printf "input: %S\n" content;
+  flush_all ();
+  match Parser_1307.root Lexer_1307.token (Lexing.from_string content) with
+  | n ->
+      Printf.printf "%d\n" n;
+      print_endline "success"
+  | exception e ->
+      print_endline (Printexc.to_string e);
+      print_endline "failure"
+
+let%expect_test "parsing" =
+  let (_ : bool) = Parsing.set_trace false in
+  test "a";
+  [%expect {|
+    input: "a"
+    Stdlib.Parsing.Parse_error
+    failure |}];
+  test "aa";
+  [%expect {|
+    input: "aa"
+    0
+    success |}];
+  test "aaa";
+  [%expect {|
+    input: "aaa"
+    Stdlib.Parsing.Parse_error
+    failure |}]
