eb2876d8e07ed880b6730079d57f9fd815fc7194
Hugo Heuzard <hugo.heuzard@gmail.com>
2022-12-05 11:17:37 +0100

    Tests: improve toplevel tests

--- compiler/tests-toplevel/test_toplevel.ml
+++ compiler/tests-toplevel/test_toplevel.ml
@@ -1,10 +1,20 @@
 let () =
   let content = {|
 let () = print_endline "hello";;
-1+;;|} in
+1+;;
+Missing_module.f;;
+|} in
   Topdirs.dir_directory "/static/cmis";
   Toploop.initialize_toplevel_env ();
   Toploop.input_name := "//toplevel//";
   let lexbuf = Lexing.from_string content in
-  let phr = !Toploop.parse_toplevel_phrase lexbuf in
-  ignore (Toploop.execute_phrase true Format.std_formatter phr)
+  while true do
+    try
+      let phr = !Toploop.parse_toplevel_phrase lexbuf in
+      if not (Toploop.execute_phrase true Format.std_formatter phr) then raise Exit
+    with
+    | End_of_file ->
+        flush_all ();
+        exit 0
+    | x -> Location.report_exception Format.std_formatter x
+  done
