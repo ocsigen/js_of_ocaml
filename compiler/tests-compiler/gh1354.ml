1fc205af04c8e0e68048bd2904286bd014f3e0b9
Jérôme Vouillon <jerome.vouillon@gmail.com>
2022-12-11 23:55:20 +0100

    Compiler: Modify the bytecode parser rather than the code generator
    This is cleaner since, when we have an 'Assign (x, y)', x is in scope.

--- compiler/tests-compiler/gh1354.ml
+++ compiler/tests-compiler/gh1354.ml
@@ -56,9 +56,10 @@ with Exit ->
          Stdlib=global_data.Stdlib,
          Stdlib_Printf=global_data.Stdlib__Printf,
          _c_=[0,[4,0,0,0,[12,10,0]],runtime.caml_string_of_jsbytes("%d\n")],
+         _b_=0,
          _a_=0;
         try
-         {var _b_=_a_;_b_ = _a_ + 1 | 0;0;throw Stdlib[3]}
+         {_b_ = _a_ + 1 | 0;0;throw Stdlib[3]}
         catch(_d_)
          {_d_ = caml_wrap_exception(_d_);
           if(_d_ !== Stdlib[3])throw _d_;
@@ -120,14 +121,16 @@ with Exit ->
           [0,
            [4,0,0,0,[12,32,[4,0,0,0,[12,10,0]]]],
            caml_string_of_jsbytes("%d %d\n")],
+         _c_=0,
          _a_=0,
          _b_=0;
         try
-         {var _c_=_a_,_e_=_a_ + 1 | 0;
+         {var _e_=_a_ + 1 | 0;
           _c_ = _e_;
           0;
+          var _f_=_e_;
           try
-           {var _f_=_e_,_h_=_e_ + 1 | 0;_f_ = _h_;_c_ = _h_;0;throw Stdlib[3]}
+           {var _h_=_e_ + 1 | 0;_f_ = _h_;_c_ = _h_;0;throw Stdlib[3]}
           catch(_j_){caml_call3(Stdlib_Printf[3],_g_,_f_ | 0,_b_);throw Stdlib[3]}}
         catch(_i_)
          {_i_ = caml_wrap_exception(_i_);
@@ -183,11 +186,12 @@ with Exit ->
          Stdlib_Printf=global_data.Stdlib__Printf,
          _e_=[0,[4,0,0,0,[12,10,0]],caml_string_of_jsbytes("%d\n")],
          _c_=[0,[4,0,0,0,[12,10,0]],caml_string_of_jsbytes("%d\n")],
+         _b_=0,
          _a_=0;
         try
-         {var _b_=_a_;
+         {var _d_=_a_;
           try
-           {var _d_=_a_,_f_=_a_ + 1 | 0;_d_ = _f_;_b_ = _f_;0;throw Stdlib[3]}
+           {var _f_=_a_ + 1 | 0;_d_ = _f_;_b_ = _f_;0;throw Stdlib[3]}
           catch(_h_){caml_call2(Stdlib_Printf[3],_e_,_d_);throw Stdlib[3]}}
         catch(_g_)
          {_g_ = caml_wrap_exception(_g_);
