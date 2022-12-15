3b0e0a2a33d3d089fafe0059ea64f82062458a4a
Hugo Heuzard <hugo.heuzard@gmail.com>
2022-12-15 16:56:12 +0100

    accept test

--- compiler/tests-compiler/lazy.ml
+++ compiler/tests-compiler/lazy.ml
@@ -40,7 +40,7 @@ let%expect_test "static eval of string get" =
        var _d_=lz[1];
       else
        {var switch$0=0;
-        if(246 === _c_ || 244 === _c_)switch$0 = 1;else var _d_=lz;
-        if(switch$0)var _d_=caml_call1(CamlinternalLazy[2],lz)}
+        if(246 !== _c_ && 244 !== _c_){var _d_=lz;switch$0 = 1}
+        if(! switch$0)var _d_=caml_call1(CamlinternalLazy[2],lz)}
       return [0,_d_,_b_]}
     //end |}]
