101fb0c521c0c69ce8eaa89ece426edb5a8ce543
Jérôme Vouillon <jerome.vouillon@gmail.com>
2022-12-30 11:31:45 +0100

    WIP

--- compiler/lib/parse_bytecode.ml
+++ compiler/lib/parse_bytecode.ml
@@ -1492,11 +1492,7 @@ and compile infos pc state instrs =
         let offset = gets code (pc + 1) in
         let x = State.accu state in
         let args = State.stack_vars state in
-        let y, state' = State.fresh_var state in
-        let args' = State.stack_vars state' in
-        ( Let (y, const 0l) :: instrs
-        , Cond (x, (pc + 2, args), (pc + offset + 1, args'))
-        , state )
+        instrs, Cond (x, (pc + 2, args), (pc + offset + 1, args)), state
     | SWITCH ->
         if debug_parser () then Format.printf "switch ...@.";
         let sz = getu code (pc + 1) in
