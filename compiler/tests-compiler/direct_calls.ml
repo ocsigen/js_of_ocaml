b7576d779c9a08d4ab5e877b50d21b308af6b581
Jérôme Vouillon <jerome.vouillon@gmail.com>
2023-02-02 15:34:25 +0100

    Test: direct calls

--- compiler/tests-compiler/direct_calls.ml
+++ compiler/tests-compiler/direct_calls.ml
@@ -18,7 +18,79 @@
 
 open Util
 
-let%expect_test "test-compiler/lib-effects/effects_call_opt.ml" =
+let%expect_test "direct calls without --enable effects" =
+  let code =
+    compile_and_parse
+      {|
+         (* Arity of the argument of a function / direct call *)
+         let test1 () =
+           let f g x = g x in
+           ignore (f (fun x -> x + 1) 7);
+           ignore (f (fun x -> x *. 2.) 4.)
+
+         (* Arity of the argument of a function / CPS call *)
+         let test2 () =
+           let f g x = g x in
+           ignore (f (fun x -> x + 1) 7);
+           ignore (f (fun x -> x ^ "a") "a")
+
+         (* Arity of functions in a functor / direct call *)
+         let test3 x =
+           let module F(_ : sig end) = struct let f x = x + 1 end in
+           let module M1 = F (struct end) in
+           let module M2 = F (struct end) in
+           (M1.f 1, M2.f 2)
+
+         (* Arity of functions in a functor / CPS call *)
+         let test4 x =
+           let module F(_ : sig end) =
+             struct let f x = Printf.printf "%d" x end in
+           let module M1 = F (struct end) in
+           let module M2 = F (struct end) in
+           M1.f 1; M2.f 2
+|}
+  in
+  print_fun_decl code (Some "test1");
+  print_fun_decl code (Some "test2");
+  print_fun_decl code (Some "test3");
+  print_fun_decl code (Some "test4");
+  [%expect
+    {|
+    function test1(param){
+     function f(g, x){return caml_call1(g, x);}
+     var _d_ = 7;
+     f(function(x){return x + 1 | 0;}, _d_);
+     var _e_ = 4.;
+     f(function(x){return x * 2.;}, _e_);
+     return 0;
+    }
+    //end
+    function test2(param){
+     function f(g, x){return caml_call1(g, x);}
+     var _c_ = 7;
+     f(function(x){return x + 1 | 0;}, _c_);
+     f(function(x){return caml_call2(Stdlib[28], x, cst_a$0);}, cst_a);
+     return 0;
+    }
+    //end
+    function test3(x){
+     function F(symbol){function f(x){return x + 1 | 0;} return [0, f];}
+     var M1 = F([0]), M2 = F([0]), _b_ = caml_call1(M2[1], 2);
+     return [0, caml_call1(M1[1], 1), _b_];
+    }
+    //end
+    function test4(x){
+     function F(symbol){
+      function f(x){return caml_call2(Stdlib_Printf[2], _a_, x);}
+      return [0, f];
+     }
+     var M1 = F([0]), M2 = F([0]);
+     caml_call1(M1[1], 1);
+     return caml_call1(M2[1], 2);
+    }
+    //end |}]
+
+let%expect_test "direct calls with --enable effects" =
   let code =
     compile_and_parse
       ~effects:true
