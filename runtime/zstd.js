fd20d0c6a7dd440463d99f47c88c456804b7f95a
Hugo Heuzard <hugo.heuzard@gmail.com>
2023-02-20 15:23:17 +0100

    PoC

--- runtime/zstd.js
+++ runtime/zstd.js
@@ -1,6 +1,6 @@
 
 //Provides: zstd_decompress
-var zstd_decompress = function () {
+var zstd_decompress = (function () {
 "use strict";
 // aliases for shorter compressed code (most minifers don't do this)
 var ab = ArrayBuffer, u8 = Uint8Array, u16 = Uint16Array, i16 = Int16Array, u32 = Uint32Array, i32 = Int32Array;
@@ -643,4 +643,4 @@ return function decompress(dat, buf) {
     }
     return cct(bufs, ol);
 }
-}
+}) ()
