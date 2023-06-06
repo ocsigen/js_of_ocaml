e7f3e63649d4eafea0d1270779df355b1db89456
Jérôme Vouillon <jerome.vouillon@gmail.com>
2023-06-06 15:19:51 +0200

    Fix tail call optimization
    There should be no tail call within a try body

--- compiler/lib/wasm/wa_tail_call.ml
+++ compiler/lib/wasm/wa_tail_call.ml
@@ -8,7 +8,7 @@ let rec instruction ~tail i =
   | Try (ty, l, catches, catch_all) ->
       Try
         ( ty
-        , instructions ~tail:false l
+        , l
         , List.map ~f:(fun (tag, l) -> tag, instructions ~tail l) catches
         , Option.map ~f:(fun l -> instructions ~tail l) catch_all )
   | Return (Some (Call (symb, l))) -> Return_call (symb, l)
