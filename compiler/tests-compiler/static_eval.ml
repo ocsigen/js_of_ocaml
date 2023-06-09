97287395380a905acef74f8e1bc3e5f63c296aa7
Micah Cantor <micahcantor01@gmail.com>
2023-06-09 10:46:04 +0200

    whitespace change

--- compiler/tests-compiler/static_eval.ml
+++ compiler/tests-compiler/static_eval.ml
@@ -136,24 +136,24 @@ let%expect_test "static eval of string get" =
   print_fun_decl program (Some "copy_bucketlist");
   [%expect
     {|
-      function copy_bucketlist(param){
-       if(! param) return 0;
-       var
-        key = param[1],
-        data = param[2],
-        next = param[3],
-        prec$1 = [0, key, data, next],
-        prec = prec$1,
-        param$0 = next;
-       for(;;){
-        if(! param$0) return prec$1;
-        var
-         key$0 = param$0[1],
-         data$0 = param$0[2],
-         next$0 = param$0[3],
-         prec$0 = [0, key$0, data$0, next$0];
-        prec[3] = prec$0;
-        var prec = prec$0, param$0 = next$0;
-       }
+    function copy_bucketlist(param){
+      if(! param) return 0;
+      var
+      key = param[1],
+      data = param[2],
+      next = param[3],
+      prec$1 = [0, key, data, next],
+      prec = prec$1,
+      param$0 = next;
+      for(;;){
+      if(! param$0) return prec$1;
+      var
+        key$0 = param$0[1],
+        data$0 = param$0[2],
+        next$0 = param$0[3],
+        prec$0 = [0, key$0, data$0, next$0];
+      prec[3] = prec$0;
+      var prec = prec$0, param$0 = next$0;
       }
-      //end |}]
+    }
+    //end |}]
