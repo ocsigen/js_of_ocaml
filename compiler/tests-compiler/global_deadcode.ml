16606a68164eed4ae8e79df48fd139ece15dd7f6
Micah Cantor <micahcantor01@gmail.com>
2023-07-31 11:53:33 +0200

    add new test

--- compiler/tests-compiler/global_deadcode.ml
+++ compiler/tests-compiler/global_deadcode.ml
@@ -2,13 +2,16 @@ open Util
 
 let%expect_test "Eliminates unused functions from functor" =
   let program =
+    (* Create two instances of Set functor so compiler can't inline one implementation. *)
     compile_and_parse_whole_program
       {|
       module Int_set = Set.Make (Int);;
+      module String_set = Set.Make (String);;
 
-      let set = Int_set.singleton 1 in
-      let set = Int_set.add 2 set in
-      print_endline (string_of_int (Int_set.find 1 set + Int_set.find 2 set))
+      let int_set = Int_set.singleton 1 in
+      let string_set = String_set.empty in
+      let string_set = String_set.add "hello" string_set in
+      print_endline (string_of_int (Int_set.find 1 int_set) ^ (String_set.find "hello" string_set))
       |}
   in
   (* Expect: compiles functor members used in the program *)
@@ -23,7 +26,7 @@ let%expect_test "Eliminates unused functions from functor" =
        r = param$0[3],
        v = param$0[2],
        l = param$0[1],
-       c = caml_int_compare(x, v);
+       c = caml_call2(Ord[1], x, v);
       if(0 === c) return v;
       var r$0 = 0 <= c ? r : l, param$0 = r$0;
      }
@@ -33,7 +36,7 @@ let%expect_test "Eliminates unused functions from functor" =
   [%expect {|
     function add(x, t){
      if(! t) return [0, 0, x, 0, 1];
-     var r = t[3], v = t[2], l = t[1], c = caml_int_compare(x, v);
+     var r = t[3], v = t[2], l = t[1], c = caml_call2(Ord[1], x, v);
      if(0 === c) return t;
      if(0 <= c){var rr = add(x, r); return r === rr ? t : bal(l, v, rr);}
      var ll = add(x, l);
@@ -42,4 +45,31 @@ let%expect_test "Eliminates unused functions from functor" =
     //end |}];
   (* Expect: does not compile unused functor member *)
   print_fun_decl program (Some "union");
+  [%expect {| not found |}];
+  print_fun_decl program (Some "inter");
   [%expect {| not found |}]
+
+let%expect_test "Substitutes unused fields with undefined" =
+  let program =
+    compile_and_parse
+      {|
+      let f b x =
+        let t = if b then (1, 2, x) else (3, x, 4) in
+        let (u, _, v) = t in
+        (u, v)
+      in print_int (fst (f true 1) + snd (f false 2))
+      |}
+  in 
+  (* Expect second field in each triple to be replaced by a sentinal variable. *)
+  print_fun_decl program (Some "f");
+  [%expect {|
+    function f(b, x){
+     var t = b ? [0, 1, _c_, x] : [0, 3, _c_, 4], v = t[3], u = t[1];
+     return [0, u, v];
+    }
+    //end |}];
+  (* And that variable is defined as `undefined` *)
+  print_var_decl program "_c_";
+  [%expect {|
+    var _c_ = undefined;
+    //end |}]
