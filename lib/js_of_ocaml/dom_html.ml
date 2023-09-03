de31b5a384c821a917925c98b655c211fa3b0497
Stephane Legrand <slegrand45@gmail.com>
2023-09-03 13:27:18 +0200

    Add download attribute to anchor element

--- lib/js_of_ocaml/dom_html.ml
+++ lib/js_of_ocaml/dom_html.ml
@@ -1351,6 +1351,8 @@ class type anchorElement = object
 
   method coords : js_string t prop
 
+  method download : js_string t prop
+
   method href : js_string t prop
 
   method hreflang : js_string t prop
--- lib/js_of_ocaml/dom_html.mli
+++ lib/js_of_ocaml/dom_html.mli
@@ -1169,6 +1169,8 @@ class type anchorElement = object
 
   method coords : js_string t prop
 
+  method download : js_string t prop
+
   method href : js_string t prop
 
   method hreflang : js_string t prop
