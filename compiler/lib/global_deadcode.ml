658dcb0dcadbfc6ec7db1d4824b797e8c4ff06bc
Micah Cantor <micahcantor01@gmail.com>
2023-10-04 11:41:22 -0500

    Move documentation

--- compiler/lib/global_deadcode.ml
+++ compiler/lib/global_deadcode.ml
@@ -14,16 +14,6 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-
- This module provides a global liveness analysis more powerful than that found in [deadcode.ml]. In particular,
- this analysis annotates blocks with the specific fields that are live. It also uses [global_flow.ml] to determine
- the liveness of function return values. It first computes an initial liveness of each variable by traversing the program IR.
- Then it propagates this information to the dependencies of each variable using a flow analysis solver. Lastly it replaces
- dead variables with a sentinal zero variable.
-
- Although this module does not perform any dead-code elimination itself, it is designed to be used to identify and substitute 
- dead variables that are then removed by [deadcode.ml]. In particular it can allow the elimination of unused functions defined 
- in functors, which the original deadcode elimination cannot. 
  *)
 
 open Code
