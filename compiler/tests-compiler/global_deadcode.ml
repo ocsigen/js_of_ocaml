99caa929ee6f66437260688c9fc057bbc98a75a1
Hugo Heuzard <hugo.heuzard@gmail.com>
2023-10-26 11:43:34 +0200

    Compiler: add purity for jsoo-special primitives

--- compiler/tests-compiler/global_deadcode.ml
+++ compiler/tests-compiler/global_deadcode.ml
@@ -41,30 +41,30 @@ let%expect_test "Eliminates unused functions from functor" =
     function create(l, v, r){
      if(l) var h = l[4], hl = h; else var hl = 0;
      if(r) var h$0 = r[4], hr = h$0; else var hr = 0;
-     var _m_ = hr <= hl ? hl + 1 | 0 : hr + 1 | 0;
-     return [0, l, v, r, _m_];
+     var _n_ = hr <= hl ? hl + 1 | 0 : hr + 1 | 0;
+     return [0, l, v, r, _n_];
     }
     function bal(l, v, r){
      if(l) var h = l[4], hl = h; else var hl = 0;
      if(r) var h$0 = r[4], hr = h$0; else var hr = 0;
      if((hr + 2 | 0) < hl){
       if(! l) return invalid_arg(_b_);
-      var lr = l[3], lv = l[2], ll = l[1], _h_ = height(lr);
-      if(_h_ <= height(ll)) return create(ll, lv, create(lr, v, r));
+      var lr = l[3], lv = l[2], ll = l[1], _i_ = height(lr);
+      if(_i_ <= height(ll)) return create(ll, lv, create(lr, v, r));
       if(! lr) return invalid_arg(_a_);
-      var lrr = lr[3], lrv = lr[2], lrl = lr[1], _i_ = create(lrr, v, r);
-      return create(create(ll, lv, lrl), lrv, _i_);
+      var lrr = lr[3], lrv = lr[2], lrl = lr[1], _j_ = create(lrr, v, r);
+      return create(create(ll, lv, lrl), lrv, _j_);
      }
      if((hl + 2 | 0) >= hr){
-      var _l_ = hr <= hl ? hl + 1 | 0 : hr + 1 | 0;
-      return [0, l, v, r, _l_];
+      var _m_ = hr <= hl ? hl + 1 | 0 : hr + 1 | 0;
+      return [0, l, v, r, _m_];
      }
      if(! r) return invalid_arg(_d_);
-     var rr = r[3], rv = r[2], rl = r[1], _j_ = height(rl);
-     if(_j_ <= height(rr)) return create(create(l, v, rl), rv, rr);
+     var rr = r[3], rv = r[2], rl = r[1], _k_ = height(rl);
+     if(_k_ <= height(rr)) return create(create(l, v, rl), rv, rr);
      if(! rl) return invalid_arg(_c_);
-     var rlr = rl[3], rlv = rl[2], rll = rl[1], _k_ = create(rlr, rv, rr);
-     return create(create(l, v, rll), rlv, _k_);
+     var rlr = rl[3], rlv = rl[2], rll = rl[1], _l_ = create(rlr, rv, rr);
+     return create(create(l, v, rll), rlv, _l_);
     }
     function add(x, t){
      if(! t) return [0, 0, x, 0, 1];
