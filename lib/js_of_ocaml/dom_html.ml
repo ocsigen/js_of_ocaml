5a0a1197c66b4fc602bd7e1d601fcd613f18a86e
Julien Sagot <1826552+sagotch@users.noreply.github.com>
2024-02-29 16:13:25 +0100

    Lib: Dialog element support (#1257)
    * Support for dialogElement
    
    * Added cancel and close events
    
    ---------
    
    Co-authored-by: Hugo Heuzard <hugo.heuzard@gmail.com>

--- lib/js_of_ocaml/dom_html.ml
+++ lib/js_of_ocaml/dom_html.ml
@@ -773,7 +773,11 @@ let invoke_handler = Dom.invoke_handler
 module Event = struct
   type 'a typ = 'a Dom.Event.typ
 
+  let cancel = Dom.Event.make "cancel"
+
   let click = Dom.Event.make "click"
+
+  let close = Dom.Event.make "close"
 
   let copy = Dom.Event.make "copy"
 
@@ -1291,6 +1295,26 @@ class type oListElement = element
 class type dListElement = element
 
 class type liElement = element
+
+class type dialogElement = object
+  inherit element
+
+  method close : unit meth
+
+  method close_returnValue : js_string t -> unit meth
+
+  method open_ : bool t prop
+
+  method returnValue : js_string t prop
+
+  method show : unit meth
+
+  method showModal : unit meth
+
+  method oncancel : ('self t, event t) event_listener prop
+
+  method onclose : ('self t, event t) event_listener prop
+end
 
 class type divElement = element
 
@@ -2527,6 +2551,8 @@ let createDl doc : dListElement t = unsafeCreateElement doc "dl"
 
 let createLi doc : liElement t = unsafeCreateElement doc "li"
 
+let createDialog doc : dialogElement t = unsafeCreateElement doc "dialog"
+
 let createDiv doc : divElement t = unsafeCreateElement doc "div"
 
 let createEmbed doc : embedElement t = unsafeCreateElement doc "embed"
@@ -3350,6 +3376,7 @@ type taggedElement =
   | Col of tableColElement t
   | Colgroup of tableColElement t
   | Del of modElement t
+  | Dialog of dialogElement t
   | Div of divElement t
   | Dl of dListElement t
   | Embed of embedElement t
