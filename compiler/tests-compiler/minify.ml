8175682dbdb2545c4f4c247c7fe74039e27fe976
Hugo Heuzard <hugo.heuzard@gmail.com>
2024-03-19 09:59:45 +0100

    WIP

--- compiler/tests-compiler/minify.ml
+++ compiler/tests-compiler/minify.ml
@@ -420,66 +420,52 @@ test()
 
 let%expect_test _ =
   with_temp_dir ~f:(fun () ->
-      let js_prog =
-        {|
+      let minify js_prog =
+        let js_file =
+          js_prog |> Filetype.js_text_of_string |> Filetype.write_js ~name:"test.js"
+        in
+        let js_min_file =
+          js_file |> jsoo_minify ~flags:[ "--enable"; "shortvar" ] ~pretty:false
+        in
+        print_file (Filetype.path_of_js_file js_min_file)
+      in
+      minify {|
 function f (x) {
   let {toto} = x;
   return toto;
 }
-
+|};
+      [%expect {|
+        $ cat "test.min.js"
+          1: function
+          2: f(a){let{toto:b}=a;return b} |}];
+      minify
+        {|
 function g(x) {
   var toto, test, tata; 
   for( { toto : tata = test } in x ) {
     console.log(tata);
   }
 }
-
+|};
+      [%expect {|
+        $ cat "test.min.js"
+          1: function
+          2: g(a){var
+          3: d,c,b;for({toto:b=c}in
+          4: a)console.log(b)} |}];
+      minify
+        {|
 function h(x) {
   var toto;
   for( { toto } in x ) {
     console.log(toto);
   }
 }
-|}
-      in
-      let js_file =
-        js_prog |> Filetype.js_text_of_string |> Filetype.write_js ~name:"test.js"
-      in
-      let js_min_file =
-        js_file |> jsoo_minify ~flags:[ "--enable"; "shortvar" ] ~pretty:false
-      in
-      print_file (Filetype.path_of_js_file js_file);
-      print_file (Filetype.path_of_js_file js_min_file);
-      [%expect
-        {|
-        $ cat "test.js"
-          1:
-          2: function f (x) {
-          3:   let {toto} = x;
-          4:   return toto;
-          5: }
-          6:
-          7: function g(x) {
-          8:   var toto, test, tata;
-          9:   for( { toto : tata = test } in x ) {
-         10:     console.log(tata);
-         11:   }
-         12: }
-         13:
-         14: function h(x) {
-         15:   var toto;
-         16:   for( { toto } in x ) {
-         17:     console.log(toto);
-         18:   }
-         19: }
+|};
+      [%expect {|
         $ cat "test.min.js"
           1: function
-          2: f(a){let{toto:b}=a;return b}function
-          3: g(a){var
-          4: d,c,b;for({toto:b=c}in
-          5: a)console.log(b)}function
-          6: h(a){var
-          7: b;for({toto:b}in
-          8: a)console.log(b)} |}];
-      print_endline (run_javascript js_min_file);
-      [%expect {||}])
+          2: h(a){var
+          3: b;for({toto:b}in
+          4: a)console.log(b)} |}])
