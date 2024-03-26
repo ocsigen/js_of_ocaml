c248337d40c89d3b2ed433554f61a04684dc9f58
Hugo Heuzard <hugo.heuzard@gmail.com>
2024-03-26 14:57:09 +0100

    Runtime: abort instead of exit on unimplemented js primitives

--- lib/js_of_ocaml/js_of_ocaml_stubs.c
+++ lib/js_of_ocaml/js_of_ocaml_stubs.c
@@ -1,26 +1,32 @@
 #include <stdlib.h>
 #include <stdio.h>
+
 void caml_js_error_of_exception () {
   fprintf(stderr, "Unimplemented Javascript primitive caml_js_error_of_exception!\n");
-  exit(1);
+  abort();
 }
+
 void caml_js_get_console () {
   fprintf(stderr, "Unimplemented Javascript primitive caml_js_get_console!\n");
-  exit(1);
+  abort();
 }
+
 void caml_js_html_entities () {
   fprintf(stderr, "Unimplemented Javascript primitive caml_js_html_entities!\n");
-  exit(1);
+  abort();
 }
+
 void caml_js_html_escape () {
   fprintf(stderr, "Unimplemented Javascript primitive caml_js_html_escape!\n");
-  exit(1);
+  abort();
 }
+
 void caml_js_on_ie () {
   fprintf(stderr, "Unimplemented Javascript primitive caml_js_on_ie!\n");
-  exit(1);
+  abort();
 }
+
 void caml_xmlhttprequest_create () {
   fprintf(stderr, "Unimplemented Javascript primitive caml_xmlhttprequest_create!\n");
-  exit(1);
+  abort();
 }
