7ca0c83af8d68cca0108193b4f87632d84a82ff0
Hugo Heuzard <hugo.heuzard@gmail.com>
2024-04-09 22:19:47 +0200

    Toplevel: add test_lib_jsoo library as an example of loading precompiled cma.js files

---
+++ toplevel/examples/lwt_toplevel/test_lib/a.ml
@@ -0,0 +1,3 @@
+external test_stubs : unit -> unit = "test_lib_jsoo_a"
+
+let () = print_endline "This is A"
---
+++ toplevel/examples/lwt_toplevel/test_lib/stubs.js
@@ -0,0 +1,5 @@
+//Provides: test_lib_jsoo_a
+function test_lib_jsoo_a (unit) {
+  console.log("hello");
+  return 0
+}
