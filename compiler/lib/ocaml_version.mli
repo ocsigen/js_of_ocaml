0c0f2921589b460027655fb26e55db5ad0e08e03
Hugo Heuzard <hugo.heuzard@gmail.com>
2024-04-11 15:10:09 +0200

    Misc: refactor version handling

--- compiler/lib/ocaml_version.mli
+++ compiler/lib/ocaml_version.mli
@@ -23,16 +23,3 @@ val current : t
 val compare : t -> t -> int
 
 val split : string -> t
-
-val v :
-  [ `V4_08 (* OCaml 4.08 *)
-  | `V4_09 (* OCaml 4.09 *)
-  | `V4_10 (* OCaml 4.10 *)
-  | `V4_11 (* OCaml 4.11 *)
-  | `V4_12 (* OCaml 4.12 *)
-  | `V4_13 (* OCaml 4.13 *)
-  | `V4_14 (* OCaml 4.14 *)
-  | `V5_00
-  | `V5_01
-  | `V5_02
-  ]
