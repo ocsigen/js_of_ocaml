df2eacb8be3923840c87f25b911722a4e2a10f36
John <37978984+johnridesabike@users.noreply.github.com>
2024-05-03 07:40:45 -0400

    Update documentation

--- lib/js_of_ocaml/typed_array.mli
+++ lib/js_of_ocaml/typed_array.mli
@@ -257,13 +257,13 @@ end
 
 module Bytes : sig
   val of_uint8Array : uint8Array Js.t -> bytes
-  (** This offers an efficient way to convert typed arrays to [bytes] because it
-      will not usually copy its input.
+  (** This efficiently converts a typed array to [bytes] because it will usually
+      not copy its input.
 
       Modifying its input may also modify its output, and vice versa when
       modifying its output. This is not a guarantee, however, since certain
       [bytes] operations may require the runtime to make a copy. One should not
-      use on inputs that are sensitive to modification. *)
+      use this on input that is sensitive to modification. *)
 
   val to_uint8Array : bytes -> uint8Array Js.t
   (** See the words of caution for {!of_uint8Array}. *)
