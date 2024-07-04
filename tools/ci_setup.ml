e15a9bc0bea405ebf3fc2bae6f9829057210915f
Jérôme Vouillon <jerome.vouillon@gmail.com>
2024-07-04 15:09:13 +0200

    Updated dune support

--- tools/ci_setup.ml
+++ tools/ci_setup.ml
@@ -25,11 +25,11 @@ let do_pin = StringSet.of_list [ "base"; "ppx_expect"; "ppx_inline_test"; "time_
 let aliases = [ "ocaml-cstruct", "cstruct" ]
 
 let dune_workspace =
-  {|(lang dune 3.13)
+  {|(lang dune 3.17)
 (env
  (_
   (env-vars (TESTING_FRAMEWORK inline-test))
-  (js_of_ocaml (targets wasm))
+  (js_of_ocaml (submodes wasm))
   (flags :standard -warn-error -8-32-34-49-52-55 -w -67-69)))
 |}
 
