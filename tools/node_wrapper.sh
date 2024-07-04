cdea18fe8a2e6bc65ad68b332e3fa0fa1d32ee2b
Jérôme Vouillon <jerome.vouillon@gmail.com>
2024-07-04 10:25:18 +0200

    Enable the new exception handling instructions in node

--- tools/node_wrapper.sh
+++ tools/node_wrapper.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 export PATH=$(echo $PATH | cut -d : -f 2-) # Do not call oneself recursively
-exec node --experimental-wasm-imported-strings --experimental-wasm-stack-switching --stack-size=10000 "$@"
+exec node --experimental-wasm-exnref --experimental-wasm-imported-strings --experimental-wasm-stack-switching --stack-size=10000 "$@"
