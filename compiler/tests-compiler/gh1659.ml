49ac969537e1ece7a70516757ea1fb4d9ed554c4
Hugo Heuzard <hugo.heuzard@gmail.com>
2024-08-27 10:33:50 +0200

    complete the tests

--- compiler/tests-compiler/gh1659.ml
+++ compiler/tests-compiler/gh1659.ml
@@ -1,46 +1,62 @@
 let%expect_test _ =
-  let prog = {|
-let x = (0., 0.) = (-0., 0.);;
-
-Printf.printf "%B\n" x;;
-  |} in
-  Util.compile_and_run prog;
-  [%expect {|
-    true |}]
-
-let%expect_test _ =
-  let prog = {|
-let f a b = a = b
-let x = f 0. (-0.);;
-
-Printf.printf "%B\n" x;;
-  |} in
-  Util.compile_and_run prog;
-  [%expect {|
-    true |}]
-
-let%expect_test _ =
   let prog =
     {|
-
-let nan = 0. /. 0.
-let f x y = x = y
-let x = f nan nan;;
-Printf.printf "%B\n" x;;
+let f a b = a = b
+let () = Printf.printf "(0., 0.) = (-0., 0.) => %B\n" (f (0., 0.) (-0., 0.))
+let f a b = a = b
+let () = Printf.printf "0. = -0. => %B\n" (f 0. (-0.));;
+let f a b = a = b
+let nan1 = 0. /. 0.
+let nan2 = 0. /. 0.
+let () = Printf.printf "nan = nan => %B\n" (f nan1 nan2);;
   |}
   in
   Util.compile_and_run prog;
-  [%expect {|
-            true |}]
+  [%expect
+    {|
+    (0., 0.) = (-0., 0.) => true
+    0. = -0. => true
+    nan = nan => false
+    |}]
 
 let%expect_test _ =
   let prog =
     {|
 external equals : 'a -> 'a -> bool = "caml_js_equals";;
-let x = equals (0, 0) (0, 0);;
-Printf.printf "%B\n" x;;
+let () = Printf.printf "x = (0., 0.); x = x => %B\n" (let x = (0., 0.) in equals x x)
+let () = Printf.printf "(0., 0.) = (0., 0.) => %B\n" (equals (0., 0.) (0., 0.))
+let () = Printf.printf "0. = -0. => %B\n" (equals 0. (-0.));;
+let nan1 = 0. /. 0.
+let nan2 = 0. /. 0.
+let () = Printf.printf "nan = nan => %B\n" (equals nan1 nan2);;
   |}
   in
   Util.compile_and_run prog;
-  [%expect {|
-    false |}]
+  [%expect
+    {|
+    x = (0., 0.); x = x => true
+    (0., 0.) = (0., 0.) => false
+    0. = -0. => true
+    nan = nan => false
+    |}]
+
+let%expect_test _ =
+  let prog =
+    {|
+external equals : 'a -> 'a -> bool = "caml_js_strict_equals";;
+let () = Printf.printf "x = (0., 0.); x = x => %B\n" (let x = (0., 0.) in equals x x)
+let () = Printf.printf "(0., 0.) = (0., 0.) => %B\n" (equals (0., 0.) (0., 0.))
+let () = Printf.printf "0. = -0. => %B\n" (equals 0. (-0.));;
+let nan1 = 0. /. 0.
+let nan2 = 0. /. 0.
+let () = Printf.printf "nan = nan => %B\n" (equals nan1 nan2);;
+  |}
+  in
+  Util.compile_and_run prog;
+  [%expect
+    {|
+    x = (0., 0.); x = x => true
+    (0., 0.) = (0., 0.) => false
+    0. = -0. => true
+    nan = nan => false
+    |}]
