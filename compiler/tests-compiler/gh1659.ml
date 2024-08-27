f4d950e47008c7931893355b97cb16b45f53182c
Hugo Heuzard <hugo.heuzard@gmail.com>
2024-08-27 10:20:27 +0200

    fix

--- compiler/tests-compiler/gh1659.ml
+++ compiler/tests-compiler/gh1659.ml
@@ -9,6 +9,31 @@ Printf.printf "%B\n" x;;
     true |}]
 
 let%expect_test _ =
+  let prog = {|
+let f a b = a = b
+let x = f 0. (-0.);;
+
+Printf.printf "%B\n" x;;
+  |} in
+  Util.compile_and_run prog;
+  [%expect {|
+    true |}]
+
+let%expect_test _ =
+  let prog =
+    {|
+
+let nan = 0. /. 0.
+let f x y = x = y
+let x = f nan nan;;
+Printf.printf "%B\n" x;;
+  |}
+  in
+  Util.compile_and_run prog;
+  [%expect {|
+            true |}]
+
+let%expect_test _ =
   let prog =
     {|
 external equals : 'a -> 'a -> bool = "caml_js_equals";;
