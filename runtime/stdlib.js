e3429e503fc0c37b8ea84422630f6703cf2dd7c7
Hugo Heuzard <hugo.heuzard@gmail.com>
2024-09-23 13:17:44 +0200

    Runtime: add support for OCaml 5.3

--- runtime/stdlib.js
+++ runtime/stdlib.js
@@ -203,3 +203,9 @@ function caml_is_printable(c) {
 function caml_maybe_print_stats(unit) {
   return 0;
 }
+
+//Provides: caml_process_pending_actions_with_root
+//Version: >= 5.3
+function caml_process_pending_actions_with_root(extra_root) {
+  return 0;
+}
