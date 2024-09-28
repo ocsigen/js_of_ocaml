10847ff43a1a6e8a38da74af4bd23b2a04c5dd08
Emilio Jesus Gallego Arias <e+git@x80.org>
2024-09-28 18:00:11 +0200

    [compiler] [build-fs] Test for #1694

---
+++ compiler/tests-full/fs.expected.js
@@ -0,0 +1,31 @@
+(function(a){
+   typeof globalThis !== "object"
+   &&
+    (this
+      ? b()
+      : (a.defineProperty
+         (a.prototype, "_T_", {configurable: true, get: b}),
+        _T_));
+   function b(){
+    var b = this || self;
+    b.globalThis = b;
+    delete a.prototype._T_;
+   }
+  }
+  (Object));
+(function(c){
+   "use strict";
+   function a(a, b){
+    if(c.jsoo_create_file)
+     c.jsoo_create_file(a, b);
+    else{
+     if(! c.caml_fs_tmp) c.caml_fs_tmp = [];
+     c.caml_fs_tmp.push({name: a, content: b});
+    }
+    return 0;
+   }
+   a("/static/file1", "This is file 1");
+   a("/static/dir/file2", "This is file 2");
+   return;
+  }
+  (globalThis));
