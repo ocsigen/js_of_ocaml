189b231133e6eb0f6482fbfc5175c0020a93b875
Jérôme Vouillon <jerome.vouillon@gmail.com>
2024-10-22 15:23:48 +0200

    Change strategy to avoid ambiguity after a return statement
    When pretty-printing, there is no ambiguity since we always have a space
    after the return statement. In compact mode, use a newline instead.

--- compiler/tests-compiler/sourcemap.ml
+++ compiler/tests-compiler/sourcemap.ml
@@ -75,7 +75,7 @@ let%expect_test _ =
         3: (function(globalThis){
         4:    "use strict";
         5:    var runtime = globalThis.jsoo_runtime;
-        6:    function id(x){return x; }
+        6:    function id(x){return x;}
         7:    var Test = [0, id];
         8:    runtime.caml_register_global(0, Test, "Test");
         9:    return;
@@ -89,7 +89,7 @@ let%expect_test _ =
       /dune-root/test.ml:1:4 -> 6:12
       /dune-root/test.ml:1:7 -> 6:15
       /dune-root/test.ml:1:11 -> 6:18
-      /dune-root/test.ml:1:12 -> 6:28
+      /dune-root/test.ml:1:12 -> 6:27
       /dune-root/test.ml:1:12 -> 7:0
       /dune-root/test.ml:1:12 -> 7:7
       /builtin/blackbox.ml:1:0 -> 7:14
