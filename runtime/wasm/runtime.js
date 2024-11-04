a84940da5b710dac6ccfb3b28d06e5094efdbd38
Jérôme Vouillon <jerome.vouillon@gmail.com>
2024-11-04 17:16:51 +0100

    Quiet biome warning regarding "use strict".

--- runtime/wasm/runtime.js
+++ runtime/wasm/runtime.js
@@ -16,6 +16,7 @@
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
 (js) => async (args) => {
+  // biome-ignore lint/suspicious/noRedundantUseStrict:
   "use strict";
   const { link, src, generated } = args;
 
