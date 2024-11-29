dd457be3576a0e710a4bdbba444cf4c32b7007be
Jérôme Vouillon <jerome.vouillon@gmail.com>
2024-11-29 11:49:17 +0100

    Parsing test: remove no longer necessary flush

--- compiler/tests-jsoo/test_parsing.ml
+++ compiler/tests-jsoo/test_parsing.ml
@@ -17,22 +17,15 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  *)
 
-(* see https://github.com/ocaml/ocaml/pull/12046 *)
-external flush_stdout_stderr : unit -> unit = "flush_stdout_stderr"
-
 let parse s =
-  flush_stdout_stderr ();
   try
     let lexbuf = Lexing.from_string s in
     while true do
       let result = Calc_parser.main Calc_lexer.token lexbuf in
-      flush_stdout_stderr ();
       print_int result;
       print_newline ()
     done
-  with Calc_lexer.Eof ->
-    flush_stdout_stderr ();
-    print_endline "EOF"
+  with Calc_lexer.Eof -> print_endline "EOF"
 
 let%expect_test "parsing" =
   let (old : bool) = Parsing.set_trace true in
