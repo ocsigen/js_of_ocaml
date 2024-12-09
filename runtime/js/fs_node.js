1761fe3a40f5d57d1048ede3bb61d88c6818c759
Hugo Heuzard <hugo.heuzard@gmail.com>
2024-12-09 17:16:36 +0100

    WIP

--- runtime/js/fs_node.js
+++ runtime/js/fs_node.js
@@ -34,7 +34,7 @@ function fs_node_supported() {
 //Requires: MlNodeFd, caml_raise_sys_error, caml_raise_with_args
 //Requires: make_unix_err_args, caml_named_value, caml_string_of_jsstring
 function MlNodeDevice(root) {
-  this.fs = nodefs
+  this.fs = globalThis.nodefs
   this.root = root;
 }
 MlNodeDevice.prototype.nm = function (name) {
@@ -266,7 +266,7 @@ function MlNodeDevice() {}
 //Provides: MlNodeFd
 //Requires: MlFile, caml_uint8_array_of_string, caml_uint8_array_of_bytes, caml_bytes_set, caml_raise_sys_error
 function MlNodeFd(fd, flags) {
-  this.fs = require("node:fs");
+  this.fs = globalThis.nodefs;
   this.fd = fd;
   this.flags = flags;
 }
@@ -325,7 +325,7 @@ function MlNodeFd() {}
 function caml_sys_open_for_node(fd, flags) {
   if (flags.name) {
     try {
-      var fs = require("node:fs");
+      var fs = globalThis.nodefs;
       var fd2 = fs.openSync(flags.name, "rs");
       return new MlNodeFd(fd2, flags);
     } catch (e) {}
