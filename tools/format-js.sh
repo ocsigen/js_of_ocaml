e0ec46eb83a54fe0f82cfa31e3507629c4b571c7
Hugo Heuzard <hugo.heuzard@gmail.com>
2024-12-09 09:30:01 +0100

    Misc: remove unused files

--- tools/format-js.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "# Removing tabs"
-find ./ -regex "^\.\(/[a-zA-Z0-9_-.]*\)*\.js" -exec sed -i 's/	//g' {} \;
-
-echo "# Indent files"
-SCRIPT=$(readlink -f $0)
-SCRIPTPATH=`dirname $SCRIPT`
-find ./ -regex "^\.\(/[a-zA-Z0-9_-.]*\)*\.js" -exec emacs -batch {} -l ${SCRIPTPATH}/emacs-format-js.el -f emacs-format-js \;
