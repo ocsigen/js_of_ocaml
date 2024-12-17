be85e6f73903f91a04ed4e137e6dd1baaa6d44d0
hhugo <hugo.heuzard@gmail.com>
2024-12-17 13:22:49 +0100

    Tests: better constraint for tests (#1773)

---
+++ compiler/tests-check-prim/gen_dune.ml
@@ -0,0 +1,106 @@
+(** *)
+
+type version =
+  [ `V4_08
+  | `V4_09
+  | `V4_10
+  | `V4_11
+  | `V4_12
+  | `V4_13
+  | `V4_14
+  | `V5_0
+  | `V5_1
+  | `V5_2
+  | `V5_3
+  | `V5_4
+  ]
+
+let string_of_version : version -> string = function
+  | `V4_08 -> "4.08"
+  | `V4_09 -> "4.09"
+  | `V4_10 -> "4.10"
+  | `V4_11 -> "4.11"
+  | `V4_12 -> "4.12"
+  | `V4_13 -> "4.13"
+  | `V4_14 -> "4.14"
+  | `V5_0 -> "5.0"
+  | `V5_1 -> "5.1"
+  | `V5_2 -> "5.2"
+  | `V5_3 -> "5.3"
+  | `V5_4 -> "5.4"
+
+let next_version : version -> version option = function
+  | `V4_08 -> Some `V4_09
+  | `V4_09 -> Some `V4_10
+  | `V4_10 -> Some `V4_11
+  | `V4_11 -> Some `V4_12
+  | `V4_12 -> Some `V4_13
+  | `V4_13 -> Some `V4_14
+  | `V4_14 -> Some `V5_0
+  | `V5_0 -> Some `V5_1
+  | `V5_1 -> Some `V5_2
+  | `V5_2 -> Some `V5_3
+  | `V5_3 -> Some `V5_4
+  | `V5_4 -> None
+
+type os_type =
+  | Unix
+  | Win32
+
+let string_of_os_type = function
+  | Unix -> "Unix"
+  | Win32 -> "Win32"
+
+let rule bc ocaml_version os_type =
+  let vl =
+    [ Printf.sprintf "(>= %%{ocaml_version} %s)" (string_of_version ocaml_version) ]
+  in
+  let vu =
+    match next_version ocaml_version with
+    | None -> []
+    | Some up -> [ Printf.sprintf "(< %%{ocaml_version} %s)" (string_of_version up) ]
+  in
+  let os =
+    match os_type with
+    | None -> []
+    | Some os_type -> [ Printf.sprintf "(= %%{os_type} %s)" (string_of_os_type os_type) ]
+  in
+  let enabled_if = Printf.sprintf "(and %s)" (String.concat "" (vl @ vu @ os)) in
+
+  let target =
+    Filename.chop_extension bc
+    ^ (match os_type with
+      | None -> ""
+      | Some os_type -> "-" ^ string_of_os_type os_type)
+    ^ "."
+    ^ string_of_version ocaml_version
+    ^ ".output"
+  in
+  Printf.sprintf
+    {|(rule
+ (targets %s)
+ (mode
+  (promote (until-clean)))
+ (enabled_if %s)
+ (action
+  (with-stdout-to
+   %%{targets}
+   (run
+    %%{bin:js_of_ocaml}
+    check-runtime
+    +dynlink.js
+    +toplevel.js
+    %%{dep:%s}))))
+|}
+    target
+    enabled_if
+    bc
+
+let () =
+  let versions : version list = [ `V4_14; `V5_2 ] in
+  List.iter
+    (fun ocaml_version ->
+      List.iter
+        (fun (bc, os_type) -> print_endline (rule bc ocaml_version os_type))
+        [ "main.bc", None; "unix.bc", Some Win32; "unix.bc", Some Unix ])
+    versions
