7b2d639eca951a804c4810f0d8f7dd7bd9570115
Jérôme Vouillon <jerome.vouillon@gmail.com>
2024-12-20 09:38:26 +0100

    WIP

--- tools/ci_setup.ml
+++ tools/ci_setup.ml
@@ -12,7 +12,7 @@ module StringSet = Set.Make (String)
 
 let repo = "janestreet/opam-repository/packages"
 
-let roots = [ "bonsai"; "string_dict" ]
+let roots = [ "bonsai_web_components"; "string_dict"; "ppx_html" ]
 
 let omitted_others = StringSet.of_list [ "cohttp-async"; "cohttp"; "uri"; "uri-sexp" ]
 
@@ -32,7 +32,7 @@ let dune_workspace =
  (_
   (env-vars (TESTING_FRAMEWORK inline-test))
   (js_of_ocaml (enabled_if false))
-  (flags :standard -warn-error -8-32-34-49-52-55 -w -67-69)))
+  (flags :standard -warn-error -7-8-27-30-32-34-37-49-52-55 -w -67-69)))
 |}
 
 let patches =
@@ -60,11 +60,9 @@ diff --git a/dune-project b/dune-project
 index e563d7e..b87e356 100644
 --- a/dune-project
 +++ b/dune-project
-@@ -1,3 +1,3 @@
--(lang dune 2.0)
+@@ -1 +1 @@
+-(lang dune 3.11)
 +(lang dune 3.17)
- 
- (formatting disabled)
 diff --git a/test/src/dune b/test/src/dune
 index f93ae3f..3f00557 100644
 --- a/test/src/dune
@@ -72,7 +70,7 @@ index f93ae3f..3f00557 100644
 @@ -2,5 +2,6 @@
   (name bignum_test)
   (libraries bigint bignum core expect_test_helpers_core
-    sexp_grammar_validation)
+    sexp_grammar_validation zarith)
 + (inline_tests (flags -drop-tag no-js -drop-tag no-wasm -drop-tag 64-bits-only) (modes js wasm))
   (preprocess
    (pps ppx_jane)))
@@ -91,58 +89,7 @@ index c6d09fb..61b1e5b 100644
 +
  let%expect_test "Bignum.abs" =
    let test t =
-     let t' = require_no_allocation [%here] (fun () -> abs t) in
-@@ -71,7 +76,7 @@ let%expect_test "Bignum.sexp_of_t does use Scientific Notation" =
- let compare_floats ~of_float x =
-   let x' = x |> of_float |> Bignum.to_float in
-   if not (Float.( = ) x x' || (Float.is_nan x && Float.is_nan x'))
--  then raise_s [%message "mismatch" (x : float) (x' : float)]
-+  then raise_s [%message "mismatch" (x : float) (x' : float) (x |> of_float : Bignum.t)]
- ;;
- 
- let%expect_test "roundtrip: f |> Bignum.of_float_decimal |> Bignum.to_float" =
-@@ -783,7 +788,7 @@ let%test_module _ =
-          -1073741825 -> ( 6) \001\253\255\255\255\191 |}]
-     ;;
- 
--    let%expect_test ("bin_io serialization V2 (javascript)" [@tags "js-only"]) =
-+    let%expect_test ("bin_io serialization V2 (javascript)" [@tags "js-only", "no-wasm"]) =
-       bin_io_tests (module V2);
-       [%expect
-         {|
-@@ -811,6 +816,34 @@ let%test_module _ =
-          -1073741825 -> ( 6) \001\253\255\255\255\191 |}]
-     ;;
- 
-+    let%expect_test ("bin_io serialization V2 (Wasm)" [@tags "wasm-only"]) =
-+      bin_io_tests (module V2);
-+      [%expect
-+        {|
-+                   0 -> ( 1) \000
-+                   1 -> ( 2) \001\001
-+                  -1 -> ( 3) \001\255\255
-+           100000001 -> ( 6) \001\253\001\225\245\005
-+           1000000.1 -> ( 6) \002\253\129\150\152\000
-+           100000.01 -> ( 6) \003\253\129\150\152\000
-+           10000.001 -> ( 6) \004\253\129\150\152\000
-+           1000.0001 -> ( 6) \005\253\129\150\152\000
-+           100.00001 -> ( 6) \006\253\129\150\152\000
-+           10.000001 -> ( 6) \007\253\129\150\152\000
-+           1.0000001 -> ( 6) \008\253\129\150\152\000
-+          0.10000001 -> ( 6) \009\253\129\150\152\000
-+         0.010000001 -> (11) \010\253\129\150\152\000\253\000\202\154\059
-+        0.0010000001 -> (22) \011\02010000001\04710000000000
-+      10000000000000 -> (16) \011\01410000000000000
-+     -10000000000000 -> (17) \011\015\04510000000000000
-+12345678901234567.12345678901234567 -> (55) \01151234567890123456712345678901234567\047100000000000000000
-+       1099511627775 -> (15) \011\0131099511627775
-+          1073741823 -> ( 6) \001\253\255\255\255\063
-+         -1073741824 -> ( 6) \001\253\000\000\000\192
-+          1073741824 -> (12) \011\0101073741824
-+         -1073741825 -> (13) \011\011\0451073741825 |}]
-+    ;;
-+
-     let%expect_test "bin_io de-serialization V2" =
+     let t' = require_no_allocation (fun () -> abs t) in
 |bignum}
     )
   ]
@@ -179,6 +126,13 @@ let rec traverse visited p =
     | exception Not_found -> visited
     | opam ->
         let l = dependencies opam in
+        Format.eprintf
+          "%s: %a@."
+          p
+          (Format.pp_print_list
+             ~pp_sep:(fun f () -> Format.fprintf f " ")
+             (fun f -> Format.fprintf f "%s"))
+          l;
         List.fold_left traverse visited l
 
 let forked_packages =
@@ -192,7 +146,7 @@ let forked_packages =
   close_in ch;
   StringSet.of_list l
 
-let is_forked p = StringSet.mem p forked_packages
+let is_forked p = false (*ZZZ StringSet.mem p forked_packages*)
 
 let exec_async ~delay cmd =
   let p =
@@ -257,8 +211,12 @@ let () =
     List.fold_left traverse StringSet.empty roots
     |> StringSet.partition (fun p -> List.mem_assoc p packages)
   in
+  (* ZZZ
+  opam repo add js /tmp/jane-street/opam-repository/
+
   pin_packages js;
   install_others others;
+*)
   sync_exec (fun i () -> clone i "ocaml-uri" "https://github.com/mirage/ocaml-uri") [ () ];
   sync_exec
     (fun i nm ->
