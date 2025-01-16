30df8375243a1ef938cf2f3c57700ced01befa14
Jérôme Vouillon <jerome.vouillon@gmail.com>
2025-01-16 11:27:49 +0100

    Add repro test

---
+++ compiler/tests-jsoo/gh_1798.ml
@@ -0,0 +1,10 @@
+let%expect_test _ =
+  (match Marshal.to_buffer (Bytes.create 1024) 0 1024 10 [] with
+  | _ -> print_endline "success"
+  | exception e ->
+      print_endline (Printexc.to_string e);
+      print_endline "failure");
+  [%expect {|
+    Failure("TypeError: Cannot read properties of undefined (reading 'length')")
+    failure
+ |}]
