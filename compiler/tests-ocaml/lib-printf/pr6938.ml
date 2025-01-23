e3bdaa87af5493d05804132cb547664e003c1244
Hugo Heuzard <hugo.heuzard@gmail.com>
2025-01-23 16:39:19 +0100

    FMT

--- compiler/tests-ocaml/lib-printf/pr6938.ml
+++ compiler/tests-ocaml/lib-printf/pr6938.ml
@@ -1,7 +1,7 @@
 (* TEST
-   include testing;
-   flags = "-no-strict-formats";
- *)
+ include testing;
+ flags = "-no-strict-formats";
+*)
 
 (* these are not valid under -strict-formats, but we test them here
    for backward-compatibility *)
