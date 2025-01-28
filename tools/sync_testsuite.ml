9d744f57ef4b735293a6656af46edba3b22c9c94
Hugo Heuzard <hugo.heuzard@gmail.com>
2025-01-28 11:12:19 +0100

    Misc: update test sync tool

--- tools/sync_testsuite.ml
+++ tools/sync_testsuite.ml
@@ -46,18 +46,81 @@ let _ignore_ x =
   then `Dynlink
   else
     match x with
-    | "lib-either" -> `Expect
-    | "lib-array" -> `Expect
-    | "lib-bigarray-2" -> `Stubs
-    | "lib-digest/blake2b_self_test.ml" -> `Stubs
-    | "lib-bigarray-file" -> `Mapfile
-    | "lib-lazy" -> `Expect
-    | "lib-internalformat" -> `Expect
-    | "lib-random/parallel.ml" | "lib-str/parallel.ml" -> `Parallel
-    | "lib-hashtbl/compatibility.ml" -> `Old
+    | "afl-instrumentation" (* not relevant *)
+    | "arch-power" (* not relevant *)
+    | "asmcomp" (* not relevant *)
+    | "asmgen" (* not relevant *)
+    | "ast-invariants" (* not relevant *)
+    | "badly-ordered-deps" (* not relevant *)
+    | "basic-multdef" (* What does it test ? *)
+    | "c-api" (* not relevant *)
+    | "compaction" (* GC *)
+    | "compiler-libs" (* not relevant *)
+    | "cxx-api" (* not relevant *)
+    | "embedded" (* not relevant *)
+    | "ephe-c-api" (* not relevant *)
+    | "exotic-syntax" (* just syntax *)
+    | "flambda" (* not relevant *)
+    | "frame-pointers" (* not relevant *)
+    | "functors" (* not relevant *)
+    | "gc-roots" (* not relevant *)
+    | "generalized-open" (* not relevant *)
+    | "generated-parse-errors" (* not relevant *)
+    | "hidden_includes" (* not relevant *)
+    | "int64-unboxing" (* not relevant *)
+    | "let-syntax" (* just syntax *)
+    | "lexing" (* not relevant *)
+    | "link-test" (* not relevant *)
+    | "load_path" (* not relevant *)
+    | "locale" (* ?? *)
+    | "match-exception-warnings" (* not relevant *)
+    | "match-side-effects" (* not relevant *)
+    | "manual-intf-c" (* not relevant *)
+    | "memory-model" (* ?? *)
+    | "messages" (* not relevant *)
+    | "no-alias-deps" (* not relevant *)
+    | "output-complete-obj" (* not relevant *)
+    | "parallel" (* ?? *)
+    | "parse-errors" (* not relevant *)
+    | "parsetree" (* not relevant *)
+    | "parsing" (* ?? maybe relevant to test the parsing runtime *)
+    | "ppx-attributes" (* not relevant *)
+    | "ppx-contexts" (* not relevant *)
+    | "reproducibility" (* not relevant *)
+    | "required-external" (* ?? *)
+    | "runtime-C-exceptions" (* ?? *)
+    | "runtime-errors" (* ?? *)
+    | "self-contained-toplevel" (* ?? *)
+    | "shadow_include" (* not relevant *)
+    | "shape-index" (* not relevant *)
+    | "shapes" (* not relevant *)
+    | "statmemprof" (* not relevant *)
+    | "syntactic-arity" (* not relevant *)
+    | "translprim" (* not relevant *)
+    | "tsan" (* not relevant *)
+    | "uid-deps" (* not relevant *)
+    | "uids" (* not relevant *)
+    | "unboxed-primitive-args" (* ?? *)
+    | "unicode" (* not relevant *)
+    | "unwind" (* not relevant *)
+    | "utils" (* not relevant *)
+    | "warnings" (* not relevant *)
+    | "weak-ephe-final" (* ?? *)
+    | "win-unicode" (* not relevant *)
+    | "lf_skiplist" (* not relevant *)
+    | "lib-bigarray-2" -> `Ignore
+    | "lib-digest/blake2b_self_test.ml" -> `Ignore
+    | "lib-bigarray-file" -> `Ignore
+    | "lib-random/parallel.ml" | "lib-str/parallel.ml" -> `Ignore
+    | "lib-hashtbl/compatibility.ml" -> `Ignore
     | _ -> `No
 
 let () =
+  let check_not_ignored path x =
+    match _ignore_ (Filename.concat path x) with
+    | `No -> ()
+    | _ -> failwith (Printf.sprintf "remove %s/%s from ignore" path x)
+  in
   let rec diff f a b path =
     let a0 = Filename.concat a path and b0 = Filename.concat b path in
     let ad = readdir a0 and bd = readdir b0 in
@@ -66,8 +129,12 @@ let () =
     FileSet.iter (fun x -> f (`Extra (File.concat path x))) extra;
     FileSet.iter
       (function
-        | Dir x -> diff f a b (Filename.concat path x)
-        | Ml x -> f (`Same (Filename.concat path x))
+        | Dir x ->
+            check_not_ignored path x;
+            diff f a b (Filename.concat path x)
+        | Ml x ->
+            check_not_ignored path x;
+            f (`Same (Filename.concat path x))
         | Expected x -> f (`Expected (Filename.concat path x)))
       common
   in
@@ -75,20 +142,17 @@ let () =
     (function
       | `Missing (Dir x | Ml x) -> (
           match _ignore_ x with
-          | `Tool | `Typing | `Dynlink | `Expect -> ()
-          | `Stubs | `Old -> ()
-          | `Parallel -> ()
-          | `Mapfile -> ()
+          | `Tool | `Typing | `Dynlink -> ()
+          | `Ignore -> ()
           | `No -> Printf.eprintf "missing %s\n" x)
       | `Missing (Expected x) -> (
           match _ignore_ (x ^ ".ml") with
-          | `Tool | `Typing | `Dynlink | `Expect -> ()
-          | `Stubs | `Old -> ()
-          | `Parallel -> ()
-          | `Mapfile -> ()
+          | `Tool | `Typing | `Dynlink -> ()
+          | `Ignore -> ()
           | `No -> Printf.eprintf "missing expected %s\n" x)
       | `Extra (Dir "effects-2") -> ()
       | `Extra (Ml "testing.ml") -> ()
+      | `Extra (Ml "expect.ml") -> ()
       | `Extra (Expected x) -> Printf.eprintf "extra expected %s\n" x
       | `Extra (Dir x | Ml x) -> Printf.eprintf "extra %s\n" x
       | `Same x -> (
