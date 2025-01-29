9b08ee24aa9a4609536e19aa2d00e9074f05390d
Jérôme Vouillon <jerome.vouillon@gmail.com>
2025-01-29 10:47:54 +0100

    Add some OCaml unix tests

---
+++ compiler/tests-ocaml/lib-unix/common/uexit.ml
@@ -0,0 +1,14 @@
+(* TEST
+ include unix;
+ hasunix;
+ {
+   bytecode;
+ }{
+   native;
+ }
+*)
+
+let _ =
+  at_exit (fun () -> print_string "B\n"; flush stdout);
+  print_string "A\n"; (* don't flush *)
+  Unix._exit 0
