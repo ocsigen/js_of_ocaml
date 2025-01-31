dc2da0cc82c26fcd2719ba319d06cf0f6a311d68
Jérôme Vouillon <jerome.vouillon@gmail.com>
2025-01-31 13:51:51 +0100

    Add test for non-ascii file names

---
+++ compiler/tests-io/non_ascii_filenames.ml
@@ -0,0 +1,34 @@
+let test f v =
+  try
+    ignore (f v);
+    Printf.printf "  %s: PASS\n" v
+  with Sys_error _ | Not_found | Exit -> Printf.printf "  %s: FAIL\n" v
+
+let () =
+  Printf.printf "opening files\n";
+  test open_in "/static/accentué";
+  test open_in "/static/accentu�";
+  test open_in "accentué";
+  test open_in "accentu�";
+  Printf.printf "reading directories\n";
+  let check_file d =
+    let a = Sys.readdir d in
+    if
+      not
+        (Array.exists
+           (fun x ->
+             prerr_endline x;
+             x = "accentué")
+           a)
+    then raise Not_found
+  in
+  test check_file ".";
+  test check_file "/static";
+  Printf.printf "current working directory\n";
+  let test_chdir d =
+    Sys.mkdir d 0o777;
+    Sys.chdir d;
+    if Filename.basename (Sys.getcwd ()) <> "répertoire" then raise Exit
+  in
+  test test_chdir "./répertoire";
+  test test_chdir "/static/répertoire"
