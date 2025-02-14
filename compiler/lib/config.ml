d2774a6d6335503c06b7f32fd8449920581df48d
Jérôme Vouillon <jerome.vouillon@gmail.com>
2025-02-14 10:40:26 +0100

    Add flag trap-on-exception
    To test with Wasm engines which do not support exceptions

--- compiler/lib/config.ml
+++ compiler/lib/config.ml
@@ -108,6 +108,8 @@ module Flag = struct
   let es6 = o ~name:"es6" ~default:false
 
   let wasi = o ~name:"wasi" ~default:false
+
+  let trap_on_exception = o ~name:"trap-on-exception" ~default:false
 end
 
 module Param = struct
