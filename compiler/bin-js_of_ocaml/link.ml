3ca3bf996e9297ce1ade6d642c599b5ab7d7d8a7
Olivier Nicole <olivier@chnik.fr>
2025-03-13 17:14:08 +0100

    Add flag --empty-sourcemap
    This patch implements the `--empty-sourcemap` flag. It is used to
    generate an empty sourcemap, which is cheap. Sometimes application
    authors would like the cheapness of not building sourcemap files to
    speed up development iterations, but their build pipelines do not handle
    the case where `.map` files are not produced at all (think, e.g., custom
    Dune rules depending on `.bc.map` files). In that cases, they would like
    to still generate the empty `.bc.map` files.
    
    Co-authored-by: enoumy <enoumy@gmail.com>

--- compiler/bin-js_of_ocaml/link.ml
+++ compiler/bin-js_of_ocaml/link.ml
@@ -23,7 +23,7 @@ open Cmdliner
 
 type t =
   { common : Jsoo_cmdline.Arg.t
-  ; source_map : (string option * Source_map.Standard.t) option
+  ; source_map : Source_map.Encoding_spec.t option
   ; js_files : string list
   ; output_file : string option
   ; resolve_sourcemap_url : bool
@@ -50,6 +50,10 @@ let options =
   let sourcemap_inline_in_js =
     let doc = "Inline sourcemap in the generated JavaScript." in
     Arg.(value & flag & info [ "source-map-inline" ] ~doc)
+  in
+  let sourcemap_empty =
+    let doc = "Always generate empty source maps." in
+    Arg.(value & flag & info [ "empty-sourcemap"; "empty-source-map" ] ~doc)
   in
   let sourcemap_root =
     let doc = "root dir for source map." in
@@ -83,6 +87,7 @@ let options =
       no_sourcemap
       sourcemap
       sourcemap_inline_in_js
+      sourcemap_empty
       sourcemap_root
       output_file
       resolve_sourcemap_url
@@ -100,12 +105,19 @@ let options =
           | Some file -> Some file, Some (chop_extension file ^ ".map")
           | None -> None, None
         in
-        Some
-          ( sm_output_file
-          , { (Source_map.Standard.empty ~inline_source_content:true) with
-              file
-            ; sourceroot = sourcemap_root
-            } )
+        let source_map =
+          { (Source_map.Standard.empty ~inline_source_content:true) with
+            file
+          ; sourceroot = sourcemap_root
+          }
+        in
+        let spec =
+          { Source_map.Encoding_spec.output_file = sm_output_file
+          ; source_map
+          ; keep_empty = sourcemap_empty
+          }
+        in
+        Some spec
       else None
     in
     `Ok
@@ -126,6 +138,7 @@ let options =
       $ no_sourcemap
       $ sourcemap
       $ sourcemap_inline_in_js
+      $ sourcemap_empty
       $ sourcemap_root
       $ output_file
       $ resolve_sourcemap_url
