2d227147c1e983e32d2d3d93c9725de094a6a158
Hugo Heuzard <hugo.heuzard@gmail.com>
2025-03-18 15:30:18 +0100

    WIP

--- ppx/ppx_js/lib_internal/ppx_js_internal.ml
+++ ppx/ppx_js/lib_internal/ppx_js_internal.ml
@@ -581,16 +581,13 @@ let preprocess_literal_object mappper fields :
     | Pcf_method (id, priv, Cfk_concrete (bang, body)) ->
         let names = check_name id names in
         let body, body_ty = drop_pexp_poly (mappper body) in
-        let rec create_meth_ty exp =
+        let create_meth_ty exp =
           match exp.pexp_desc with
-          | Pexp_function (params, _, Pfunction_body body) ->
-              let params =
-                List.filter_map params ~f:(function
-                  | { pparam_desc = Pparam_newtype _; _ } -> None
-                  | { pparam_desc = Pparam_val (label, _, _arg); _ } ->
-                      Some (Arg.make ~label ()))
-              in
-              params @ create_meth_ty body
+          | Pexp_function (params, _, _) ->
+              List.filter_map params ~f:(function
+                | { pparam_desc = Pparam_newtype _; _ } -> None
+                | { pparam_desc = Pparam_val (label, _, _arg); _ } ->
+                    Some (Arg.make ~label ()))
           | _ -> []
         in
         let fun_ty = create_meth_ty body in
