b4dc100316d0a05e2196ccf5672fac18781f901a
Hugo Heuzard <hugo.heuzard@gmail.com>
2025-04-04 23:25:30 +0200

    cleanup

--- compiler/lib/code.ml
+++ compiler/lib/code.ml
@@ -857,15 +857,6 @@ let invariant { blocks; start; _ } =
         assert (not (Var.ISet.mem defs x));
         Var.ISet.add defs x)
     in
-    let check_constant = function
-      | NativeInt _ | Int32 _ -> ()
-      | String _ | NativeString _ | Float _ | Float_array _ | Int _ | Int64 _
-      | Tuple (_, _, _) -> ()
-    in
-    let check_prim_arg = function
-      | Pc c -> check_constant c
-      | Pv _ -> ()
-    in
     let check_expr = function
       | Apply _ -> ()
       | Block (_, _, _, _) -> ()
@@ -873,8 +864,8 @@ let invariant { blocks; start; _ } =
       | Closure (l, cont) ->
           List.iter l ~f:define;
           check_cont cont
-      | Constant c -> check_constant c
-      | Prim (_, args) -> List.iter ~f:check_prim_arg args
+      | Constant _ -> ()
+      | Prim (_, _) -> ()
       | Special _ -> ()
     in
     let check_instr i =
