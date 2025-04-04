0f330c6f6eec4fd92ea1fb66c494a68ec9d52b8e
Jérôme Vouillon <jerome.vouillon@gmail.com>
2025-04-04 13:04:11 +0200

    WIP

--- benchmarks/utils/aggregate.sh
+++ benchmarks/utils/aggregate.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 TEMP=$(mktemp)
 
-tee $TEMP | jq '.name |= "Js vs Wasm" | .results[].metrics[].name |= .+"/'$1'"'
+tee $TEMP | \
+jq '.name |= "Js vs Wasm"' | \
+jq '.results[] |= select(.name | test("Code size") | not)' |\
+jq '.results[].metrics[] |= select(.name as $n | ["kb", "boyer", "fannkuch_redux_2"] | index($n) | not)' | \
+jq '.results[].metrics[].name |= if test("/") then .+"-'$1'" else .+"/'$1'" end'
 cat $TEMP
 rm $TEMP
