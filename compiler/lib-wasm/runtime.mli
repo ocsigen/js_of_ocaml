18cc92667d862724afbca6cd9f5613c567ce36cc
Jérôme Vouillon <jerome.vouillon@gmail.com>
2025-04-10 14:37:22 +0200

    Runtime compilation: fail if importing from unexpected modules

--- compiler/lib-wasm/runtime.mli
+++ compiler/lib-wasm/runtime.mli
@@ -1,5 +1,6 @@
 val build :
-     link_options:string list
+     allowed_imports:string list option
+  -> link_options:string list
   -> opt_options:string list
   -> variables:(string * Wat_preprocess.value) list
   -> inputs:Wat_preprocess.input list
