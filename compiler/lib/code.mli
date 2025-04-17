997ca81080f84822e50230af9d7011b79e4b7a47
Jérôme Vouillon <jerome.vouillon@gmail.com>
2025-04-17 11:26:30 +0200

    Move Effect.last_intr to Code.last_instr

--- compiler/lib/code.mli
+++ compiler/lib/code.mli
@@ -300,6 +300,9 @@ val traverse :
 val preorder_traverse :
   fold_blocs_poly -> (Addr.t -> 'c -> 'c) -> Addr.t -> block Addr.Map.t -> 'c -> 'c
 
+val last_instr : instr list -> instr option
+(** Last instruction of a block body, ignoring events *)
+
 val prepend : program -> instr list -> program
 
 val empty : program
