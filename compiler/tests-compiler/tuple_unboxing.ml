cdbe68faf4d4045413dbcfdee6d17996535cdd09
Jérôme Vouillon <jerome.vouillon@gmail.com>
2025-04-30 20:06:12 +0200

    Update tests

--- compiler/tests-compiler/tuple_unboxing.ml
+++ compiler/tests-compiler/tuple_unboxing.ml
@@ -15,8 +15,9 @@ let%expect_test _ =
     (function(globalThis){
        "use strict";
        var runtime = globalThis.jsoo_runtime, _a_ = [0, 1, 2];
-       function f(param){var y = param[2], x = param[1]; return x + y | 0;}
-       var x = f(_a_), Test = [0, f, x];
+       function f$0(x, y){return x + y | 0;}
+       function f(_b_){return f$0(_b_[1], _b_[2]);}
+       var x = f$0(_a_[1], _a_[2]), Test = [0, f, x];
        runtime.caml_register_global(1, Test, "Test");
        return;
       }
@@ -39,8 +40,8 @@ let%expect_test _ =
     (function(globalThis){
        "use strict";
        var runtime = globalThis.jsoo_runtime, _a_ = [0, 1, 1];
-       function f(b, y, t){var match = b ? [0, 1] : t, x = match[1]; return x;}
-       function g(b, t){var match = b ? _a_ : t, x = match[1]; return x;}
+       function f(b, y, t){var x = b ? 1 : t[1]; return x;}
+       function g(b, t){var x = b ? _a_[1] : t[1]; return x;}
        var Test = [0, f, g];
        runtime.caml_register_global(1, Test, "Test");
        return;
@@ -79,13 +80,13 @@ let%expect_test _ =
           [0,
            function(_c_, _b_, _a_){
             switch(_c_){
-              case 0:
-               var _d_ = [0, _b_]; break;
+              case 2:
+               var _d_ = _a_ + 1 | 0; break;
               case 1:
-               var _d_ = [1, _a_]; break;
-              default: var _d_ = [1, _a_ + 1 | 0];
+               var _d_ = _a_; break;
+              default: var _d_ = _b_;
             }
-            return _d_[1];
+            return _d_;
            }];
        runtime.caml_register_global(0, Test, "Test");
        return;
