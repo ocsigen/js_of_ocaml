6f5be51b564be9fa86192c5d171dd44d661eff08
Jérôme Vouillon <jerome.vouillon@gmail.com>
2025-05-12 17:12:01 +0200

    Test: deadcode elimination of cyclic values

---
+++ compiler/tests-compiler/update_dummy.ml
@@ -0,0 +1,23 @@
+open Util
+
+let%expect_test "deadcode elimination of cyclic values" =
+  let program =
+    compile_and_parse
+      {|
+      let f () =
+        let rec x = 1 :: x in
+        let rec y = 1 :: y in
+        snd (x, y)
+  |}
+  in
+  print_fun_decl program (Some "f");
+  [%expect
+    {|
+    function f(param){
+     var x = [];
+     caml_update_dummy(x, [0, 1, x]);
+     var y = [];
+     caml_update_dummy(y, [0, 1, y]);
+     return y;
+    }
+    //end |}]
