60b2465dcfdc7e787bd8de3a4ab91a6e9efaf8d3
Jérôme Vouillon <jerome.vouillon@gmail.com>
2025-07-10 21:54:25 +0200

    js_of_ocaml-no-compression-module-5.2

--- compiler/tests-jsoo/test_marshal_compressed.ml
+++ compiler/tests-jsoo/test_marshal_compressed.ml
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  *)
-
+(*
 let%expect_test _ =
   let data =
     "\132\149\166\189\r\022\206\021\001\147F\137d(\181/\253\000Xm\000\0000\n\
@@ -41,4 +41,4 @@ let%expect_test _ =
   in
   close_in ch;
   Printf.printf "%s ... (%d)\n" (String.sub s 0 20) (String.length s);
-  [%expect {| cccccccccccccccccccc ... (10000) |}]
+  [%expect {| cccccccccccccccccccc ... (10000) |}] *)
