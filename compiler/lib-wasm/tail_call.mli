e307fb316fc1635ede8732c1c8cc76cf11cca8ec
Jérôme Vouillon <jerome.vouillon@gmail.com>
2025-07-10 14:23:12 +0200

    Disable tail-call optimization for Math builtins
    V8 currently generates suboptimal code for this (the builtins are no longer
    inlined).

--- compiler/lib-wasm/tail_call.mli
+++ compiler/lib-wasm/tail_call.mli
@@ -16,4 +16,7 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  *)
 
-val f : Wasm_ast.instruction list -> Wasm_ast.instruction list
+val f :
+     no_tail_call:unit Code.Var.Hashtbl.t
+  -> Wasm_ast.instruction list
+  -> Wasm_ast.instruction list
