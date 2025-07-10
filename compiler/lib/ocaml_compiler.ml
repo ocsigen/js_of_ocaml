99fb97edf125dd70e61bc46d89228a3e12856690
Jérôme Vouillon <jerome.vouillon@gmail.com>
2025-07-10 21:39:59 +0200

    js_of_ocaml-symtable-5.2-api

--- compiler/lib/ocaml_compiler.ml
+++ compiler/lib/ocaml_compiler.ml
@@ -169,7 +169,7 @@ module Symtable = struct
     let get i = Char.code (Bytes.get buf i) in
     let n = get 0 + (get 1 lsl 8) + (get 2 lsl 16) + (get 3 lsl 24) in
     n
-  [@@if ocaml_version < (5, 2, 0)]
+  (* [@@if ocaml_version < (5, 2, 0)]
 
   let reloc_ident name =
     let buf = Bigarray.(Array1.create char c_layout 4) in
@@ -182,6 +182,8 @@ module Symtable = struct
     let n = get 0 + (get 1 lsl 8) + (get 2 lsl 16) + (get 3 lsl 24) in
     n
   [@@if ocaml_version >= (5, 2, 0)]
+  *)
+
 
   let current_state () : GlobalMap.t =
     let x : Symtable.global_map = Symtable.current_state () in
