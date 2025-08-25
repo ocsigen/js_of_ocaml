77b89216617af56d36cecc43676c8ecab3f84cf6
Olivier Nicole <olivier@chnik.fr>
2025-08-25 15:41:00 +0200

    Make aggressive lambda lifting opt-in

--- compiler/lib/driver.ml
+++ compiler/lib/driver.ml
@@ -142,10 +142,21 @@ let collects_shapes ~shapes (p : Code.program) =
     map)
   else StringMap.empty
 
+let all_functions p =
+  let open Code in
+  fold_closures
+    p
+    (fun name _ _ _ acc ->
+      match name with
+      | Some name -> Var.Set.add name acc
+      | None -> acc)
+    Var.Set.empty
+
 let effects_and_exact_calls
     ~keep_flow_data
     ~deadcode_sentinal
     ~shapes
+    ~lambda_lift_all
     (profile : Profile.t)
     p =
   let fast =
@@ -166,11 +177,8 @@ let effects_and_exact_calls
     else Deadcode.f pure_fun p
   in
   let p =
-    match Config.(target (), effects ()) with
-    | `JavaScript, `Disabled ->
-        (* If effects are disabled, we lambda-lift aggressively. While not
-           necessary, it results in a substantial gain in performance in some
-           programs in Javascript. *)
+    match lambda_lift_all, Config.target (), Config.effects () with
+    | true, `JavaScript, `Disabled ->
         let to_lift = all_functions p in
         let p, _ = Lambda_lifting_simple.f ~to_lift p in
         p
@@ -707,17 +715,7 @@ let link_and_pack ?(standalone = true) ?(wrap_with_fun = `Iife) ?(link = `No) p 
   |> pack ~wrap_with_fun ~standalone
   |> check_js
 
-let all_functions p =
-  let open Code in
-  fold_closures
-    p
-    (fun name _ _ _ acc ->
-      match name with
-      | Some name -> Var.Set.add name acc
-      | None -> acc)
-    Var.Set.empty
-
-let optimize ~shapes ~profile ~keep_flow_data p =
+let optimize ~shapes ~profile ~keep_flow_data ~lambda_lift_all p =
   let deadcode_sentinal =
     (* If deadcode is disabled, this field is just fresh variable *)
     Code.Var.fresh_n "dummy"
@@ -730,7 +728,12 @@ let optimize ~shapes ~profile ~keep_flow_data p =
        | O2 -> o2
        | O3 -> o3)
     +> specialize_js_once_after
-    +> effects_and_exact_calls ~keep_flow_data ~deadcode_sentinal ~shapes profile
+    +> effects_and_exact_calls
+         ~keep_flow_data
+         ~deadcode_sentinal
+         ~shapes
+         ~lambda_lift_all
+         profile
     +> map_fst5
          (match Config.target (), Config.effects () with
          | `JavaScript, `Disabled -> Generate_closure.f
@@ -750,15 +753,26 @@ let optimize ~shapes ~profile ~keep_flow_data p =
 
 let optimize_for_wasm ~shapes ~profile p =
   let optimized_code, global_flow_data =
-    optimize ~shapes ~profile ~keep_flow_data:true p
+    optimize ~shapes ~profile ~keep_flow_data:true ~lambda_lift_all:false p
   in
   ( optimized_code
   , match global_flow_data with
     | Some data -> data
     | None -> Global_flow.f ~fast:false optimized_code.program )
 
-let full ~standalone ~wrap_with_fun ~shapes ~profile ~link ~source_map ~formatter p =
-  let optimized_code, _ = optimize ~shapes ~profile ~keep_flow_data:false p in
+let full
+    ~standalone
+    ~wrap_with_fun
+    ~shapes
+    ~profile
+    ~link
+    ~source_map
+    ~formatter
+    ~lambda_lift_all
+    p =
+  let optimized_code, _ =
+    optimize ~shapes ~profile ~keep_flow_data:false ~lambda_lift_all p
+  in
   let exported_runtime = not standalone in
   let emit formatter =
     generate ~exported_runtime ~wrap_with_fun ~warn_on_unhandled_effect:standalone
@@ -778,9 +792,26 @@ let full ~standalone ~wrap_with_fun ~shapes ~profile ~link ~source_map ~formatte
     shapes_v;
   emit formatter optimized_code, shapes_v
 
-let full_no_source_map ~formatter ~shapes ~standalone ~wrap_with_fun ~profile ~link p =
+let full_no_source_map
+    ~formatter
+    ~shapes
+    ~standalone
+    ~wrap_with_fun
+    ~profile
+    ~link
+    ~lambda_lift_all
+    p =
   let (_ : Source_map.info * _) =
-    full ~shapes ~standalone ~wrap_with_fun ~profile ~link ~source_map:false ~formatter p
+    full
+      ~shapes
+      ~standalone
+      ~wrap_with_fun
+      ~profile
+      ~link
+      ~source_map:false
+      ~formatter
+      ~lambda_lift_all
+      p
   in
   ()
 
@@ -792,17 +823,36 @@ let f
     ~link
     ~source_map
     ~formatter
+    ~lambda_lift_all
     p =
-  full ~standalone ~wrap_with_fun ~shapes ~profile ~link ~source_map ~formatter p
+  full
+    ~standalone
+    ~wrap_with_fun
+    ~shapes
+    ~profile
+    ~link
+    ~source_map
+    ~formatter
+    ~lambda_lift_all
+    p
 
 let f'
     ?(standalone = true)
     ?(wrap_with_fun = `Iife)
     ?(profile = Profile.O1)
+    ?(lambda_lift_all = false)
     ~link
     formatter
     p =
-  full_no_source_map ~formatter ~shapes:false ~standalone ~wrap_with_fun ~profile ~link p
+  full_no_source_map
+    ~formatter
+    ~shapes:false
+    ~standalone
+    ~wrap_with_fun
+    ~profile
+    ~link
+    ~lambda_lift_all
+    p
 
 let from_string ~prims ~debug s formatter =
   let p = Parse_bytecode.from_string ~prims ~debug s in
@@ -813,4 +863,5 @@ let from_string ~prims ~debug s formatter =
     ~wrap_with_fun:`Anonymous
     ~profile:O1
     ~link:`No
+    ~lambda_lift_all:false
     p
