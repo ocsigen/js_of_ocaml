8e53ed664ab51a6691bbba461ef53da62ce03e52
Jérôme Vouillon <jerome.vouillon@gmail.com>
2025-08-29 14:54:00 +0200

    WIP

--- compiler/lib-wasm/call_graph_analysis.mli
+++ compiler/lib-wasm/call_graph_analysis.mli
@@ -20,4 +20,7 @@ type t
 
 val direct_calls_only : t -> Code.Var.t -> bool
 
+val raising_functions :
+  Code.program -> Global_flow.info -> t -> (Code.Var.t -> bool) -> unit Code.Var.Hashtbl.t
+
 val f : Code.program -> Global_flow.info -> t
