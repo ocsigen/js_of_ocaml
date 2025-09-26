b0df5f1ce8e270bb767b8422ad8b599f7a3d28f7
Jérôme Vouillon <jerome.vouillon@gmail.com>
2025-09-26 19:02:38 +0200

    OxCaml: implement Float32 (32-bit float) support
    Add compiler and runtime support for OxCaml's unboxed float32 type,
    including arithmetic, conversions, bigarray, and marshaling.

--- compiler/lib/ocaml_compiler.ml
+++ compiler/lib/ocaml_compiler.ml
@@ -39,6 +39,8 @@ let rec constant_of_const c : Code.constant =
   | ((Const_float s) [@if ocaml_version >= (5, 5, 0)])
   | ((Const_base (Const_unboxed_float s)) [@if oxcaml]) ->
       Float (Int64.bits_of_float (float_of_string s))
+  | ((Const_base (Const_float32 s | Const_unboxed_float32 s)) [@if oxcaml]) ->
+      Float32 (Int64.bits_of_float (Float32.of_string s |> Float32.to_float))
   | ((Const_base (Const_int32 i)) [@if ocaml_version < (5, 5, 0)])
   | ((Const_int32 i) [@if ocaml_version >= (5, 5, 0)])
   | ((Const_base (Const_unboxed_int32 i)) [@if oxcaml]) -> Int32 i
@@ -62,8 +64,6 @@ let rec constant_of_const c : Code.constant =
   | ((Const_mixed_block (tag, _, l)) [@if oxcaml]) ->
       let l = Array.of_list (List.map l ~f:constant_of_const) in
       Tuple (tag, l, Unknown)
-  | ((Const_base (Const_float32 _ | Const_unboxed_float32 _)) [@if oxcaml]) ->
-      failwith "Float32 unsupported"
   | (Const_null [@if oxcaml]) -> Null_
 
 type module_or_not =
