13c2c607115107613f1a4083823859ed0be2ed56
Jérôme Vouillon <jerome.vouillon@gmail.com>
2026-02-26 23:47:51 +0100

    -

--- compiler/tests-wasm_of_ocaml/lcm_regression.ml
+++ compiler/tests-wasm_of_ocaml/lcm_regression.ml
@@ -1,15 +1,10 @@
-let slow_path x =
-  (* This forces x to be boxed as a generic value on a cold path. *)
-  ignore (Obj.magic x : int)
-
 let test n =
   let rec loop i acc =
     if i = 0
     then acc
-    else (
+    else
       let acc' = Int64.add acc (Int64.of_int i) in
-      if i = 1 then slow_path acc';
-      loop (i - 1) acc')
+      loop (i - 1) acc'
   in
   loop n 0L
 
