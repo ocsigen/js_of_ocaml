80ca356d20c3329186d7e2532acab49d92530820
Jérôme Vouillon <jerome.vouillon@gmail.com>
2026-02-26 18:20:24 +0100

    -

---
+++ compiler/tests-wasm_of_ocaml/lcm/lcm_test.ml
@@ -0,0 +1,18 @@
+let slow_path x =
+  (* This forces x to be boxed as a Value *)
+  ignore (Obj.magic x : 'a)
+
+let test n =
+  let rec loop i acc =
+    if i = 0 then acc
+    else
+      let acc' = acc + i in
+      if i = 1 then slow_path i; (* Conditional slow path *)
+      loop (i - 1) acc'
+  in
+  loop n 0
+
+let () =
+  let r = test 100 in
+  Printf.printf "Result: %d
+" r
