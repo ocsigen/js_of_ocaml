a0c3eeeefcf78168cec722895668a9ee7e3a3cf4
Jérôme Vouillon <jerome.vouillon@gmail.com>
2026-03-30 22:30:23 +0200

    Add more reference unboxing tests

--- compiler/tests-jsoo/ref_unboxing.ml
+++ compiler/tests-jsoo/ref_unboxing.ml
@@ -9,3 +9,15 @@ let f () =
     if Random.int 3 < 0 then incr r else decr r;
     !r
   with _ -> assert false
+
+(* Test: while loop with cross-assignment between refs.
+   This is the pattern from #2209: i := !pi followed by
+   reading !i (which should be the old !pi value). *)
+let loop_cross_assign () =
+  let i = ref 4 in
+  let pi = ref 2 in
+  while !i > 0 do
+    i := !pi;
+    pi := !i - 1
+  done;
+  !i
