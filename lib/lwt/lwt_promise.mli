b87e0e7b887bfbdc017c2b55e7cb6c41261c1471
Hugo Heuzard <hugo.heuzard@gmail.com>
2026-05-12 13:57:33 +0200

    Lib: add Lwt Promise interop
    Js_of_ocaml_lwt.Promise extends Js_of_ocaml.Promise with [to_lwt] and
    [of_lwt] for converting between Lwt promises and JavaScript promises.
    Rejections of foreign promises are surfaced to Lwt as a Failure carrying
    [Js_error.to_string].

--- lib/lwt/lwt_promise.mli
+++ lib/lwt/lwt_promise.mli
@@ -1,6 +1,5 @@
 (* Js_of_ocaml library
  * http://www.ocsigen.org/js_of_ocaml/
- * Copyright (C) 2017 Hugo Heuzard
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -17,21 +16,20 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  *)
 
-module XmlHttpRequest = struct
-  include Js_of_ocaml.XmlHttpRequest
-  include Lwt_xmlHttpRequest
-end
+(** Conversions between {!Js_of_ocaml.Promise} and {!Lwt}. *)
 
-module File = struct
-  include Js_of_ocaml.File
-  include Lwt_file
-end
+open Js_of_ocaml
 
-module Promise = struct
-  include Js_of_ocaml.Promise
-  include Lwt_promise
-end
+exception Rejected of Promise.error
+(** Raised by the [Lwt.t] returned by {!to_lwt} when the underlying
+    [Promise] rejects. The rejection reason is wrapped unchanged — even
+    when it is an OCaml exception (e.g. produced via
+    {!Promise.error_of_exn}), use {!Promise.error_to_any} to inspect it. *)
 
-module Jsonp = Lwt_jsonp
-module Lwt_js = Lwt_js
-module Lwt_js_events = Lwt_js_events
+val to_lwt : 'a Promise.t -> 'a Lwt.t
+(** [to_lwt p] is an Lwt thread that fulfils with the value [p] resolves
+    with, or fails with {!Rejected} if [p] is rejected. *)
+
+val of_lwt : 'a Lwt.t -> 'a Promise.t
+(** [of_lwt t] is a [Promise] that resolves with the value [t] returns,
+    or rejects with [Promise.error_of_exn exn] if [t] fails. *)
