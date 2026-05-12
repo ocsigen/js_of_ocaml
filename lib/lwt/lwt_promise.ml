edef468f19cf963ab7ef45b1b1cac55924c0fb98
Hugo Heuzard <hugo.heuzard@gmail.com>
2026-05-12 13:57:33 +0200

    Lib: add Lwt Promise interop
    Js_of_ocaml_lwt.Promise extends Js_of_ocaml.Promise with [to_lwt] and
    [of_lwt] for converting between Lwt promises and JavaScript promises.
    Rejections of foreign promises are surfaced to Lwt as a Failure carrying
    [Js_error.to_string].

--- lib/lwt/lwt_promise.ml
+++ lib/lwt/lwt_promise.ml
@@ -1,6 +1,5 @@
 (* Js_of_ocaml library
  * http://www.ocsigen.org/js_of_ocaml/
- * Copyright (C) 2017 Hugo Heuzard
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -17,21 +16,24 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  *)
 
-module XmlHttpRequest = struct
-  include Js_of_ocaml.XmlHttpRequest
-  include Lwt_xmlHttpRequest
-end
+open Js_of_ocaml
 
-module File = struct
-  include Js_of_ocaml.File
-  include Lwt_file
-end
+exception Rejected of Promise.error
 
-module Promise = struct
-  include Js_of_ocaml.Promise
-  include Lwt_promise
-end
+let to_lwt (p : 'a Promise.t) : 'a Lwt.t =
+  let t, w = Lwt.task () in
+  let _ : unit Promise.t =
+    Promise.then_
+      ~on_error:(fun e ->
+        Lwt.wakeup_later_exn w (Rejected e);
+        Promise.resolve ())
+      (fun x ->
+        Lwt.wakeup_later w x;
+        Promise.resolve ())
+      p
+  in
+  t
 
-module Jsonp = Lwt_jsonp
-module Lwt_js = Lwt_js
-module Lwt_js_events = Lwt_js_events
+let of_lwt (t : 'a Lwt.t) : 'a Promise.t =
+  Promise.make (fun ~resolve ~reject ->
+      Lwt.on_any t resolve (fun exn -> reject (Promise.error_of_exn exn)))
