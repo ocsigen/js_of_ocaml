6b69e4720a7312ed4a5e23265819d9a33b088fe0
Jérôme Vouillon <jerome.vouillon@gmail.com>
2026-05-26 12:43:14 +0200

    jsooTopError: support OCaml 5.6 typing error reorganisation
    In OCaml 5.6 the Typecore/Typetexp/Typeclass/Typedecl Error exception
    moved into an Error sub-module as `Error.In_context`, and Typemod's
    `Error` was replaced by `Errors` carrying a `Typing_recovery.Error_set.t`.
    Gate each affected pattern with `ppx_optcomp_light` so the toplevel
    builds against both pre- and post-5.6 compiler-libs.

--- toplevel/lib-common/error_loc.ml
+++ toplevel/lib-common/error_loc.ml
@@ -17,14 +17,20 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  *)
 
-let loc = function
+let loc e =
+  match e with
   | Syntaxerr.Error x -> Some (Syntaxerr.location_of_error x)
   | Lexer.Error (_, loc)
-  | Typecore.Error (loc, _, _)
-  | Typetexp.Error (loc, _, _)
-  | Typeclass.Error (loc, _, _)
-  | Typemod.Error (loc, _, _)
-  | Typedecl.Error (loc, _)
+  | ((Typecore.Error (loc, _, _)) [@if ocaml_version < (5, 6, 0)])
+  | ((Typecore.Error.In_context (loc, _, _)) [@if ocaml_version >= (5, 6, 0)])
+  | ((Typetexp.Error (loc, _, _)) [@if ocaml_version < (5, 6, 0)])
+  | ((Typetexp.Error.In_context (loc, _, _)) [@if ocaml_version >= (5, 6, 0)])
+  | ((Typeclass.Error (loc, _, _)) [@if ocaml_version < (5, 6, 0)])
+  | ((Typeclass.Error.In_context (loc, _, _)) [@if ocaml_version >= (5, 6, 0)])
+  | ((Typemod.Error (loc, _, _)) [@if ocaml_version < (5, 6, 0)])
+  | ((Typemod.Errors (loc, _)) [@if ocaml_version >= (5, 6, 0)])
+  | ((Typedecl.Error (loc, _)) [@if ocaml_version < (5, 6, 0)])
+  | ((Typedecl.Error.In_context (loc, _)) [@if ocaml_version >= (5, 6, 0)])
   | Translcore.Error (loc, _)
   | Translclass.Error (loc, _)
   | Translmod.Error (loc, _) -> Some loc
