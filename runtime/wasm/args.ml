a69ce164ee440223d8036b93811cda919d365aab
Shon Feder <shon.feder@gmail.com>
2026-06-24 22:25:25 -0400

    Runtime/wasm: fix path handling from dune variable in args.ml
    Dune `deps` variables are expanded to paths. The representation of this
    has changed with https://github.com/ocaml/dune/pull/15156, causing the
    wasm_of_ocaml runtime build to fail on the pending dune 3.24: when
    `args.ml` is run with the `.wat` deps in the rule's own directory those
    arguments are now "./foo.wat" rather than "foo.wat", and
    `Filename.chop_suffix ".wat"` on them produces "./foo", which is not a valid
    wasm-link module name. By taking the basename of the path before chopping the
    suffix, we get a fix that works regardless of the path representation.

--- runtime/wasm/args.ml
+++ runtime/wasm/args.ml
@@ -1,4 +1,7 @@
 let () =
   for i = 1 to Array.length Sys.argv - 1 do
-    Format.printf "%s:%s@." (Filename.chop_suffix Sys.argv.(i) ".wat") Sys.argv.(i)
+    Format.printf
+      "%s:%s@."
+      (Filename.chop_suffix (Filename.basename Sys.argv.(i)) ".wat")
+      Sys.argv.(i)
   done
