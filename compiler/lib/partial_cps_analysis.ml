f6350b1d9e1fd8eed6e4e124bc0c39747d70573f
Robin Ricard <rricard@janestreet.com>
2026-08-20 21:49:10 +0100

    OxCaml: preemptible fibers support (oxcaml/oxcaml#5913)
    Changes for https://github.com/oxcaml/oxcaml/pull/5913
    
    Co-authored-by: Aspen Smith <aspsmith@janestreet.com>

--- compiler/lib/partial_cps_analysis.ml
+++ compiler/lib/partial_cps_analysis.ml
@@ -110,7 +110,9 @@ let block_deps ~info ~vars ~tail_deps ~deps ~blocks ~fun_name pc =
                     | "%reperform"
                     | "%resume"
                     | "%with_stack"
-                    | "%with_stack_bind" )
+                    | "%with_stack_bind"
+                    | "%with_stack_preemptible"
+                    | "%with_stack_bind_preemptible" )
                   , _ )
               , _ ) ) -> (
           add_var vars x;
@@ -220,7 +222,13 @@ let cps_needed ~info ~in_mutual_recursion ~rev_deps ~below st x =
   | Expr
       (Prim
          ( Extern
-             ( ("%perform" | "%reperform" | "%resume" | "%with_stack" | "%with_stack_bind")
+             ( ( "%perform"
+               | "%reperform"
+               | "%resume"
+               | "%with_stack"
+               | "%with_stack_bind"
+               | "%with_stack_preemptible"
+               | "%with_stack_bind_preemptible" )
              , _ )
          , _ )) ->
       (* Effects primitives are in CPS *)
