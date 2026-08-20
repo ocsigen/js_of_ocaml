c8e0dcf08cbf36746f0b0fdbf971c746ea9194c1
Robin Ricard <rricard@janestreet.com>
2026-08-20 21:49:12 +0100

    OxCaml: handle the Event_after_untyped debug event kind
    Handle the [Event_after_untyped] debug event kind.
    Adapted for this tree: the Event_after_untyped alternatives are gated on
    [@if oxcaml], since mainstream OCaml has no such debug event kind.

--- compiler/lib/parse_bytecode.ml
+++ compiler/lib/parse_bytecode.ml
@@ -202,7 +202,8 @@ end = struct
           match ev.ev_kind, ev.ev_repr with
           | Event_pseudo, Event_none -> `Tail
           | Event_pseudo, (Event_parent _ | Event_child _)
-          | (Event_before | Event_after _), _ -> `Non_tail
+          | (Event_before | Event_after _), _
+          | ((Event_after_untyped, _) [@if oxcaml]) -> `Non_tail
         in
         Some (call, nargs)
     | Event_function | Event_return _ | Event_other -> None
@@ -1061,7 +1062,7 @@ let string_of_addr debug_data addr =
       let kind =
         match kind with
         | Event_before -> "(before)"
-        | Event_after _ -> "(after)"
+        | Event_after _ | (Event_after_untyped [@if oxcaml]) -> "(after)"
         | Event_pseudo -> "(pseudo)"
       in
       Printf.sprintf "%s:%s-%s %s" file (pos loc.loc_start) (pos loc.loc_end) kind)
@@ -1235,7 +1236,10 @@ and compile infos pc state (instrs : instr list) =
             instrs
         | ( { ev_kind = Event_pseudo | Event_after _; ev_info = Event_return _; _ }
           , (Let (_, (Apply _ | Prim _)) as i) :: rem )
-        | (( { ev_kind = Event_pseudo | Event_after _
+        | (( { ev_kind = Event_after_untyped; ev_info = Event_return _; _ }
+           , (Let (_, (Apply _ | Prim _)) as i) :: rem )
+           [@if oxcaml])
+        | (( { ev_kind = Event_pseudo | Event_after _ | Event_after_untyped
              ; ev_info = Event_unyielding_call _
              ; _
              }
@@ -1254,7 +1258,9 @@ and compile infos pc state (instrs : instr list) =
             if debug_parser () then Format.eprintf "Added event at function start@.";
             push_event Before source event instrs
         | { ev_kind = Event_after _ | Event_pseudo; ev_info = Event_return _; _ }, _
-        | (( { ev_kind = Event_after _ | Event_pseudo
+        | (({ ev_kind = Event_after_untyped; ev_info = Event_return _; _ }, _)
+           [@if oxcaml])
+        | (( { ev_kind = Event_after _ | Event_after_untyped | Event_pseudo
              ; ev_info = Event_unyielding_call _
              ; _
              }
@@ -1268,7 +1274,9 @@ and compile infos pc state (instrs : instr list) =
             then
               Format.eprintf "Ignored useless event (beginning of a block after a call)@.";
             instrs
-        | { ev_kind = Event_after _; ev_info = Event_other; _ }, _ ->
+        | { ev_kind = Event_after _; ev_info = Event_other; _ }, _
+        | (({ ev_kind = Event_after_untyped; ev_info = Event_other; _ }, _) [@if oxcaml])
+          ->
             if debug_parser ()
             then Format.eprintf "Ignored useless event (before a raise)@.";
             (* We already have an event for the exception. The
@@ -1279,6 +1287,8 @@ and compile infos pc state (instrs : instr list) =
             if debug_parser () then Format.eprintf "added event@.";
             push_event Before source event instrs
         | { ev_kind = Event_after _; ev_info = Event_function; _ }, _
+        | (({ ev_kind = Event_after_untyped; ev_info = Event_function; _ }, _)
+           [@if oxcaml])
         | { ev_kind = Event_before; ev_info = Event_return _; _ }, _
         | (({ ev_kind = Event_before; ev_info = Event_unyielding_call _; _ }, _)
            [@if oxcaml]) ->
