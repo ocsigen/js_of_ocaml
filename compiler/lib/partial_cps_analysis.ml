9e7688bc15e96bd0445bb3008568dc3efb9519cf
Robin Ricard <rricard@janestreet.com>
2026-08-20 21:49:11 +0100

    OxCaml: remove with_stack_bind

--- compiler/lib/partial_cps_analysis.ml
+++ compiler/lib/partial_cps_analysis.ml
@@ -110,9 +110,7 @@ let block_deps ~info ~vars ~tail_deps ~deps ~blocks ~fun_name pc =
                     | "%reperform"
                     | "%resume"
                     | "%with_stack"
-                    | "%with_stack_bind"
-                    | "%with_stack_preemptible"
-                    | "%with_stack_bind_preemptible" )
+                    | "%with_stack_preemptible" )
                   , _ )
               , _ ) ) -> (
           add_var vars x;
@@ -226,9 +224,7 @@ let cps_needed ~info ~in_mutual_recursion ~rev_deps ~below st x =
                | "%reperform"
                | "%resume"
                | "%with_stack"
-               | "%with_stack_bind"
-               | "%with_stack_preemptible"
-               | "%with_stack_bind_preemptible" )
+               | "%with_stack_preemptible" )
              , _ )
          , _ )) ->
       (* Effects primitives are in CPS *)
