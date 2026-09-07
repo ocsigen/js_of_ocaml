12c30ef169efd236f8f5b564b07b31fba9222104
Jérôme Vouillon <jerome.vouillon@gmail.com>
2026-09-07 15:47:56 +0200

    Revert "runtime/wasm: pass a boolean to TextDecoder ignoreBOM"
    This reverts commit 4faaa82625422c4ea4ca6cf7d1ffdc9bb89df9cd.

--- runtime/wasm/runtime.js
+++ runtime/wasm/runtime.js
@@ -163,7 +163,7 @@
       : f;
   }
 
-  const decoder = new TextDecoder("utf-8", { ignoreBOM: true });
+  const decoder = new TextDecoder("utf-8", { ignoreBOM: 1 });
   const encoder = new TextEncoder();
 
   // View on the scratch area used to convert between OCaml and JavaScript
