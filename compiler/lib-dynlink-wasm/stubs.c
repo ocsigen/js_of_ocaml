b4aeaf8b8a5c67a216927251986364ae315c802b
Robin Ricard <rricard@janestreet.com>
2026-09-09 07:51:50 +0100

    Lib: emit the native runtime stubs as weak symbols
    Allow libraries linking JavaScript primitives in native code to override
    the generated unimplemented-primitive stubs.

--- compiler/lib-dynlink-wasm/stubs.c
+++ compiler/lib-dynlink-wasm/stubs.c
@@ -1,29 +1,29 @@
 #include <caml/misc.h>
 
-void caml_wasm_load_module () {
+CAMLweakdef void caml_wasm_load_module () {
   caml_fatal_error("Unimplemented Javascript primitive caml_wasm_load_module!");
 }
 
-void caml_wasm_load_wasmo () {
+CAMLweakdef void caml_wasm_load_wasmo () {
   caml_fatal_error("Unimplemented Javascript primitive caml_wasm_load_wasmo!");
 }
 
-void caml_wasm_register_fragments () {
+CAMLweakdef void caml_wasm_register_fragments () {
   caml_fatal_error("Unimplemented Javascript primitive caml_wasm_register_fragments!");
 }
 
-void wasm_get_bytecode_sections () {
+CAMLweakdef void wasm_get_bytecode_sections () {
   caml_fatal_error("Unimplemented Javascript primitive wasm_get_bytecode_sections!");
 }
 
-void wasm_get_runtime_aliases () {
+CAMLweakdef void wasm_get_runtime_aliases () {
   caml_fatal_error("Unimplemented Javascript primitive wasm_get_runtime_aliases!");
 }
 
-void wasm_toplevel_init_compile () {
+CAMLweakdef void wasm_toplevel_init_compile () {
   caml_fatal_error("Unimplemented Javascript primitive wasm_toplevel_init_compile!");
 }
 
-void wasm_toplevel_init_reloc () {
+CAMLweakdef void wasm_toplevel_init_reloc () {
   caml_fatal_error("Unimplemented Javascript primitive wasm_toplevel_init_reloc!");
 }
